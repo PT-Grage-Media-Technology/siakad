8c4be534955683b00455783c91527745169eb97f
Baihaqi34 <abay999mmm@gmail.com>
2024-10-03 10:16:21 +0700

    sdd

--- login.php
+++ login.php
@@ -80,8 +80,8 @@ if (isset($_POST[login])) {
   $passlain = anti_injection($_POST[b]);
   $data = md5(anti_injection($_POST[b]));
   $pass = hash("sha512", $data);
-  var_dump($data);
-  var_dump($pass);
+  var_dump('pass', $data);
+  var_dump('pass sha512', $pass);
   $admin = mysql_query("SELECT * FROM rb_users WHERE username='" . anti_injection($_POST[a]) . "' AND password='$pass'");
   $guru = mysql_query("SELECT * FROM rb_guru WHERE nip='" . anti_injection($_POST[a]) . "' AND password='$passlain'");
   $siswa = mysql_query("SELECT * FROM rb_siswa WHERE nisn='" . anti_injection($_POST[a]) . "' AND password='$passlain'");
@@ -116,8 +116,8 @@ if (isset($_POST[login])) {
     mysql_query("INSERT INTO rb_users_aktivitas VALUES('','$r[nisn]','$ip','$user_browser $version','$user_os','siswa','" . date('H:i:s') . "','" . date('Y-m-d') . "')");
     echo "<script>document.location='index.php';</script>";
   } else {
-    var_dump($data);
-    var_dump($pass);
+    // var_dump($data);
+    // var_dump($pass);
     // echo "<script>window.alert('Maaf, Anda Tidak Memiliki akses');
     //                               window.location=('index.php?view=login')</script>";
   }
