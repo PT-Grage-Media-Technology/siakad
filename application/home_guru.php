9e54ad65c73bbb15bb1327df0c1b388a3b6ae29f
jipyy <jahdanpandita16@gmail.com>
2024-10-16 09:43:00 +0700

    update

--- application/home_guru.php
+++ application/home_guru.php
@@ -1,87 +1,90 @@
-<?php
-// Ambil tahun akademik terbaru (id_tahun_akademik paling besar)
-$latest_year = mysql_fetch_array(mysql_query("SELECT id_tahun_akademik FROM rb_tahun_akademik ORDER BY id_tahun_akademik DESC LIMIT 1"));
+            <div class="col-xs-12">
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title"><?php if (isset($_GET[tahun])) {
+                                          echo "Jadwal Mengajar Anda";
+                                        } else {
+                                          echo "Jadwal Mengajar Anda " . date('Y');
+                                        } ?></h3>
+                  <?php
+                  // Ambil tahun akademik terbaru (id_tahun_akademik paling besar)
+                  $latest_year = mysql_fetch_array(mysql_query("SELECT id_tahun_akademik FROM rb_tahun_akademik ORDER BY id_tahun_akademik DESC LIMIT 1"));
 
-// Jika tidak ada tahun akademik dipilih, set default ke tahun terbaru dan redirect
-$tahun_dipilih = isset($_GET['tahun']) ? $_GET['tahun'] : $latest_year['id_tahun_akademik'];
+                  // Jika tidak ada tahun akademik dipilih, set default ke tahun terbaru
+                  $tahun_dipilih = isset($_GET['tahun']) ? $_GET['tahun'] : $latest_year['id_tahun_akademik'];
+                  ?>
 
-// Jika tahun tidak ada di URL, redirect dengan tahun terbaru
-if (!isset($_GET['tahun'])) {
-  header("Location: ?tahun=" . $latest_year['id_tahun_akademik']);
-  exit();
-}
-?>
+                  <form style='margin-right:5px; margin-top:0px' class='pull-right' action='' method='GET'>
+                    <select name='tahun' style='padding:4px'>
+                      <?php
+                      echo "<option value=''>- Pilih Tahun Akademik -</option>";
+                      $tahun = mysql_query("SELECT * FROM rb_tahun_akademik");
+                      while ($k = mysql_fetch_array($tahun)) {
+                        // Jika tahun yang dipilih sama dengan tahun yang ada di query, set selected
+                        if ($tahun_dipilih == $k['id_tahun_akademik']) {
+                          echo "<option value='$k[id_tahun_akademik]' selected>$k[nama_tahun]</option>";
+                        } else {
+                          echo "<option value='$k[id_tahun_akademik]'>$k[nama_tahun]</option>";
+                        }
+                      }
+                      ?>
+                    </select>
+                    <input type="submit" style='margin-top:-4px' class='btn btn-success btn-sm' value='Lihat'>
+                  </form>
 
-<div class="col-xs-12">
-  <div class="box">
-    <div class="box-header">
-      <h3 class="box-title">Jadwal Mengajar Anda <?php echo "Tahun Akademik $tahun_dipilih"; ?></h3>
 
-      <form style='margin-right:5px; margin-top:0px' class='pull-right' action='' method='GET'>
-        <select name='tahun' style='padding:4px'>
-          <?php
-          echo "<option value=''>- Pilih Tahun Akademik -</option>";
-          $tahun = mysql_query("SELECT * FROM rb_tahun_akademik");
-          while ($k = mysql_fetch_array($tahun)) {
-            // Jika tahun yang dipilih sama dengan tahun yang ada di query, set selected
-            if ($tahun_dipilih == $k['id_tahun_akademik']) {
-              echo "<option value='$k[id_tahun_akademik]' selected>$k[nama_tahun]</option>";
-            } else {
-              echo "<option value='$k[id_tahun_akademik]'>$k[nama_tahun]</option>";
-            }
-          }
-          ?>
-        </select>
-        <input type="submit" style='margin-top:-4px' class='btn btn-success btn-sm' value='Lihat'>
-      </form>
-    </div><!-- /.box-header -->
-
-    <div class="box-body">
-      <table id="example1" class="table table-bordered table-striped">
-        <thead>
-          <tr>
-            <th style='width:20px'>No</th>
-            <th>Kode Pelajaran</th>
-            <th>Jadwal Pelajaran</th>
-            <th>Kelas</th>
-            <th>Guru</th>
-            <th>Hari</th>
-            <th>Mulai</th>
-            <th>Selesai</th>
-            <th>Ruangan</th>
-            <th>Semester</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-          // Tampilkan jadwal berdasarkan tahun yang dipilih
-          $tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan 
-                                FROM rb_jadwal_pelajaran a 
-                                JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
-                                JOIN rb_guru c ON a.nip=c.nip 
-                                JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
-                                JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
-                                WHERE a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$tahun_dipilih' 
-                                ORDER BY a.hari DESC");
-
-          $no = 1;
-          while ($r = mysql_fetch_array($tampil)) {
-            echo "<tr><td>$no</td>
-                  <td>$r[kode_pelajaran]</td>
-                  <td>$r[namamatapelajaran]</td>
-                  <td>$r[nama_kelas]</td>
-                  <td>$r[nama_guru]</td>
-                  <td>$r[hari]</td>
-                  <td>$r[jam_mulai]</td>
-                  <td>$r[jam_selesai]</td>
-                  <td>$r[nama_ruangan]</td>
-                  <td>$r[id_tahun_akademik]</td>
-                </tr>";
-            $no++;
-          }
-          ?>
-        </tbody>
-      </table>
-    </div><!-- /.box-body -->
-  </div>
-</div>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                  <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th style='width:20px'>No</th>
+                        <th>Kode Pelajaran</th>
+                        <th>Jadwal Pelajaran</th>
+                        <th>Kelas</th>
+                        <th>Guru</th>
+                        <th>Hari</th>
+                        <th>Mulai</th>
+                        <th>Selesai</th>
+                        <th>Ruangan</th>
+                        <th>Semester</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php
+                      if (isset($_GET[tahun])) {
+                        $tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan FROM rb_jadwal_pelajaran a 
+                                            JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
+                                              JOIN rb_guru c ON a.nip=c.nip 
+                                                JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
+                                                  JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
+                                                  where a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$_GET[tahun]' ORDER BY a.hari DESC");
+                      } else {
+                        $tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan FROM rb_jadwal_pelajaran a 
+                                            JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
+                                              JOIN rb_guru c ON a.nip=c.nip 
+                                                JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
+                                                JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
+                                                  where a.nip='$_SESSION[id]' AND a.id_tahun_akademik LIKE '" . date('Y') . "%' ORDER BY a.hari DESC");
+                      }
+                      $no = 1;
+                      while ($r = mysql_fetch_array($tampil)) {
+                        echo "<tr><td>$no</td>
+                              <td>$r[kode_pelajaran]</td>
+                              <td>$r[namamatapelajaran]</td>
+                              <td>$r[nama_kelas]</td>
+                              <td>$r[nama_guru]</td>
+                              <td>$r[hari]</td>
+                              <td>$r[jam_mulai]</td>
+                              <td>$r[jam_selesai]</td>
+                              <td>$r[nama_ruangan]</td>
+                              <td>$r[id_tahun_akademik]</td>
+                          </tr>";
+                        $no++;
+                      }
+                      ?>
+                    </tbody>
+                  </table>
+                </div><!-- /.box-body -->
+              </div>
+            </div>
