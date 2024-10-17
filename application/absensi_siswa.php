f7e17343753684f13ba32c4e549f82591de0e627
Baihaqi34 <abay999mmm@gmail.com>
2024-10-17 15:52:37 +0700

    SDAD

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -157,7 +157,7 @@
   }
   $d = mysql_fetch_array(mysql_query("SELECT * FROM rb_kelas where kode_kelas='$_GET[id]'"));
   $m = mysql_fetch_array(mysql_query("SELECT * FROM rb_mata_pelajaran where kode_pelajaran='$_GET[kd]'"));
-  $j = mysql_fetch_array(mysql_query("SELECT * FROM rb_journal_list where kodejdwl='$_GET[kd]'"));
+  // $j = mysql_fetch_array(mysql_query("SELECT * FROM rb_journal_list where kodejdwl='$_GET[kd]'"));
   $j = mysql_fetch_array(mysql_query("SELECT * FROM rb_journal_list where kodejdwl='33'"));
 
   $ex = explode('-', $filtertgl);
