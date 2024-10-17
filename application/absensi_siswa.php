bc820cfae74dfa58a2ed8c8d5ac2e199f05944e2
Baihaqi34 <abay999mmm@gmail.com>
2024-10-17 15:47:03 +0700

    sasasas

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -157,7 +157,7 @@
   }
   $d = mysql_fetch_array(mysql_query("SELECT * FROM rb_kelas where kode_kelas='$_GET[id]'"));
   $m = mysql_fetch_array(mysql_query("SELECT * FROM rb_mata_pelajaran where kode_pelajaran='$_GET[kd]'"));
-  $j = $r[kodejdwl];
+  $j = mysql_fetch_array(mysql_query("SELECT * FROM rb_journal_list where kodejdwl='$_GET[kd]'"));
 
   $ex = explode('-', $filtertgl);
   $tahun = $ex[0];
@@ -187,7 +187,7 @@
                     <tr><th width='120px' scope='row'>Kode Kelas</th> <td>$d[kode_kelas]</td></tr>
                     <tr><th scope='row'>Nama Kelas</th>               <td>$d[nama_kelas]</td></tr>
                     <tr><th scope='row'>Mata Pelajaran</th>           <td>$m[namamatapelajaran]</td></tr>
-                    <tr><th scope='row'>Tujuan Pembelajaran</th>           <td>$j</td></tr>
+                    <tr><th scope='row'>Tujuan Pembelajaran</th>           <td>$j[materi]</td></tr>
                   </tbody>
               </table>
               </div>
