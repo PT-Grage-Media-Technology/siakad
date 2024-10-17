90b07a3532524f3843d778c337a2e3bad3e903da
ArvinoDel <andikasupriyadi27@gmail.com>
2024-10-17 14:03:44 +0700

    undo

--- application/journal_guru.php
+++ application/journal_guru.php
@@ -26,80 +26,76 @@
           </select>
           <input type="submit" style='margin-top:-4px' class='btn btn-success btn-sm' value='Lihat'>
         </form>
+
       </div><!-- /.box-header -->
-
-      <!-- Tabel dibungkus dengan table-responsive untuk scroll-x -->
       <div class="box-body">
-        <div class="table-responsive"> <!-- Tambahkan div ini -->
-          <table class="table table-bordered table-striped">
-            <thead>
-              <tr>
-                <th style='width:20px'>No</th>
-                <th>Kode Pelajaran</th>
-                <th>Jadwal Pelajaran</th>
-                <th>Kelas</th>
-                <th>Guru</th>
-                <th>Hari</th>
-                <th>Mulai</th>
-                <th>Selesai</th>
-                <th>Ruangan</th>
-                <th>Semester</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              if (isset($_GET[tahun])) {
-                $tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan FROM rb_jadwal_pelajaran a 
-                                              JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
-                                                JOIN rb_guru c ON a.nip=c.nip 
-                                                  JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
-                                                    JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
-                                                    where a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$_GET[tahun]' ORDER BY a.hari DESC");
-
-              } else {
-                $tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan FROM rb_jadwal_pelajaran a 
-                                              JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
-                                                JOIN rb_guru c ON a.nip=c.nip 
-                                                  JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
+        <table class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th style='width:20px'>No</th>
+              <th>Kode Pelajaran</th>
+              <th>Jadwal Pelajaran</th>
+              <th>Kelas</th>
+              <th>Guru</th>
+              <th>Hari</th>
+              <th>Mulai</th>
+              <th>Selesai</th>
+              <th>Ruangan</th>
+              <th>Semester</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            if (isset($_GET[tahun])) {
+              $tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan FROM rb_jadwal_pelajaran a 
+                                            JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
+                                              JOIN rb_guru c ON a.nip=c.nip 
+                                                JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
                                                   JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
-                                                    where a.nip='$_SESSION[id]' AND a.id_tahun_akademik LIKE '" . date('Y') . "%' ORDER BY a.hari DESC");
-              }
-              $no = 1;
-              while ($r = mysql_fetch_array($tampil)) {
-                echo "<tr><td>$no</td>
-                                <td>$r[kode_pelajaran]</td>
-                                <td>$r[namamatapelajaran]</td>
-                                <td>$r[nama_kelas]</td>
-                                <td>$r[nama_guru]</td>
-                                <td>$r[hari]</td>
-                                <td>$r[jam_mulai]</td>
-                                <td>$r[jam_selesai]</td>
-                                <td>$r[nama_ruangan]</td>
-                                <td>$r[id_tahun_akademik]</td>
-                                <td style='width:80px !important'><center>
-                                          <a class='btn btn-success btn-xs' title='Lihat Journal' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]'><span class='glyphicon glyphicon-search'></span> Lihat Journal</a>
-                                        </center></td>
-                            </tr>";
-                $no++;
-              }
-              ?>
-            </tbody>
-          </table>
-        </div> <!-- Akhir div table-responsive -->
+                                                  where a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$_GET[tahun]' ORDER BY a.hari DESC");
+
+            } else {
+              $tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan FROM rb_jadwal_pelajaran a 
+                                            JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
+                                              JOIN rb_guru c ON a.nip=c.nip 
+                                                JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
+                                                JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
+                                                  where a.nip='$_SESSION[id]' AND a.id_tahun_akademik LIKE '" . date('Y') . "%' ORDER BY a.hari DESC");
+            }
+            $no = 1;
+            while ($r = mysql_fetch_array($tampil)) {
+              echo "<tr><td>$no</td>
+                              <td>$r[kode_pelajaran]</td>
+                              <td>$r[namamatapelajaran]</td>
+                              <td>$r[nama_kelas]</td>
+                              <td>$r[nama_guru]</td>
+                              <td>$r[hari]</td>
+                              <td>$r[jam_mulai]</td>
+                              <td>$r[jam_selesai]</td>
+                              <td>$r[nama_ruangan]</td>
+                              <td>$r[id_tahun_akademik]</td>
+                              <td style='width:80px !important'><center>
+                                        <a class='btn btn-success btn-xs' title='Lihat Journal' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]'><span class='glyphicon glyphicon-search'></span> Lihat Journal</a>
+                                      </center></td>
+                          </tr>";
+              $no++;
+            }
+            ?>
+          </tbody>
+        </table>
       </div><!-- /.box-body -->
     </div>
   </div>
 
-
-  <?php
+<?php
 } elseif ($_GET[act] == 'lihat') {
   $d = mysql_fetch_array(mysql_query("SELECT a.kode_kelas, b.nama_kelas, c.namamatapelajaran, d.nama_guru FROM `rb_jadwal_pelajaran` a JOIN rb_kelas b ON a.kode_kelas=b.kode_kelas JOIN rb_mata_pelajaran c ON a.kode_pelajaran=c.kode_pelajaran JOIN rb_guru d ON a.nip=d.nip where a.kodejdwl='$_GET[id]'"));
   echo "<div class='col-xs-12'>  
               <div class='box'>
                 <div class='box-header'>
                   <h3 class='box-title'>Journal Kegiatan Belajar Mengajar</h3>
-                      <a style='margin-left:5px;display:none;' class='pull-right btn btn-success btn-sm' href='index.php?view=kompetensidasar&act=lihat&id=$_GET[id]'>Lihat Kompetensi Dasar</a>";
+                      <a style='margin-left:5px' class='pull-right btn btn-success btn-sm' href='index.php?view=kompetensidasar&act=lihat&id=$_GET[id]'>Lihat Kompetensi Dasar</a>";
   if ($_SESSION[level] != 'kepala') {
     echo "<a class='pull-right btn btn-primary btn-sm' href='index.php?view=journalguru&act=tambah&jdwl=$_GET[id]'>Tambahkan Journal</a>";
   }
