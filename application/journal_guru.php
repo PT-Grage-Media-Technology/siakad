fdfcb77d56b3fc834009fcec85bba09adf6d3316
Baihaqi34 <abay999mmm@gmail.com>
2024-10-18 08:37:35 +0700

    update

--- application/journal_guru.php
+++ application/journal_guru.php
@@ -147,7 +147,7 @@
                               <td>$r[keterangan]</td>";
     if ($_SESSION[level] != 'kepala') {
       echo "<td style='width:80px !important'><center>
-                                        <a class='btn btn-success btn-xs' title='Edit Data' href='index.php?view=absensiswa&act=tampilabsen&id=$d[kode_kelas]&kd=$d[kode_pelajaran]&idjr=$_GET[id]'><span class='glyphicon glyphicon-edit'>Absen</span></a>
+                                        <a class='btn btn-success btn-xs' title='Edit Data' href='index.php?view=absensiswa&act=tampilabsen&id=$d[kode_kelas]&kd=$d[kode_pelajaran]&idjr=$_GET[id]&tgl=" . tgl_indo($r[tanggal]) . "&jam=$r[jam_ke]'><span class='glyphicon glyphicon-edit'>Absen</span></a>
                                         <a class='btn btn-success btn-xs' title='Edit Data' href='index.php?view=journalguru&act=edit&id=$r[id_journal]&jdwl=$_GET[id]'><span class='glyphicon glyphicon-edit'></span></a>
                                         <a class='btn btn-danger btn-xs' title='Delete Data' href='index.php?view=journalguru&hapus=$r[id_journal]&jdwl=$_GET[id]'><span class='glyphicon glyphicon-remove'></span></a>
                                       </center></td>";
