40f05facee6081863036ad6aff3c6135afbbe749
Baihaqi34 <abay999mmm@gmail.com>
2024-10-23 09:28:09 +0700

    ddad

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if (isset($_SESSION[id])) {
     }
   } elseif ($_SESSION[level] == 'wakaKurikulum') {
     $iden = mysql_fetch_array(mysql_query("SELECT * FROM rb_guru where nip='$_SESSION[id]' AND id_jenis_ptk=6"));
-    $nama = $iden[nama_guru];
+    $nama = 'testing kuri';
     $level = 'Kurikulum';
     if (trim($iden[foto]) == '') {
       $foto = 'foto_siswa/no-image.jpg';
