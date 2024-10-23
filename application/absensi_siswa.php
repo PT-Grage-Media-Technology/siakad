152d211277aad3f0f5a9efce2513bdf937664d6b
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-10-23 14:02:10 +0700

    321

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -203,9 +203,11 @@
                         </tr>
                         <tr>
                             <th scope='row'>
-                                <a class='btn btn-success btn-sm d-flex flex-column' title='Bahan dan Tugas' href='https://siakad.demogmt.online/index.php?view=bahantugas&act=listbahantugas&jdwl=$_GET[idjr]&id=$_GET[id]&kd=$_GET[kd]'>
+                                <a class='btn btn-success btn-sm' title='Bahan dan Tugas' href='https://siakad.demogmt.online/index.php?view=bahantugas&act=listbahantugas&jdwl=$_GET[idjr]&id=$_GET[id]&kd=$_GET[kd]'>
+                                  <div class='d-flex flex-column'>
                                     <span class='glyphicon glyphicon-tasks' style='font-size:18px; margin-right:5px;'></span>
                                     <span class='' style='font-size:16px;'>Tugas</span>
+                                  </div>
                                 </a>
                             </th>
                         </tr>
