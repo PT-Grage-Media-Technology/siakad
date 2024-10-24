cb7da4e5a49f415b6817b08198d6db22ed9c479c
Baihaqi34 <abay999mmm@gmail.com>
2024-10-24 16:31:42 +0700

    123

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -205,8 +205,8 @@
                             <th scope='row'>
                                 <a class='btn btn-success btn-sm' title='Bahan dan Tugas' href='https://siakad.demogmt.online/index.php?view=bahantugas&act=listbahantugas&jdwl=$_GET[idjr]&id=$_GET[id]&kd=$_GET[kd]'>
                                   <div class='d-flex flex-column'>
-                                    <span class='glyphicon glyphicon-tasks' style='font-size:28px; margin-right:5px;'></span>
-                                    <span class='' style='font-size:20px;'>Tugas</span>
+                                    <div class='glyphicon glyphicon-tasks' style='font-size:28px; margin-right:5px;'></div>
+                                    <div class='' style='font-size:20px;'>Tugas</div>
                                   </div>
                                 </a>
                             </th>
