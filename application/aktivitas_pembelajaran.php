7e288af5975f9e0bbb1cf23c606115feb00c0982
Baihaqi34 <abay999mmm@gmail.com>
2024-10-25 09:22:51 +0700

    sas

--- application/aktivitas_pembelajaran.php
+++ application/aktivitas_pembelajaran.php
@@ -46,7 +46,33 @@
             </tr>
           </thead>
           <tbody>
-           
+            <?php
+            $tampil = mysql_query("SELECT a.*, e.nip, b.nama_guru, b.kode_pelajaran, c.nama_guru, d.nama_ruangan 
+                                   FROM rb_jadwal_pelajaran a 
+                                   JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
+                                   JOIN rb_guru c ON a.nip=c.nip 
+                                   JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
+                                   JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
+                                   WHERE a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$tahun_dipilih' 
+                                   ORDER BY a.hari DESC");
+
+            var_dump($tampil);
+            $no = 1;
+            while ($r = mysql_fetch_array($tampil)) {
+              echo "<tr>
+                      <td>$no</td>
+                      <td>$r[nip]</td>
+                      <td>$r[nama_guru]</td>
+                      <td>$r[hari]</td>
+                      <td>$r[tanggal]</td>
+                      <td>$r[jam]</td>
+                      <td>$r[kode_kelas]</td>
+                      <td>$r[kode_pelajaran]</td>
+                      <td><a class='btn btn-success btn-xs' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]'>Tujuan Pembelajaran</a></td>
+                    </tr>";
+              $no++;
+            }
+            ?>
           </tbody>
         </table> -->
 
@@ -67,38 +93,34 @@
   <tbody>
     <?php
     // Query untuk mengambil data dari beberapa tabel
-    $tampil = mysql_query("SELECT a.*, c.nip, c.nama_guru, a.hari, a.tanggal, a.jam, a.kode_kelas, a.kode_pelajaran 
+    $tampil = mysql_query("
+      SELECT a.*, e.nip, b.nama_guru, a.hari, a.tanggal, a.jam, a.kode_kelas, a.kode_pelajaran 
       FROM rb_jadwal_pelajaran a
+      JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
       JOIN rb_guru c ON a.nip=c.nip 
       JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
       JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
       WHERE a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$tahun_dipilih' 
       ORDER BY a.hari DESC");
 
-    if (!$tampil) {
-        die('Query Error: ' . mysql_error());
-    }
-
     // Inisialisasi nomor
     $no = 1;
-    var_dump($tampil);
-    exit();
     
     // Loop untuk menampilkan hasil query
-    // while ($r = mysql_fetch_array($tampil)) {
-    //   echo "<tr>
-    //           <td>$no</td>
-    //           <td>$r[nip]</td> 
-    //           <td>$r[nama_guru]</td>
-    //           <td>$r[hari]</td> 
-    //           <td>$r[tanggal]</td> 
-    //           <td>$r[jam]</td> 
-    //           <td>$r[kode_kelas]</td> 
-    //           <td>$r[kode_pelajaran]</td> 
-    //           <td><a class='btn btn-success btn-xs' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]'>Tujuan Pembelajaran</a></td>
-    //         </tr>";
-    //   $no++; // Increment nomor urut
-    // }
+    while ($r = mysql_fetch_array($tampil)) {
+      echo "<tr>
+              <td>$no</td>
+              <td>$r[nip]</td> 
+              <td>$r[nama_guru]</td>
+              <td>$r[hari]</td> 
+              <td>$r[tanggal]</td> 
+              <td>$r[jam]</td> 
+              <td>$r[kode_kelas]</td> 
+              <td>$r[kode_pelajaran]</td> 
+              <td><a class='btn btn-success btn-xs' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]'>Tujuan Pembelajaran</a></td>
+            </tr>";
+      $no++; // Increment nomor urut
+    }
     ?>
   </tbody>
 </table>
