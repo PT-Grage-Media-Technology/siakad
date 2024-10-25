e1491acecef296c8878ecedd32d4aaa45482ec8e
Baihaqi34 <abay999mmm@gmail.com>
2024-10-25 13:10:15 +0700

    sasas

--- application/aktivitas_pembelajaran.php
+++ application/aktivitas_pembelajaran.php
@@ -88,7 +88,7 @@
                                   JOIN rb_kelas b ON a.kode_kelas = b.kode_kelas 
                                   JOIN rb_mata_pelajaran c ON a.kode_pelajaran = c.kode_pelajaran 
                                   JOIN rb_guru d ON a.nip = d.nip
-                                  -- WHERE DAY(jl.waktu_input) = '$tanggal_dipilih' 
+                                  WHERE DAY(jl.waktu_input) = '$tanggal_dipilih' 
                                   ORDER BY jl.waktu_input DESC"); 
 
             // Hapus var_dump untuk menampilkan semua data
