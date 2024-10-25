ff96b5ea16e88d7fd3587321a230bbb186709d30
Baihaqi34 <abay999mmm@gmail.com>
2024-10-25 13:18:17 +0700

    dada

--- application/aktivitas_pembelajaran.php
+++ application/aktivitas_pembelajaran.php
@@ -101,7 +101,7 @@ ORDER BY jl.waktu_input DESC;
                       <td>$r[hari]</td>
                       <td>" . tgl_indo($r['tanggal']) . "</td>
                       <td><center>$r[kodejdwl]</td>
-                      <td>$r[namamatapelajaran]</td>
+                      <td>$r[materi]</td>
                       <td><center><a class='btn btn-success btn-xs' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]'>Tujuan Pembelajaran</a></center></td>
                     </tr>";
               $no++;
