4235fb23b86886ccfb6e06d4aa3e5e4ca28c4e19
Baihaqi34 <abay999mmm@gmail.com>
2024-10-25 09:19:30 +0700

    sass

--- application/aktivitas_pembelajaran.php
+++ application/aktivitas_pembelajaran.php
@@ -76,6 +76,10 @@
       WHERE a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$tahun_dipilih' 
       ORDER BY a.hari DESC");
 
+    if (!$tampil) {
+        die('Query Error: ' . mysql_error());
+    }
+
     // Inisialisasi nomor
     $no = 1;
     var_dump($tampil);
