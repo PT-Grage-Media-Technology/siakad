4ce77465278c07fa325a01b6ef0c12063338c81f
Baihaqi34 <abay999mmm@gmail.com>
2024-10-25 11:29:21 +0700

    saas

--- application/aktivitas_pembelajaran.php
+++ application/aktivitas_pembelajaran.php
@@ -93,10 +93,9 @@
             WHERE DATE(jl.waktu_input) = '$tanggal_hari_ini' 
             ORDER BY jl.waktu_input DESC");
 
-            // Mengganti var_dump dengan print_r untuk output yang lebih rapi
-            while ($row = mysql_fetch_array($tampil)) {
-                echo "<pre>" . print_r($row, true) . "</pre>";
-            }
+
+
+                  var_dump(mysql_fetch_array($tampil));
 
             // Hapus var_dump untuk menampilkan semua data
             $no = 1;
