8a3d7ab24474dd30cb9e4de14eabfe13381eceed
Baihaqi34 <abay999mmm@gmail.com>
2024-10-25 11:07:47 +0700

    sasas

--- application/aktivitas_pembelajaran.php
+++ application/aktivitas_pembelajaran.php
@@ -88,7 +88,6 @@
                                   JOIN rb_jadwal_pelajaran a ON jl.kodejdwl = a.kodejdwl
                                   JOIN rb_kelas b ON a.kode_kelas = b.kode_kelas 
                                   JOIN rb_mata_pelajaran c ON   a.kode_pelajaran = c.kode_pelajaran 
-                                  JOIN rb_guru d ON a.nip = d.nip 
                                   WHERE DAY(jl.waktu_input) = '$tanggal_dipilih' 
                                   -- Hapus filter berdasarkan guru
                                   ORDER BY jl.waktu_input DESC");
