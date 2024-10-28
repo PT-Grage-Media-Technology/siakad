0b5d4ada4321714a5d5a1aea1a45207f33466a5c
Baihaqi34 <abay999mmm@gmail.com>
2024-10-28 11:36:56 +0700

    update aja

--- application/raport/raport_halaman_siswa.php
+++ application/raport/raport_halaman_siswa.php
@@ -1,4 +1,14 @@
 <?php
+session_start(); // Memulai session
+
+// Simpan tahun yang dipilih ke dalam session
+if (isset($_GET['tahun'])) {
+    $_SESSION['tahun_terpilih'] = $_GET['tahun'];
+}
+
+// Set default tahun dari session jika ada
+$tahun_terpilih = isset($_SESSION['tahun_terpilih']) ? $_SESSION['tahun_terpilih'] : '';
+
 echo "<div class='col-xs-12 col-md-12'>  
 <div class='box'>
   <div class='box-header'>
@@ -10,11 +20,11 @@ echo "<div class='col-xs-12 col-md-12'>
         <option value=''>- Pilih Tahun Akademik -</option>";
 $tahun = mysql_query("SELECT * FROM rb_tahun_akademik");
 while ($k = mysql_fetch_array($tahun)) {
-  if ($_GET['tahun'] == $k['id_tahun_akademik']) {
-    echo "<option value='$k[id_tahun_akademik]' selected>$k[nama_tahun]</option>";
-  } else {
-    echo "<option value='$k[id_tahun_akademik]'>$k[nama_tahun]</option>";
-  }
+    if ($tahun_terpilih == $k['id_tahun_akademik']) {
+        echo "<option value='$k[id_tahun_akademik]' selected>$k[nama_tahun]</option>";
+    } else {
+        echo "<option value='$k[id_tahun_akademik]'>$k[nama_tahun]</option>";
+    }
 }
 
 echo "</select>
@@ -37,41 +47,41 @@ echo "</select>
           <th style='border:1px solid #ffffff; background-color:lightblue'><center>Nilai</center></th>
           <th style='border:1px solid #ffffff; background-color:lightblue'><center>Predikat</center></th>
         </tr>";
-if ($_GET['tahun'] == '') {
-  echo "<tr><td colspan=7><center style='padding:60px; color:red'>Silahkan Memilih Tahun akademik Terlebih dahulu...</center></td></tr>";
+if ($tahun_terpilih == '') {
+    echo "<tr><td colspan=7><center style='padding:60px; color:red'>Silahkan Memilih Tahun akademik Terlebih dahulu...</center></td></tr>";
 }
 $kelompok = mysql_query("SELECT * FROM rb_kelompok_mata_pelajaran");
 while ($k = mysql_fetch_array($kelompok)) {
-  echo "<tr>
+    echo "<tr>
                 <td style='border:1px solid #e3e3e3' colspan='2'><b>$k[nama_kelompok_mata_pelajaran]</b></td>
                 <td style='border:1px solid #e3e3e3' colspan='5'></td>
               </tr>";
-  $mapel = mysql_query("SELECT * FROM  rb_jadwal_pelajaran a 
+    $mapel = mysql_query("SELECT * FROM  rb_jadwal_pelajaran a 
                                     JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran 
                                       where a.kode_kelas='$_SESSION[kode_kelas]' 
-                                        AND a.id_tahun_akademik='$_GET[tahun]' 
+                                        AND a.id_tahun_akademik='$tahun_terpilih' 
                                           AND b.id_kelompok_mata_pelajaran='$k[id_kelompok_mata_pelajaran]'
                                             AND b.kode_kurikulum='$kurikulum[kode_kurikulum]'");
 
-  $no = 1;
-  while ($m = mysql_fetch_array($mapel)) {
-    $rapn = mysql_fetch_array(mysql_query("SELECT sum((nilai1+nilai2+nilai3+nilai4+nilai5)/5)/count(nisn) as raport FROM rb_nilai_pengetahuan where kodejdwl='$m[kodejdwl]' AND nisn='$iden[nisn]'"));
-    $cekpredikat = mysql_num_rows(mysql_query("SELECT * FROM rb_predikat where kode_kelas='$_SESSION[kelas]'"));
-    if ($cekpredikat >= 1) {
-      $grade3 = mysql_fetch_array(mysql_query("SELECT * FROM `rb_predikat` where (" . number_format($rapn['raport']) . " >=nilai_a) AND (" . number_format($rapn['raport']) . " <= nilai_b) AND kode_kelas='$_SESSION[kelas]'"));
-    } else {
-      $grade3 = mysql_fetch_array(mysql_query("SELECT * FROM `rb_predikat` where (" . number_format($rapn['raport']) . " >=nilai_a) AND (" . number_format($rapn['raport']) . " <= nilai_b) AND kode_kelas='0'"));
-    }
+    $no = 1;
+    while ($m = mysql_fetch_array($mapel)) {
+        $rapn = mysql_fetch_array(mysql_query("SELECT sum((nilai1+nilai2+nilai3+nilai4+nilai5)/5)/count(nisn) as raport FROM rb_nilai_pengetahuan where kodejdwl='$m[kodejdwl]' AND nisn='$iden[nisn]'"));
+        $cekpredikat = mysql_num_rows(mysql_query("SELECT * FROM rb_predikat where kode_kelas='$_SESSION[kelas]'"));
+        if ($cekpredikat >= 1) {
+            $grade3 = mysql_fetch_array(mysql_query("SELECT * FROM `rb_predikat` where (" . number_format($rapn['raport']) . " >=nilai_a) AND (" . number_format($rapn['raport']) . " <= nilai_b) AND kode_kelas='$_SESSION[kelas]'"));
+        } else {
+            $grade3 = mysql_fetch_array(mysql_query("SELECT * FROM `rb_predikat` where (" . number_format($rapn['raport']) . " >=nilai_a) AND (" . number_format($rapn['raport']) . " <= nilai_b) AND kode_kelas='0'"));
+        }
 
-    $rapnk = mysql_fetch_array(mysql_query("SELECT sum(GREATEST(nilai1,nilai2,nilai3,nilai4,nilai5,nilai6))/count(nisn) as raport FROM rb_nilai_keterampilan where kodejdwl='$m[kodejdwl]' AND nisn='$iden[nisn]'"));
-    $cekpredikat2 = mysql_num_rows(mysql_query("SELECT * FROM rb_predikat where kode_kelas='$_SESSION[kelas]'"));
-    if ($cekpredikat2 >= 1) {
-      $grade = mysql_fetch_array(mysql_query("SELECT * FROM `rb_predikat` where (" . number_format($rapnk['raport']) . " >=nilai_a) AND (" . number_format($rapnk['raport']) . " <= nilai_b) AND kode_kelas='$_SESSION[kelas]'"));
-    } else {
-      $grade = mysql_fetch_array(mysql_query("SELECT * FROM `rb_predikat` where (" . number_format($rapnk['raport']) . " >=nilai_a) AND (" . number_format($rapnk['raport']) . " <= nilai_b) AND kode_kelas='0'"));
-    }
+        $rapnk = mysql_fetch_array(mysql_query("SELECT sum(GREATEST(nilai1,nilai2,nilai3,nilai4,nilai5,nilai6))/count(nisn) as raport FROM rb_nilai_keterampilan where kodejdwl='$m[kodejdwl]' AND nisn='$iden[nisn]'"));
+        $cekpredikat2 = mysql_num_rows(mysql_query("SELECT * FROM rb_predikat where kode_kelas='$_SESSION[kelas]'"));
+        if ($cekpredikat2 >= 1) {
+            $grade = mysql_fetch_array(mysql_query("SELECT * FROM `rb_predikat` where (" . number_format($rapnk['raport']) . " >=nilai_a) AND (" . number_format($rapnk['raport']) . " <= nilai_b) AND kode_kelas='$_SESSION[kelas]'"));
+        } else {
+            $grade = mysql_fetch_array(mysql_query("SELECT * FROM `rb_predikat` where (" . number_format($rapnk['raport']) . " >=nilai_a) AND (" . number_format($rapnk['raport']) . " <= nilai_b) AND kode_kelas='0'"));
+        }
 
-    echo "<tr>
+        echo "<tr>
                   <td align=center>$no</td>
                   <td>$m[namamatapelajaran]</td>
                   <td align=center>77</td>
@@ -80,8 +90,8 @@ while ($k = mysql_fetch_array($kelompok)) {
                   <td align=center>" . number_format($rapnk['raport']) . "</td>
                   <td align=center>$grade[grade]</td>
               </tr>";
-    $no++;
-  }
+        $no++;
+    }
 }
 
 echo "</table></div></div></div></div>";
