f1537368499957d20f20ebc415cf0aba92baa532
Baihaqi34 <abay999mmm@gmail.com>
2024-10-28 11:27:08 +0700

    sasas

--- application/raport/raport_halaman_siswa.php
+++ application/raport/raport_halaman_siswa.php
@@ -10,7 +10,6 @@ echo "<div class='col-xs-12 col-md-12'>
         <option value=''>- Pilih Tahun Akademik -</option>";
 $tahun = mysql_query("SELECT * FROM rb_tahun_akademik ORDER BY id_tahun_akademik DESC LIMIT 1"); // Mengambil tahun terakhir
 $lastYear = mysql_fetch_array($tahun); // Menyimpan tahun terakhir
-$tahun = mysql_query("SELECT * FROM rb_tahun_akademik"); // Mengambil semua tahun untuk ditampilkan
 while ($k = mysql_fetch_array($tahun)) {
   if ($_GET[tahun] == $k[id_tahun_akademik] || $k[id_tahun_akademik] == $lastYear[id_tahun_akademik]) { // Memilih tahun terakhir secara default
     echo "<option value='$k[id_tahun_akademik]' selected>$k[nama_tahun]</option>";
