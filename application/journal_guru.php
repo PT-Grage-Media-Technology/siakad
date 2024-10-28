fce9c180f04321158d099ff3b38870093e9866f2
jipyy <jahdanpandita16@gmail.com>
2024-10-28 08:59:54 +0700

    qw

--- application/journal_guru.php
+++ application/journal_guru.php
@@ -130,7 +130,14 @@
                   <a class='btn btn-success btn-sm mb-2' title='Bahan dan Tugas' href='https://siakad.demogmt.online/index.php?view=raport&act=listsiswasikap&jdwl=$_GET[id]&kd=$d[kode_pelajaran]&id=$d[kode_kelas]&tahun=$_GET[tahun]'>
                     <div class='d-flex flex-column align-items-center'>
                       <div class='	glyphicon glyphicon-book' style='font-size:28px; margin-right:5px;'></div>
-                      <div class='' style='font-size:14px;'>Nilai Raport 123</div>
+                      <div class='' style='font-size:14px;'>Nilai Raport</div>
+                    </div>
+                  </a>
+
+                  <a class='btn btn-success btn-sm mb-2' title='Bahan dan Tugas' href='https://siakad.demogmt.online/index.php?view=raport&act=listsiswasikap&jdwl=$_GET[id]&kd=$d[kode_pelajaran]&id=$d[kode_kelas]&tahun=$_GET[tahun]'>
+                    <div class='d-flex flex-column align-items-center'>
+                      <div class='	glyphicon glyphicon-book' style='font-size:28px; margin-right:5px;'></div>
+                      <div class='' style='font-size:14px;'>Forum Diskusi</div>
                     </div>
                   </a>
                   
