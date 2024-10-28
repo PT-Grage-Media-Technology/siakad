5742f0d77c2b998bbbfb0f7185c15b4168e9a8d1
ArvinoDel <andikasupriyadi27@gmail.com>
2024-10-28 10:31:08 +0700

    test

--- logout.php
+++ logout.php
@@ -1,22 +1,26 @@
 <?php
-  session_start();
-  
-  echo "<script>
+session_start();
+
+
+echo "<script>
+        function confirmLogout() {
           if (confirm('Apakah anda yakin akan logout?')) {
-            " . 
-              "window.location.href = 'logout.php?action=logout';
-            " . "
+            window.location.href = 'logout.php?action=logout';
           } else {
             window.history.back();
           }
-        </script>";
-        
-  if (isset($_GET['action']) && $_GET['action'] === 'logout') {
+        }
+      </script>";
+
+echo "<script>confirmLogout();</script>";
+
+// Logika PHP untuk menangani logout saat action dipanggil
+if (isset($_GET['action']) && $_GET['action'] === 'logout') {
     session_destroy();
     echo "<script>
-            window.alert('Sukses Keluar dari sistem.');
+            alert('Sukses Keluar dari sistem.');
             window.location = 'index.php';
           </script>";
     die();
-  }
+}
 ?>
