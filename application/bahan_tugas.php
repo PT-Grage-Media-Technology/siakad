e42939db61741f9d4818d044e98a95b3223b12e2
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-10-29 18:18:04 +0700

    121

--- application/bahan_tugas.php
+++ application/bahan_tugas.php
@@ -626,7 +626,8 @@ if ($_GET[act] == '') {
 
   
     if(isset($_POST['nilai_jawaban'])){
-      mysql_query("UPDATE rb_elearning_jawaban SET nilai='$_POST[nilai]' where id_elearning_jawaban='$_POST[id_elearning_jawaban]'");
+      $coba = mysql_query("UPDATE rb_elearning_jawaban SET nilai='$_POST[nilai]' where id_elearning_jawaban='$_POST[id_elearning_jawaban]'");
+      var_dump($coba);
       echo "<script>document.location='index.php?view=bahantugas&act=listbahantugas&jdwl=" . $_GET[jdwl] . "&id=" . $_GET[id] . "&kd=" . $_GET[kd] . "';</script>";
     }
 
