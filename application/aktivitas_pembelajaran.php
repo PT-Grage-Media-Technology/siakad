1cf59016a2e305a1e55d62c0afa6619c472c3087
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-10-29 09:21:48 +0700

    432

--- application/aktivitas_pembelajaran.php
+++ application/aktivitas_pembelajaran.php
@@ -120,7 +120,7 @@
             ORDER BY jl.waktu_input DESC;
             ");
 
-            // var_dump($tampil);
+            var_dump($tampil);
             $no = 1;
             while ($r = mysql_fetch_array($tampil)) {
               echo "<tr>
@@ -129,10 +129,15 @@
                       <td>$r[nama_guru]</td>
                       <td>$r[hari]</td>
                       <td>" . tgl_indo($r['tanggal']) . "</td>
-                      <td><center>$r[jam_ke]</td>
+                      <td>$r[jam_ke]</td>
                       <td>$r[kode_kelas]</td>
                       <td>$r[namamatapelajaran]</td>
-                      <td><center><a class='btn btn-success btn-xs' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]'>Detail Tujuan Pembelajaran Guru</a></center></td>
+                      <td>
+                          <center>
+                            <a class='btn btn-warning btn-xs' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]'>Detail Tujuan Pembelajaran Guru</a>
+                            <a class='btn btn-warning btn-xs' href='index.php?view=absensiswa&act=tampilabsen&id=$r[kode_kelas]&kd=MK15&idjr=33&tgl=$r[tanggal]&jam=$r[jam_ke]'>Absensi</a>
+                          </center>
+                      </td>
                     </tr>";
               $no++;
             }
