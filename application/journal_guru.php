701e0de3243031ddbc4d10cd55e1fb49e1d1dcf6
jipyy <jahdanpandita16@gmail.com>
2024-10-29 10:22:08 +0700

    Z

--- application/journal_guru.php
+++ application/journal_guru.php
@@ -263,20 +263,17 @@
                           </select>
                       </td>
                     </tr>
-                    <tr>
-            <th scope="row">Pilih Guru</th>
-            <td>
-              <select class="form-control" name="g">
-                <option value="">-- Pilih Nama Guru --</option>
-                <?php
-                $guru = mysql_query("SELECT * FROM rb_guru");
-                while ($g = mysql_fetch_array($guru)) {
-                  echo "<option value='{$g['nip']}'>{$g['nama_guru']}</option>";
-                }
-                ?>
-              </select>
-            </td>
-          </tr>
+                     <tr>
+                         <th scope='row'>Pilih Guru</th>   
+                         <td>
+                             <select class='selectpicker form-control' name='g' data-live-search='true' data-show-subtext='true'>";
+                              $guru = mysql_query("SELECT * FROM rb_guru");
+                              while ($g = mysql_fetch_array($guru)) {
+                                echo "<option value='$g[nip]'>$g[nama_guru]</option>";
+                              }
+                              echo "</select>
+                          </td>
+                    </tr>
 
 
                     <tr><th scope='row'>Tanggal</th>  <td><input type='text' style='border-radius:0px; padding-left:12px' class='datepicker form-control' value='" . date('d-m-Y') . "' name='d' data-date-format='dd-mm-yyyy'></td></tr>
