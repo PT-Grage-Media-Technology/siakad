2976e5fc045c8cb6f130ec56d13b3c8d047f55fe
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-10-30 15:46:07 +0700

    eqeq

--- application/aktivitas_pembelajaran.php
+++ application/aktivitas_pembelajaran.php
@@ -116,6 +116,7 @@
               FROM rb_journal_list jl 
               JOIN rb_jadwal_pelajaran a ON jl.kodejdwl = a.kodejdwl
               JOIN rb_kelas b ON a.kode_kelas = b.kode_kelas 
+              JOIN rb_absensi_guru v ON jl.users = v.nip
               JOIN rb_mata_pelajaran c ON a.kode_pelajaran = c.kode_pelajaran 
               JOIN rb_guru d ON jl.users = d.nip
               WHERE DAY(jl.tanggal) = '$tanggal_dipilih' 
@@ -137,7 +138,7 @@
                         <td>$r[jam_ke]</td>
                         <td>$r[kode_kelas]</td>
                         <td>$r[namamatapelajaran]</td>
-                        <td>$r[namamatapelajaran]</td>
+                        <td>$r[kode_kehadiran]</td>
                         <td>
                             <center>
                               <a class='btn btn-warning btn-xs' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]'>Detail Tujuan Pembelajaran Guru</a>
