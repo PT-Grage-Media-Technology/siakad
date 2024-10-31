aa9ba1c734958e87e533f28a887b4730ba3380c3
Baihaqi34 <abay999mmm@gmail.com>
2024-10-31 15:07:13 +0700

    dada

--- application/aktivitas_pembelajaran.php
+++ application/aktivitas_pembelajaran.php
@@ -189,7 +189,6 @@
                
                    $insertResult = mysql_query("INSERT INTO rb_pemberitahuan_guru VALUES (null, '$nip', '$pesan', 0, '$r[kode_kelas]', '$r[kode_pelajaran]', '$r[kodejdwl]', '$r[tanggal]', '$r[jam_ke]', '$tanggal')");
                    var_dump($insertResult);
-                   exit;
                 }
                 // if (isset($_POST['peringatkan']) && $_POST['users'] == $r['users']) {
                 //    // Mendapatkan NIP pengguna
