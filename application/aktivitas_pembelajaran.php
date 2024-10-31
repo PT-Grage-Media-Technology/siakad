ccbdadfe78d6d94e969c6aa51c08a4b307017a97
Baihaqi34 <abay999mmm@gmail.com>
2024-10-31 11:00:49 +0700

    12312

--- application/aktivitas_pembelajaran.php
+++ application/aktivitas_pembelajaran.php
@@ -128,30 +128,26 @@
               //   ");
 
               $tampil = mysql_query("
-                          SELECT 
-                              jl.*, 
-                              a.kode_kelas, 
-                              b.nama_kelas, 
-                              c.namamatapelajaran, 
-                              c.kode_pelajaran, 
-                              d.nama_guru,
-                              (SELECT kode_kehadiran 
-                              FROM rb_absensi_guru ag 
-                              WHERE ag.nip = jl.users 
-                              AND ag.tanggal = jl.tanggal 
-                              LIMIT 1) AS kode_kehadiran,
-                              pg.pemberitahuan
-                          FROM rb_journal_list jl 
-                          JOIN rb_jadwal_pelajaran a ON jl.kodejdwl = a.kodejdwl
-                          JOIN rb_kelas b ON a.kode_kelas = b.kode_kelas 
-                          JOIN rb_mata_pelajaran c ON a.kode_pelajaran = c.kode_pelajaran 
-                          JOIN rb_guru d ON jl.users = d.nip
-                          LEFT JOIN rb_absensi_guru ag ON ag.nip = jl.users AND ag.tanggal = jl.tanggal
-                          LEFT JOIN rb_pemberitahuan_guru pg ON pg.nip = ag.nip
-                          WHERE DAY(jl.tanggal) = '$tanggal_dipilih' 
-                          AND MONTH(jl.tanggal) = '$bulan_dipilih'
-                          ORDER BY jl.waktu_input DESC;
-                      ");
+    SELECT 
+        jl.*, 
+        a.kode_kelas, 
+        b.nama_kelas, 
+        c.namamatapelajaran, 
+        c.kode_pelajaran, 
+        d.nama_guru,
+        pg.pemberitahuan
+    FROM rb_journal_list jl 
+    JOIN rb_jadwal_pelajaran a ON jl.kodejdwl = a.kodejdwl
+    JOIN rb_kelas b ON a.kode_kelas = b.kode_kelas 
+    JOIN rb_mata_pelajaran c ON a.kode_pelajaran = c.kode_pelajaran 
+    JOIN rb_guru d ON jl.users = d.nip
+    LEFT JOIN rb_absensi_guru ag ON ag.nip = jl.users AND ag.tanggal = jl.tanggal
+    LEFT JOIN rb_pemberitahuan_guru pg ON pg.nip = ag.nip
+    WHERE DAY(jl.tanggal) = '$tanggal_dipilih' 
+    AND MONTH(jl.tanggal) = '$bulan_dipilih'
+    ORDER BY jl.waktu_input DESC;
+");
+
 
 
 
