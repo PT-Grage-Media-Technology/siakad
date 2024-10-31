123360c57b43ee5c8eda58ddb268c1a90bf9a9e6
Baihaqi34 <abay999mmm@gmail.com>
2024-10-31 11:41:40 +0700

    asfdasdfsafd

--- application/aktivitas_pembelajaran.php
+++ application/aktivitas_pembelajaran.php
@@ -144,12 +144,12 @@
                         <td>$r[namamatapelajaran]</td>";
 
                         $pemberitahuan = mysql_query("SELECT * FROM rb_pemberitahuan_guru WHERE nip_guru='$r[users]' AND is_read=0 AND kode_kelas='$r[kode_kelas]' AND kode_mapel='$r[kode_pelajaran]' AND id_tujuan_pembelajaran='$r[id_journal]' AND tanggal_absen='$r[tanggal]' AND jam_ke='$r[jam_ke]'");
-                        $coba = "SELECT * FROM rb_pemberitahuan_guru WHERE nip_guru='$r[users]' AND is_read=0 AND kode_kelas='$r[kode_kelas]' AND kode_mapel='$r[kode_pelajaran]' AND id_tujuan_pembelajaran='$r[id_journal]' AND tanggal_absen='$r[tanggal]' AND jam_ke='$r[jam_ke]'";
+                        $pe = mysql_fetch_array($pemberitahuan);
                         echo"
                         <td>"; 
                             if (isset($r['kode_kehadiran'])) {
                                 // echo "$r[kode_kehadiran]";
-                                echo "$pemberitahuan, $coba";
+                                echo "$pe, $coba";
                             } else {
                                 echo "<form action='index.php?view=aktivitaspembelajaran' method='POST'>
                                         <input type='hidden' name='users' value='' . $r[users] . ''>
