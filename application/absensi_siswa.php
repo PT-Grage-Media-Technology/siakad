5f340d8669f525c03dfadf01fc68645592c1aaf2
Baihaqi34 <abay999mmm@gmail.com>
2024-11-01 10:25:17 +0700

    waw

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -243,7 +243,7 @@
   $jumlah_data = mysql_num_rows($tugas);
   echo "Jumlah data: " . $jumlah_data;
 
-  $jawaban_tugas = mysql_fetch_array(mysql_query("SELECT * FROM rb_elearning_jawab WHERE id_elearning='$data_tugas[id_elearning]'"));
+  $jawaban_tugas = mysql_fetch_array(mysql_query("SELECT nilai FROM rb_elearning_jawab WHERE id_elearning='$data_tugas[id_elearning]'"));
   var_dump($jawaban_tugas,$data_tugas['id_elearning']);
   $tampil = mysql_query("SELECT * FROM rb_siswa a JOIN rb_jenis_kelamin b ON a.id_jenis_kelamin=b.id_jenis_kelamin where a.kode_kelas='$_GET[id]' ORDER BY a.id_siswa");
   while ($r = mysql_fetch_array($tampil)) {
