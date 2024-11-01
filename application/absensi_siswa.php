80480fd15a18b382c26fe124656583b156a840fe
Baihaqi34 <abay999mmm@gmail.com>
2024-11-01 09:51:59 +0700

    sasa

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -282,13 +282,13 @@
                                 echo "<option value='C' " . ($a['nilai'] == 'C' ? 'selected' : '') . ">C</option>";
                                 echo "<option value='D' " . ($a['nilai'] == 'D' ? 'selected' : '') . ">D</option>";
                                 echo "</select></td>";
-
+                              }
                                   if (strtotime(date('Y-m-d')) > strtotime($_GET['tgl'])) {
                                     echo "<td><select disabled style='width:100px;' name='a[$no]' class='form-control'>";
                                   } else {
                                     echo "<td><select style='width:100px;' name='a[$no]' class='form-control'>";
                                   }
-                                }
+                              
 
 
     $kehadiran = mysql_query("SELECT * FROM rb_kehadiran");
