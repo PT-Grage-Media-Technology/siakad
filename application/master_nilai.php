8c7ce21a4472f3ec52b20b3c6533a15cf4699303
Baihaqi34 <abay999mmm@gmail.com>
2024-11-06 14:45:00 +0700

    sddsd

--- application/master_nilai.php
+++ application/master_nilai.php
@@ -14,7 +14,7 @@
                   while($kriteriaNilai = mysql_fetch_array($tampil)){
 
                     //   var_dump($kriteriaNilai) ; // Mengambil data dari array ke-0
-                      echo $kriteriaNilai[1]['kode_nilai'];
+                      echo $kriteriaNilai['kode_nilai'][0];
                   }
 
        
