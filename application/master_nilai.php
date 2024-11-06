abb19447ce8719fbd80a5e1aad747a8b236439ac
Baihaqi34 <abay999mmm@gmail.com>
2024-11-06 12:56:48 +0700

    SASAS

--- application/master_nilai.php
+++ application/master_nilai.php
@@ -16,7 +16,7 @@
 
        
         echo"<p>
-          <input type='text' placeholder='Nilai Huruf' style='width: 40px;' value='$kriteriaNilai[kode_nilai]'> = <input type='text' style='width: 50px;'> - <input type='text' style='width: 50px;'>
+          <input type='text' placeholder='Nilai Huruf' style='width: 40px;' value='{$kriteriaNilai1[0]}'> = <input type='text' style='width: 50px;'> - <input type='text' style='width: 50px;'>
         </p>
         <p>
           <input type='text' placeholder='Nilai Huruf' style='width: 40px;' value='B'> = <input type='text' style='width: 50px;'> - <input type='text' style='width: 50px;'>
