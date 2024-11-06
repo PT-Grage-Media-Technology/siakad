0ddaf5b786d974ddc71cec832def5a46d929f6b0
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-11-06 10:44:46 +0700

    das

--- application/master_nilai.php
+++ application/master_nilai.php
@@ -1,16 +1,21 @@
+<div class="col-xs-12">
+  <div class="box">
+    <div class="box-header">
+      <h3 class="box-title">Data Nilai </h3>
+      <?php if ($_SESSION['level'] != 'kepala') { ?>
+        <a class='pull-right btn btn-primary btn-sm' href='index.php?view=ruangan&act=tambah'>Tambahkan Data</a>
+      <?php } ?>
+    </div><!-- /.box-header -->
+    <div class="box-body">
+      <div class="table-responsive" style="max-height: 500px; overflow-y: auto;">
+        <p><input type="text"> = <input type="text"> - <input type="text"></p>
+        <p>
+          <input type="text" class="form-control w-25 d-inline" placeholder="Nilai Huruf">
+          = <input type="text" class="form-control d-inline" style="width: 150px;">
+          - <input type="text" class="form-control d-inline" style="width: 150px;">
+        </p>
 
-  <div class="col-xs-12">
-    <div class="box">
-      <div class="box-header">
-        <h3 class="box-title">Data Nilai </h3>
-        <?php if ($_SESSION['level'] != 'kepala') { ?>
-          <a class='pull-right btn btn-primary btn-sm' href='index.php?view=ruangan&act=tambah'>Tambahkan Data</a>
-        <?php } ?>
-      </div><!-- /.box-header -->
-      <div class="box-body">
-        <div class="table-responsive" style="max-height: 500px; overflow-y: auto;">
-          <p><input type="text"> = <input type="text"> - <input type="text"></p>
-        </div><!-- /.table-responsive -->
-      </div><!-- /.box-body -->
-    </div><!-- /.box -->
-  </div>
+      </div><!-- /.table-responsive -->
+    </div><!-- /.box-body -->
+  </div><!-- /.box -->
+</div>
