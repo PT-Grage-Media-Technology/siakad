478fff4ed0b550f31bb47fe79f3c85da669e9e60
Baihaqi34 <abay999mmm@gmail.com>
2024-11-06 15:14:06 +0700

    dsdsd

--- application/master_nilai.php
+++ application/master_nilai.php
@@ -19,7 +19,7 @@
 
        
         echo"<p>
-            <input type='text' placeholder='Nilai Huruf' style='width: 40px;' value='{$kriteriaNilai['kode_nilai']}'> = <input type='text' style='width: 50px;' value='{$kriteriaNilai['nilai_angka']}'> - <input type='text' style='width: 50px;'>";
+            <input type='text' placeholder='Nilai Huruf' style='width: 40px;' value='{$kriteriaNilai['kode_nilai']}'> = <input type='text' style='width: 50px;' value='{$kriteriaNilai['nilai_angka'] - 1}'> - <input type='text' style='width: 50px;' value='{$kriteriaNilai['nilai_angka'] - 1}'>";
     }
         ?>
             
