990d078b403d66bde033c041f672ef4a4cdfefc7
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-11-06 11:31:07 +0700

    sasas

--- application/master_nilai.php
+++ application/master_nilai.php
@@ -10,28 +10,25 @@
       <div class="table-responsive" style="max-height: 500px; overflow-y: auto;">
         <!-- <p><input type="text"> = <input type="text"> - <input type="text"></p> -->
         <?php 
-                   $query = "SELECT * FROM rb_kriteria_nilai";
-                   $tampil = mysql_query($query);
-                   
-                   if ($tampil) {
-                       while ($row = mysql_fetch_assoc($tampil)) {
-                           // Ambil nilai dari setiap kolom, dan gunakan htmlspecialchars untuk keamanan
-                           $nilaiHuruf = htmlspecialchars($row['nilai_huruf']);  
-                           $nilaiMin = htmlspecialchars($row['nilai_min']);      
-                           $nilaiMax = htmlspecialchars($row['nilai_max']);      
-                   
-                           // Tampilkan data dalam struktur HTML
-                           echo "<p>";
-                           echo "<input type='text' placeholder='Nilai Huruf' style='width: 40px;' value='$nilaiHuruf'> = ";
-                           echo "<input type='text' style='width: 50px;' value='$nilaiMin'> - ";
-                           echo "<input type='text' style='width: 50px;' value='fsdfdd'>";
-                           echo "</p>";
-                       }
-                   } else {
-                       echo "Tidak ada data yang ditemukan.";
-                   }
-                    
+                  $tampil = mysql_query("SELECT * FROM rb_kriteria_nilai");
 
+                  if ($tampil) {
+                      while ($row = mysql_fetch_assoc($tampil)) {
+                          // Ambil nilai dari setiap kolom dan gunakan htmlspecialchars untuk keamanan
+                          $nilaiHuruf = htmlspecialchars($row['nilai_huruf']);  
+                          $nilaiMin = htmlspecialchars($row['nilai_min']);      
+                          $nilaiMax = htmlspecialchars($row['nilai_max']);      
+                  
+                          // Tampilkan data dalam format input HTML
+                          echo "<p>";
+                          echo "<input type='text' placeholder='Nilai Huruf' style='width: 40px;' value='$nilaiHuruf'> = ";
+                          echo "<input type='text' style='width: 50px;' value='$nilaiMin'> - ";
+                          echo "<input type='text' style='width: 50px;' value='$nilaiMax'>";
+                          echo "</p>";
+                      }
+                  } else {
+                      echo "Tidak ada data yang ditemukan.";
+                  }
         ?>
         <p>
           <input type="text" placeholder="Nilai Huruf" style="width: 40px;" value="A"> = <input type="text" style="width: 50px;"> - <input type="text" style="width: 50px;">
