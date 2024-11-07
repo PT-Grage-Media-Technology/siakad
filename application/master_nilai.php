d4e15972f653a166e11a85d2c6b0000a60cced5c
Baihaqi34 <abay999mmm@gmail.com>
2024-11-07 08:31:51 +0700

    fdffd

--- application/master_nilai.php
+++ application/master_nilai.php
@@ -10,6 +10,12 @@
       <div class="table-responsive" style="max-height: 500px; overflow-y: auto;">
         <!-- <p><input type="text"> = <input type="text"> - <input type="text"></p> -->
         <?php 
+         // Query untuk mengambil data dengan nilai 100 secara spesifik
+    $nilaiA = mysql_query("SELECT * FROM rb_kriteria_nilai WHERE id = 2");
+    $A = mysql_fetch_array($nilaiA);
+
+    echo"<p>
+            <input type='text' placeholder='Nilai Huruf' style='width: 40px;' value='{$A['kode_nilai']}'> = <input type='text' style='width: 50px;' value='" . ($A['nilai_angka']) . "'> - <input type='text' style='width: 50px;' value='" . ($kriteriaNilai['nilai_angka'] - 1) . "'>";
                   $tampil = mysql_query("SELECT * FROM rb_kriteria_nilai");
                   while($kriteriaNilai = mysql_fetch_array($tampil)){
 
