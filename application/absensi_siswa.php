e2a9bed9b13d654b47a0bd40f662702066ceef94
Baihaqi34 <abay999mmm@gmail.com>
2024-11-08 09:51:59 +0700

    sasas

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -289,12 +289,14 @@
 
     // Ambil nilai sesuai nomor siswa
     $nilaiSiswa = isset($a['nilai']) ? $a['nilai'] : 0; // Pastikan nilai ada
-    var_dump($nilaiSiswa);
-    var_dump($nilaiSiswa >= $predikatData['nilai_bawah'] && $nilaiSiswa <= $predikatData['nilai_atas']);
+    var_dump($nilaiSiswa); // Memeriksa nilai siswa
+    var_dump($predikatData); // Memeriksa data predikat
+
+    // Cek apakah nilai siswa berada dalam rentang predikat
     if ($nilaiSiswa >= $predikatData['nilai_bawah'] && $nilaiSiswa <= $predikatData['nilai_atas']) {
-      echo "<td>tes aja $predikatData[kode_nilai]</td>"; 
+        echo "<td>tes aja $predikatData[kode_nilai]</td>"; 
     } else {
-      echo "<td>tes ga $predikatData[kode_nilai]</td>"; 
+        echo "<td>tes ga $predikatData[kode_nilai]</td>"; 
     }
 
     echo "</td><input type='hidden' value='$r[nisn]' name='nisn[$no]'>";
