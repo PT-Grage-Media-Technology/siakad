59d8e055dcd9c62e308de3afe1bdf00c16406406
Baihaqi34 <abay999mmm@gmail.com>
2024-11-08 08:56:38 +0700

    xzxzx

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -271,17 +271,17 @@
                                 if(mysql_num_rows($tugas) > 0 ){
                                 if($r['nilai']){
                                   echo"<input type='number' value='$nilai[nilai]' name='nilai[$no]' style='width:50px;'>";
-                                }else{
+                                } else {
                                   echo"<input type='number' value='0' name='nilai[$no]' style='width:50px;'>";
                                 }
-                              }else{
-                                // if($r['nilai']){
-                                //   echo"<input type='number' value='$a[nilai]' name='nilai[$no]' style='width:50px;'>";
-                                // }else{
-                                //   echo"<input type='number' value='0bukan tugas' name='nilai[$no]' style='width:50px;'>";
-                                // }
-                                echo"<td>bukan tugas ini mah</td>";
+                              } else {
+                                if($r['nilai']){
+                                  echo"<input type='number' value='$a[nilai]' name='nilai[$no]' style='width:50px;'>";
+                                } else {
+                                  echo"<input type='number' value='0' name='nilai[$no]' style='width:50px;'>";
+                                }
                               }
+                              
                               $predikat = mysql_query("SELECT * FROM rb_kriteria_nilai");
                               if($predikat['nilai_bawah'] >= $r['nilai'] &&  $predikat['nilai_atas'] <= $r['nilai'] ){
                                 echo"<td>$predikat[kode_nilai]</td>";
