b39dd0efafc3e54ecfd8738822e323d95854df07
kulkad <rdarmawan281@gmail.com>
2024-11-13 15:11:51 +0700

    icon

--- menu-guru.php
+++ menu-guru.php
@@ -19,7 +19,7 @@
       echo "<li><a href='index.php'><i class='fa fa-dashboard'></i> <span>Dashboard</span></a></li>";
     }
     ?>
-    <li><a href=""><i class="fa fa-user-check"></i><span>Rekap Absensi</span></a></li>
+    <li><a href=""><i class="fa fa-calendar-check-o" aria-hidden="true"></i><span>Rekap Absensi</span></a></li>
 
     <li><a href="index.php?view=jadwalguru"><i class="fa fa-clipboard"></i><span>Aktivitas Mengajar</span></a></li>
     <li><a href="index.php?view=aktivitaspembelajaran"><i class="glyphicon glyphicon-align-justify"></i> <span>Aktivitas Pembelajaran</span></a></li>
