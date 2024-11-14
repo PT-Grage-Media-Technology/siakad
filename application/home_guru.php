fdf864f8f8ae0dd0aae665c9ccc0c6a891e893f5
Baihaqi34 <abay999mmm@gmail.com>
2024-11-14 10:16:47 +0700

    sddsdsd

--- application/home_guru.php
+++ application/home_guru.php
@@ -2,7 +2,35 @@
   <div class="box">
     <div class="box-header">
       <h3 class="box-title">
-       
+        <?php
+        $conn = mysqli_connect($server, $username, $password, $database);
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        // Ambil tahun akademik terbaru (id_tahun_akademik paling besar)
+        $latest_year = mysql_fetch_array(mysql_query("SELECT id_tahun_akademik, nama_tahun FROM rb_tahun_akademik ORDER BY id_tahun_akademik DESC LIMIT 1"));
+
+        // Jika tidak ada tahun akademik dipilih, set default ke tahun terbaru
+        $tahun_dipilih = isset($_GET['tahun']) ? $_GET['tahun'] : $latest_year['id_tahun_akademik'];
+        $nama_tahun = isset($_GET['tahun']) ?
+          mysql_fetch_array(mysql_query("SELECT nama_tahun FROM rb_tahun_akademik WHERE id_tahun_akademik = '$tahun_dipilih'"))['nama_tahun'] :
+          $latest_year['nama_tahun'];
+
+        echo "Jadwal Mengajar Anda - $nama_tahun";
+        ?>
+      </h3>
+      <form style='margin-right:5px; margin-top:0px' class='pull-right' action='' method='GET'>
+        <!-- Tambahkan hidden input untuk menyimpan parameter view -->
+        <input type="hidden" name="view" value="jadwalguru">
+        <select name='tahun' style='padding:4px' onchange='this.form.submit()'>
+          <option value=''>- Pilih Tahun Akademik -</option>
+          <?php
+          $tahun = mysql_query("SELECT * FROM rb_tahun_akademik ORDER BY id_tahun_akademik DESC");
+          while ($k = mysql_fetch_array($tahun)) {
+            $selected = ($tahun_dipilih == $k['id_tahun_akademik']) ? 'selected' : '';
+            echo "<option value='$k[id_tahun_akademik]' $selected>$k[nama_tahun]</option>";
+          }
+          ?>
         </select>
       </form>
     </div>
@@ -26,7 +54,39 @@
             </tr>
           </thead>
           <tbody>
-           
+            <?php
+            $query = "SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan 
+           FROM rb_jadwal_pelajaran a 
+           JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
+           JOIN rb_guru c ON a.nip=c.nip 
+           JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
+           JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
+           WHERE a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$tahun_dipilih' 
+           ORDER BY a.hari DESC";
+            echo $query; // Debug query
+            $tampil = mysqli_query($conn, $query);
+
+            if (!$tampil) {
+              die("Query Error: " . mysql_error());
+            }
+            $no = 1;
+            while ($r = mysql_fetch_array($tampil)) {
+              echo "<tr>
+                      <td>$no</td>
+                      <td>$r[kode_pelajaran]</td>
+                      <td>$r[namamatapelajaran]</td>
+                      <td>$r[nama_kelas]</td>
+                      <td>$r[nama_guru]</td>
+                      <td>$r[hari]</td>
+                      <td>$r[jam_mulai]</td>
+                      <td>$r[jam_selesai]</td>
+                      <td>$r[nama_ruangan]</td>
+                      <td>$r[id_tahun_akademik]</td>
+                      <td><a class='btn btn-success btn-xs' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]&tahun=$r[id_tahun_akademik]'>Agenda Mengajar</a></td>
+                    </tr>";
+              $no++;
+            }
+            ?>
           </tbody>
         </table>
       </div><!-- /.table-responsive -->
