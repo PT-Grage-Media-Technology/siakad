0bdf6b735508a960029d506a00f2dc3056f674dc
Baihaqi34 <abay999mmm@gmail.com>
2024-11-14 08:46:06 +0700

    sasas

--- menu-guru.php
+++ menu-guru.php
@@ -21,9 +21,9 @@
     ?>
     <li><a href=""><i class="glyphicon glyphicon-list-alt"></i><span>Rekap Absensi</span></a></li>
 
-    <!-- <li><a href="https://siakad.demogmt.online/index.php?view=aktivitaspembelajaran"><i class="glyphicon glyphicon-align-justify"></i> <span>Aktivitas Pembelajaran</span></a></li> -->
+    <li><a href="https://siakad.demogmt.online/index.php?view=aktivitaspembelajaran"><i class="glyphicon glyphicon-align-justify"></i> <span>Aktivitas Pembelajaran</span></a></li>
 
-    <li class="treeview">
+    <!-- <li class="treeview">
       <a href="#" class="d-flex justify-content-between align-items-center" onclick="this.classList.toggle('fa-sort-asc')">
         <span>Modul Mengajar</span>
         <i class="fa fa-caret-down ml-10"></i>
@@ -61,7 +61,7 @@
           </a>
         </li>
       </ul>
-    </li>
+    </li> -->
 
 
 
