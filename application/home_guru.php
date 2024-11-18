58cfbfc1a8ef040b723c8abaa432b8110837ec6b
Ahmad Baihaqi <121011402+Baihaqi34@users.noreply.github.com>
2024-11-18 19:21:44 +0700

    sasas

--- application/home_guru.php
+++ application/home_guru.php
@@ -3,7 +3,7 @@ $tes = mysql_query("SELECT * FROM rb_mata_pelajaran WHERE nip='$_SESSION[id]'");
 $no = 1;
 while ($r = mysql_fetch_array($tes)){
   echo"<p>$no - $r[namamatapelajaran]</p?";
-  var_dump($r) ;
+  // var_dump($r) ;
 }
 
 // echo $_SESSION['id'];
