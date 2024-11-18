feadb8a58a4a57524aec7c6b943e38f33c7d34e6
Ahmad Baihaqi <121011402+Baihaqi34@users.noreply.github.com>
2024-11-18 20:08:56 +0700

    bnb

--- application/home_guru.php
+++ application/home_guru.php
@@ -1,7 +1,15 @@
 <?php 
-$tes = mysql_query("SELECT * FROM rb_jadwal_pelajaran WHERE nip='$_SESSION[id]'");
+// $tes = mysql_query("SELECT * FROM rb_jadwal_pelajaran WHERE nip='$_SESSION[id]'");
+$tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan 
+                                   FROM rb_jadwal_pelajaran a 
+                                   JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
+                                   JOIN rb_guru c ON a.nip=c.nip 
+                                   JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
+                                   JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
+                                   WHERE a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$tahun_dipilih' 
+                                   ORDER BY a.hari DESC");
 $no = 1;
-while ($r = mysql_fetch_array($tes)){
+while ($r = mysql_fetch_array($tampil)){
   echo"<p>$no - $r[namamatapelajaran] == $r[nip]</p>";
   // var_dump($r) ;
 }
