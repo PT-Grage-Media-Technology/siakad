c01bbe41decdced6545fa168bff3512d5e91b0b1
Ahmad Baihaqi <121011402+Baihaqi34@users.noreply.github.com>
2024-11-18 19:49:09 +0700

    sasas

--- application/home_guru.php
+++ application/home_guru.php
@@ -1,5 +1,5 @@
 <?php 
-$tes = mysql_query("SELECT * FROM rb_mata_pelajaran WHERE namamatapelajaran='Ekonomi'");
+$tes = mysql_query("SELECT * FROM rb_mata_pelajaran WHERE nip='$_SESSION[id]'");
 $no = 1;
 while ($r = mysql_fetch_array($tes)){
   echo"<p>$no - $r[namamatapelajaran] == $r[nip]</p>";
