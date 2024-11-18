8ebd5bad9dd0f08a617fd2ddc9e9d138ade4c3f2
Ahmad Baihaqi <121011402+Baihaqi34@users.noreply.github.com>
2024-11-18 19:01:52 +0700

    sasas

--- application/home_guru.php
+++ application/home_guru.php
@@ -51,14 +51,14 @@
           </thead>
           <tbody>
             <?php
-            $tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan 
-                                   FROM rb_jadwal_pelajaran a 
-                                   JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
-                                   JOIN rb_guru c ON a.nip=c.nip 
-                                   JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
-                                   JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
-                                   WHERE a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$tahun_dipilih' 
-                                   ORDER BY a.hari DESC");
+            // $tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan 
+            //                        FROM rb_jadwal_pelajaran a 
+            //                        JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
+            //                        JOIN rb_guru c ON a.nip=c.nip 
+            //                        JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
+            //                        JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
+            //                        WHERE a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$tahun_dipilih' 
+            //                        ORDER BY a.hari DESC");
             
             $no = 1;
             while ($r = mysql_fetch_array($tampil)) {
