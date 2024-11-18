627c0bd1f801542bb54e75c66294c0086ffa1d6c
Ahmad Baihaqi <121011402+Baihaqi34@users.noreply.github.com>
2024-11-18 18:57:36 +0700

    dsds

--- application/home_guru.php
+++ application/home_guru.php
@@ -1,95 +1,79 @@
-<?php
-echo "kenapa??";
-?>
+<?php if ($_GET[act] == '') { ?>
+  
+  <div class="container-fluid">
 
-<?php
-echo"<div class='col-xs-12'>
-  <div class='box'>
-    <div class='box-header'>
-      <h3 class='box-title'>";
-      
-        // Ambil tahun akademik terbaru (id_tahun_akademik paling besar)
-        $latest_year = mysql_fetch_array(mysql_query("SELECT id_tahun_akademik, nama_tahun FROM rb_tahun_akademik ORDER BY id_tahun_akademik DESC LIMIT 1"));
+    <div class="row">
+      <div class="col-12">
+        <div class="box">
+          <div class="box-header d-flex justify-content-between align-items-center">
+            <h3 class="box-title">Semua Data Guru</h3>
+            <?php if ($_SESSION['level'] != 'kepala') { ?>
+              <a class='btn btn-primary btn-sm' href='index.php?view=guru&act=tambahguru'>Tambahkan Data Guru</a>
+            <?php } ?>
+          </div><!-- /.box-header -->
 
-        // Jika tidak ada tahun akademik dipilih, set default ke tahun terbaru
-        $tahun_dipilih = isset($_GET['tahun']) ? $_GET['tahun'] : $latest_year['id_tahun_akademik'];
-        $nama_tahun = isset($_GET['tahun']) ?
-          mysql_fetch_array(mysql_query("SELECT nama_tahun FROM rb_tahun_akademik WHERE id_tahun_akademik = '$tahun_dipilih'"))['nama_tahun'] :
-          $latest_year['nama_tahun'];
+          <div class="box-body">
+            <div class="table-responsive">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>NIP</th>
+                    <th>Nama Lengkap</th>
+                    <th>Jenis Kelamin</th>
+                    <th>No Telpon</th>
+                    <th>Status Pegawai</th>
+                    <th>Jenis PTK</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  $tampil = mysql_query("SELECT * FROM rb_guru a 
+                                      LEFT JOIN rb_jenis_kelamin b ON a.id_jenis_kelamin=b.id_jenis_kelamin 
+                                      LEFT JOIN rb_status_kepegawaian c ON a.id_status_kepegawaian=c.id_status_kepegawaian 
+                                      LEFT JOIN rb_jenis_ptk d ON a.id_jenis_ptk=d.id_jenis_ptk
+                                      ORDER BY a.nip DESC");
+                  $no = 1;
+                  while ($r = mysql_fetch_array($tampil)) {
+                    echo "<tr>
+                          <td>$no</td>
+                          <td>$r[nip]</td>
+                          <td>$r[nama_guru]</td>
+                          <td>$r[jenis_kelamin]</td>
+                          <td>$r[hp]</td>
+                          <td>$r[status_kepegawaian]</td>
+                          <td>$r[jenis_ptk]</td>";
+                    if ($_SESSION['level'] != 'kepala') {
+                      echo "<td>
+                            <center>
+                              <a class='btn btn-info btn-xs' title='Lihat Detail' href='?view=guru&act=detailguru&id=$r[nip]'><span class='glyphicon glyphicon-search'></span></a>
+                              <a class='btn btn-success btn-xs' title='Edit Data' href='?view=guru&act=editguru&id=$r[nip]'><span class='glyphicon glyphicon-edit'></span></a>
+                              <a class='btn btn-danger btn-xs' title='Delete Data' href='?view=guru&hapus=$r[nip]'><span class='glyphicon glyphicon-remove'></span></a>
+                            </center>
+                          </td>";
+                    } else {
+                      echo "<td>
+                            <center>
+                              <a class='btn btn-info btn-xs' title='Lihat Detail' href='?view=guru&act=detailguru&id=$r[nip]'><span class='glyphicon glyphicon-search'></span></a>
+                            </center>
+                          </td>";
+                    }
+                    echo "</tr>";
+                    $no++;
+                  }
+                  if (isset($_GET['hapus'])) {
+                    mysql_query("DELETE FROM rb_guru WHERE nip='$_GET[hapus]'");
+                    echo "<script>document.location='index.php?view=guru';</script>";
+                  }
+                  ?>
+                </tbody>
+              </table>
+            </div><!-- /.table-responsive -->
+          </div><!-- /.box-body -->
+        </div><!-- /.box -->
+      </div><!-- /.col-12 -->
+    </div><!-- /.row -->
+  </div><!-- /.container-fluid -->
 
-        echo "Jadwal Mengajar Anda - $nama_tahun";
-echo"</h3>";
-      echo"<form style='margin-right:5px; margin-top:0px' class='pull-right' action='' method='GET'>
-        <!-- Tambahkan hidden input untuk menyimpan parameter view -->
-        <input type='hidden' name='view' value='jadwalguru'>
-        <select name='tahun' style='padding:4px' onchange='this.form.submit()'>
-          <option value=''>- Pilih Tahun Akademik -</option>";
-      
-          $tahun = mysql_query("SELECT * FROM rb_tahun_akademik ORDER BY id_tahun_akademik DESC");
-          while ($k = mysql_fetch_array($tahun)) {
-            $selected = ($tahun_dipilih == $k['id_tahun_akademik']) ? 'selected' : '';
-            echo "<option value='$k[id_tahun_akademik]' $selected>$k[nama_tahun]</option>";
-          }
-     
-        echo"</select>
-      </form>
-    </div>";
-
-//box header
-    echo"<div class='box-body'>
-      <div class='table-responsive'>
-        <table id='example1' class='table table-bordered table-striped'>
-          <thead>
-            <tr>
-              <th style='width:20px'>No</th>
-              <th>Kode Pelajaran</th>
-              <th>Jadwal Pelajaran</th>
-              <th>Kelas</th>
-              <th>Guru</th>
-              <th>Hari</th>
-              <th>Mulai</th>
-              <th>Selesai</th>
-              <th>Ruangan</th>
-              <th>Semester</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
-          <tbody>";
-            
-            $tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan 
-                                   FROM rb_jadwal_pelajaran a 
-                                   JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
-                                   JOIN rb_guru c ON a.nip=c.nip 
-                                   JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
-                                   JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
-                                   WHERE a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$tahun_dipilih' 
-                                   ORDER BY a.hari DESC");
-              if (!$tampil) {
-                    die("Query Error: " . mysql_error());
-                      }
-            $no = 1;
-            while ($r = mysql_fetch_array($tampil)) {
-              echo "<tr>
-                      <td>$no</td>
-                      <td>$r[kode_pelajaran]</td>
-                      <td>$r[namamatapelajaran]</td>
-                      <td>$r[nama_kelas]</td>
-                      <td>$r[nama_guru]</td>
-                      <td>$r[hari]</td>
-                      <td>$r[jam_mulai]</td>
-                      <td>$r[jam_selesai]</td>
-                      <td>$r[nama_ruangan]</td>
-                      <td>$r[id_tahun_akademik]</td>
-                      <td><a class='btn btn-success btn-xs' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]&tahun=$r[id_tahun_akademik]'>Agenda Mengajar</a></td>
-                    </tr>";
-              $no++;
-            }
-           
-          echo"</tbody>
-        </table>
-      </div><!-- /.table-responsive -->
-    </div><!-- /.box-body -->
-  </div>
-</div>";
-
-?>
+  <?php } ?>
