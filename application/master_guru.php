ea9ce7655a59bc40bc0f7f290702985e85ff943f
jipyy <jahdanpandita16@gmail.com>
2024-11-18 10:43:05 +0700

    niga

--- application/master_guru.php
+++ application/master_guru.php
@@ -612,10 +612,6 @@
                 <div class='modal fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>
                   <div class='modal-dialog' role='document'>
                     <div class='modal-content'>
-                    session_start();
-
-                    // Cek session guru
-                    cek_session_guru();
                       <div class='modal-header'>
                         <button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
                         <h4 class='modal-title' id='myModalLabel'>Pemberitahuan</h4>
