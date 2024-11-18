92b2a3f90c57e7e7d356b90d95c58fd75e2a0081
Ahmad Baihaqi <121011402+Baihaqi34@users.noreply.github.com>
2024-11-18 20:32:02 +0700

    sasas

--- application/home_guru.php
+++ application/home_guru.php
@@ -2,8 +2,9 @@
   <div class="box">
     <div class="box-header">
       <h3 class="box-title">
-     tes dlu 
-</h3>
+        
+    Jadwal Mengajar anda pada - <?php $tahun_dipilih ?>
+      </h3>
       <form style='margin-right:5px; margin-top:0px' class='pull-right' action='' method='GET'>
   <!-- Tambahkan hidden input untuk menyimpan parameter view -->
   <input type="hidden" name="view" value="jadwalguru">
