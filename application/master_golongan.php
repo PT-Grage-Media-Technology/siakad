9dce44cf93394058b04ca7e8f87b4c2cc837b6fe
kulkad <rdarmawan281@gmail.com>
2024-11-19 14:00:20 +0700

    TEST

--- application/master_golongan.php
+++ application/master_golongan.php
@@ -104,8 +104,7 @@
               </div>
               <div class='box-footer'>
                     <button type='submit' name='tambah' class='btn btn-info'>Tambahkan</button>
-                    <a href='index.php?view=golongan'><button class='btn btn-default pull-right'>Cancel</button></a>
-                    
+                    <a href='index.php?view=golongan' class='btn btn-default pull-right'>Cancel</a>
                   </div>
               </form>
             </div>";
