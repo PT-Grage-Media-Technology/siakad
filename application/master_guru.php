bab67a2e1eef0069853ba9ac9fed2875e65d41bc
Baihaqi34 <abay999mmm@gmail.com>
2024-11-19 10:14:30 +0700

    asasas

--- application/master_guru.php
+++ application/master_guru.php
@@ -724,21 +724,22 @@
   echo "<div class='col-md-12'>
   <div class='box box-info'>
     <div class='box-header with-border'>
-      <h3 class='box-title'>Kirimkan Tugas</h3>
+      <h3 class='box-title'>Izin tidak hadir</h3>
     </div>
   <div class='box-body'>
   <form method='POST' class='form-horizontal' action='' enctype='multipart/form-data'>
     <div class='col-md-12'>
       <table class='table table-condensed table-bordered'>
       <tbody>
-        <tr><th width=120px scope='row'>Nama File</th>             <td><div style='position:relative;''>
-                                                              <a class='btn btn-primary' href='javascript:;'>
-                                                                <span class='glyphicon glyphicon-search'></span> Cari File Tugas yang akan dikirim..."; ?>
+      <tr><th><input type='text' name='a'/></tr><th>
+      <tr><th scope='row'>Keterangan</th>       <td><textarea rows='5' class='form-control' name='b'></textarea></td></tr>
+      <tr><th width=120px scope='row'>Nama File</th>             <td><div style='position:relative;''>
+                                                            <a class='btn btn-primary' href='javascript:;'>
+                                                              <span class='glyphicon glyphicon-search'></span> Cari File Tugas yang akan dikirim..."; ?>
 <input type='file' class='files' name='c' onchange='$("#upload-file-info").html($(this).val());'>
 <?php echo "</a> <span style='width:155px' class='label label-info' id='upload-file-info'></span>
-                                                            </div>
-        </td></tr>
-        <tr><th scope='row'>Keterangan</th>       <td><textarea rows='5' class='form-control' name='a'></textarea></td></tr>
+                                                          </div>
+      </td></tr>
         
       </tbody>
       </table>
