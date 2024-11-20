498a8620aa9d3e3206e8e26ec7ee85a351cd36a4
Baihaqi34 <abay999mmm@gmail.com>
2024-11-20 15:08:52 +0700

    sasasa

--- application/jadwal_guru_piket.php
+++ application/jadwal_guru_piket.php
@@ -1,1 +1,51 @@
-tes aja
+
+            <div class="col-xs-12">  
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">Jadwal Guru Piket</h3>
+                
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                  <div class="table-responsive">
+                  <table id="example" class="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th style='width:20px'>No</th>
+                        <th>Jadwal Pelajaran</th>
+                        <th>Kelas</th>
+                        <th>Guru</th>
+                        <th>Hari</th>
+                        <th>Mulai</th>
+                        <th>Selesai</th>
+                        <th>Ruangan</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                  <?php
+                   
+                      $tampil = mysql_query("SELECT * FROM rb_jadwal_guru_piket");
+                    
+                   
+                    $no = 1;
+                    while($r=mysql_fetch_array($tampil)){
+                    echo "<tr><td>$no</td>
+                              <td>$r[nip]</td>
+                              <td>$r[hari]</td>
+                              <td>$r[nama_guru]</td>
+                              <td>$r[hari]</td>
+                              <td>$r[tanggal]</td>";
+                                echo "<td style='width:80px !important'><center>
+                                        <a class='btn btn-success btn-xs' title='Lihat Journal' href='index.php?view=journalkbm&act=lihat&id=$r[kodejdwl]'><span class='glyphicon glyphicon-search'></span> Lihat Journal</a>
+                                      </center></td>";
+                            echo "</tr>";
+                      $no++;
+                      }
+                  ?>
+                    <tbody>
+                  </table>
+                  </div>
+                </div><!-- /.box-body -->
+
+                </div>
+            </div>
