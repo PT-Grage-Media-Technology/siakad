7e1abca826da15babc97f17f2352add30224499a
Baihaqi34 <abay999mmm@gmail.com>
2024-11-20 15:22:08 +0700

    hgjh

--- application/jadwal_guru_piket.php
+++ application/jadwal_guru_piket.php
@@ -50,5 +50,51 @@
             </div>
             <?php 
 }elseif($_GET[act]=='tambah'){
-    echo"tes";
+    echo "<div class='col-md-12'>
+              <div class='box box-info'>
+                <div class='box-header with-border'>
+                  <h3 class='box-title'>Tambah Jadwal</h3>
+                </div>
+              <div class='box-body'>
+              <form method='POST' class='form-horizontal' action='' enctype='multipart/form-data'>
+                <div class='col-md-12'>
+                  <table class='table table-condensed table-bordered'>
+                  <tbody>
+                  <input type='hidden' name='jdwl' value='$_GET[jdwl]'>
+                    <tr><th width='140px' scope='row'>Kelas</th>   <td><select class='form-control' name='a'>"; 
+                                                $kelas = mysql_query("SELECT * FROM rb_kelas");
+                                                while($a = mysql_fetch_array($kelas)){
+                                                  if ($e[kode_kelas]==$a[kode_kelas]){
+                                                    echo "<option value='$a[kode_kelas]' selected>$a[nama_kelas]</option>";
+                                                  }
+                                                }
+                                                echo "</select>
+                    </td></tr>
+                    <tr><th scope='row'>Mata Pelajaran</th>   <td><select class='form-control' name='b'>"; 
+                                                $mapel = mysql_query("SELECT * FROM rb_mata_pelajaran");
+                                                while($a = mysql_fetch_array($mapel)){
+                                                  if ($e[kode_pelajaran]==$a[kode_pelajaran]){
+                                                    echo "<option value='$a[kode_pelajaran]' selected>$a[namamatapelajaran]</option>";
+                                                  }
+                                                }
+                                                echo "</select>
+                    </td></tr>
+                   
+                    <tr><th scope='row'>Hari</th>  <td><input type='text' class='form-control' value='$hari_ini' name='c'></td></tr>
+                    <tr><th scope='row'>Tanggal</th>  <td><input type='text' style='border-radius:0px; padding-left:12px' class='datepicker form-control' value='".date('d-m-Y')."' name='d' data-date-format='dd-mm-yyyy'></td></tr>
+                    <tr><th scope='row'>Jam Ke</th>  <td><input type='number' class='form-control' value='$jam' name='e'></td></tr>
+                    <tr><th scope='row'>Materi</th>  <td><textarea style='height:80px' class='form-control' name='f'></textarea></td></tr>
+                    <tr><th scope='row'>Keterangan</th>  <td><textarea style='height:160px'  class='form-control' name='g'></textarea></td></tr>
+                    </td></tr>
+                  </tbody>
+                  </table>
+                </div>
+              </div>
+              <div class='box-footer'>
+                    <button type='submit' name='tambah' class='btn btn-info'>Tambahkan</button>
+                    <a href='index.php?view=journalguru'><button type='button' class='btn btn-default pull-right'>Cancel</button></a>
+                    
+                  </div>
+              </form>
+            </div>";
 }
