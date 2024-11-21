4ec1c4f0e7f4c8329ac68eb3b1746bf0be40e6de
Baihaqi34 <abay999mmm@gmail.com>
2024-11-21 14:04:18 +0700

    SASA

--- application/guru_piket.php
+++ application/guru_piket.php
@@ -150,7 +150,12 @@
                   <h3 class='box-title'>Lihat</b></h3>";
     echo "  </div>
               <div class='box-body'>";
-              echo "<div style='text-align:left; flex:1;'><img src='bukti_tidak_hadir/".$data['foto']."' style='max-width: 40%; height: auto;'></div>";
+              echo "<div style='text-align:left; flex:1;'><img src='bukti_tidak_hadir/".$data['foto']."' style='max-width: 40%; height: auto;'></div>
+              <h2>Judul Teks</h2>
+        <p>
+            Tambahkan deskripsi atau teks di sini. Anda bisa menyesuaikan ukuran, warna, dan tata letak teks sesuai kebutuhan.
+        </p>
+              ";
     echo"</div>";
     }
               
