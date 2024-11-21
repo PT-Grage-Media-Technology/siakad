df9056f2e7ca6d111eddbd46d68311f43041a912
Baihaqi34 <abay999mmm@gmail.com>
2024-11-21 10:45:27 +0700

    sasas

--- application/jadwal_guru_piket.php
+++ application/jadwal_guru_piket.php
@@ -186,11 +186,10 @@
             </div>";
 } elseif ($_GET[act] == 'edit') {
   if (isset($_POST[edit])) { // Mengubah 'tambah' menjadi 'edit'
-    var_dump($_POST[edit]);
-    exit;
     $tanggal = tgl_simpan($_POST[tanggal]);
     $tanggalInput = date('Y-m-d H:i:s'); // Format sesuai dengan format yang diinginkan di database
     mysql_query("UPDATE rb_jadwal_guru_piket SET nip='$_POST[nip]', hari='$_POST[hari]', tanggal='$tanggal', updated_at='$tanggalInput' WHERE nip='$_GET[nip]'"); // Mengubah query untuk update
+    echo" mysql_query('UPDATE rb_jadwal_guru_piket SET nip='$_POST[nip]', hari='$_POST[hari]', tanggal='$tanggal', updated_at='$tanggalInput' WHERE nip='$_GET[nip]''); // Mengubah query untuk update"
     echo "<script>document.location='index.php?view=jadwalgurupiket';</script>";
   }
 
