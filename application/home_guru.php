69ded95c105b522138336cdb4cfb96d292f89fc8
Baihaqi34 <abay999mmm@gmail.com>
2024-11-21 09:48:25 +0700

    sasas

--- application/home_guru.php
+++ application/home_guru.php
@@ -141,60 +141,47 @@ mysql_data_seek($tahun, 0); // Kembali ke awal untuk loop dropdown
 </form>
 
 
+</div><!-- /.box-header -->
+      <div class="box-body">
+        <div class="table-responsive">
+
+          <table id="example" class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th style='width:20px'>No</th>
+                <th>Nip</th>
+                <th>Hari</th>
+                <th>Guru</th>
+                <th>Tanggal</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+
+              $tampil = mysql_query("SELECT * FROM rb_jadwal_guru_piket a JOIN rb_guru b ON a.nip=b.nip   WHERE DAY(a.tanggal) = '$tanggal_dipilih' 
+              AND MONTH(a.tanggal) = '$bulan_dipilih'");
+
+
+              $no = 1;
+              if (mysql_num_rows($tampil) > 0) { // Memeriksa apakah ada data
+                while ($r = mysql_fetch_array($tampil)) {
+                  echo "<tr><td>$no</td>
+                                <td>$r[nip]</td>
+                                <td>$r[hari]</td>
+                                <td>$r[nama_guru]</td>
+                                <td>" . tgl_indo($r['tanggal']) . "</td>";
+                  echo "</tr>";
+                  $no++;
+                }
+              } else {
+                echo "<tr><td colspan='6' style='text-align:center;'>Tidak ada data</td></tr>"; // Menampilkan pesan jika tidak ada data
+              }
+              ?>
+            <tbody>
+          </table>
+        </div>
+      </div><!-- /.box-body -->
+
     </div>
-    <!-- /.box-header -->
-    <div class="box-body">
-      <div class="table-responsive">
-        <table id="example1" class="table table-bordered table-striped">
-          <thead>
-            <tr>
-              <th style='width:20px'>No</th>
-              <th>Kode Pelajaran</th>
-              <th>Jadwal Pelajaran</th>
-              <th>Kelas</th>
-              <th>Guru</th>
-              <th>Hari</th>
-              <th>Mulai</th>
-              <th>Selesai</th>
-              <th>Ruangan</th>
-              <th>Semester</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-            $tampil = mysql_query("SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan 
-                                   FROM rb_jadwal_pelajaran a 
-                                   JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
-                                   JOIN rb_guru c ON a.nip=c.nip 
-                                   JOIN rb_ruangan d ON a.kode_ruangan=d.kode_ruangan
-                                   JOIN rb_kelas e ON a.kode_kelas=e.kode_kelas 
-                                   WHERE a.nip='$_SESSION[id]' AND a.id_tahun_akademik='$tahun_dipilih' 
-                                   ORDER BY a.hari DESC");
-              if (!$tampil) {
-                    die("Query Error: " . mysql_error());
-                      }
-            $no = 1;
-            while ($r = mysql_fetch_array($tampil)) {
-              echo "<tr>
-                      <td>$no</td>
-                      <td>$r[kode_pelajaran]</td>
-                      <td>$r[namamatapelajaran]</td>
-                      <td>$r[nama_kelas]</td>
-                      <td>$r[nama_guru]</td>
-                      <td>$r[hari]</td>
-                      <td>$r[jam_mulai]</td>
-                      <td>$r[jam_selesai]</td>
-                      <td>$r[nama_ruangan]</td>
-                      <td>$r[id_tahun_akademik]</td>
-                      <td><a class='btn btn-success btn-xs' href='index.php?view=journalguru&act=lihat&id=$r[kodejdwl]&tahun=$r[id_tahun_akademik]'>Agenda Mengajar</a></td>
-                    </tr>";
-              $no++;
-            }
-            ?>
-          </tbody>
-        </table>
-      </div><!-- /.table-responsive -->
-    </div><!-- /.box-body -->
-  </div>
 </div>
