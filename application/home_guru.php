430e9893825630d336077236ec0dc636e81c71e1
Baihaqi34 <abay999mmm@gmail.com>
2024-11-21 09:54:48 +0700

    sasas

--- application/home_guru.php
+++ application/home_guru.php
@@ -159,8 +159,8 @@ mysql_data_seek($tahun, 0); // Kembali ke awal untuk loop dropdown
             <tbody>
               <?php
 
-              $tampil = mysql_query("SELECT * FROM rb_jadwal_guru_piket a JOIN rb_guru b ON a.nip=b.nip   WHERE DAY(a.tanggal) = '$tanggal_dipilih' 
-              AND MONTH(a.tanggal) = '$bulan_dipilih' AND a.nip = '$_SESSION[id]'");
+              $tampil = mysql_query("SELECT * FROM rb_jadwal_guru_piket a JOIN rb_guru b ON a.nip=b.nip   WHERE DAY(a.tanggal) = '$hari_ini' 
+              AND MONTH(a.tanggal) = '$hari_ini' AND a.nip = '$_SESSION[id]'");
 
 
               $no = 1;
