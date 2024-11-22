9433da003c24ef5f4ac20bc154a362012a6d5e4c
Baihaqi34 <abay999mmm@gmail.com>
2024-11-22 11:11:54 +0700

    sasas

--- application/guru_piket.php
+++ application/guru_piket.php
@@ -109,13 +109,9 @@
                                 <td>$r[kode_kehadiran]</td>
                                 <td>" . tgl_indo($r['tanggal']) . "</td>
                                 <td>
-                                  <a href='index.php?view=absensiguru&act=detail&nip=$r[nip]&bulan=$bulan_dipilih&tanggal=$tanggal_dipilih' class='btn btn-info' title='detail'><i class='fa fa-eye'></i></a>";
-                                  if (!$insertAbsensiGuru) { 
-                                      echo"<a href='index.php?view=absensiguru&act=setujui&nip=$_GET[nip]&bulan=$_GET[bulan]&tanggal=$_GET[tanggal]' class='btn btn-success' title='Setujui'><i class='fa fa-check'></i></a>";
-                                  }else{
-                                    echo"dda";
-                                  }
-                                 echo" <a href='' class='btn btn-danger' title='Hapus' onclick='return confirm(\"Apakah Anda yakin ingin menghapus?\")'><i class='fa fa-times'></i></a>
+                                  <a href='index.php?view=absensiguru&act=detail&nip=$r[nip]&bulan=$bulan_dipilih&tanggal=$tanggal_dipilih' class='btn btn-info' title='detail'><i class='fa fa-eye'></i></a>
+                                  <a href='' class='btn btn-success' title='Setujui'><i class='fa fa-check'></i></a>
+                                  <a href='' class='btn btn-danger' title='Hapus' onclick='return confirm(\"Apakah Anda yakin ingin menghapus?\")'><i class='fa fa-times'></i></a>
                                 </td>";
 
                                 echo "</tr>";
@@ -168,9 +164,7 @@
             </tr>
           </table>
                 <a href='index.php?view=absensiguru&act=lihat&nip=$r[nip]&bulan=$bulan_dipilih&tanggal=$tanggal_dipilih' class='btn btn-info' title='detail'><i class='fa fa-eye'></i> Buka absensi siswa</a>
-                <?php if (!$insertAbsensiGuru) { ?>
-                    <a href='index.php?view=absensiguru&act=setujui&nip=$_GET[nip]&bulan=$_GET[bulan]&tanggal=$_GET[tanggal]' class='btn btn-success' title='Setujui'><i class='fa fa-check'></i></a>
-                <?php } ?>
+                <a href='index.php?view=absensiguru&act=setujui&nip=$_GET[nip]&bulan=$_GET[bulan]&tanggal=$_GET[tanggal]' class='btn btn-success' title='Setujui'><i class='fa fa-check'></i></a>
                 <a href='' class='btn btn-danger' title='Hapus' onclick='return confirm(\"Apakah Anda yakin ingin menghapus?\")'><i class='fa fa-times'></i></a>
                       </div>
                   </div>
