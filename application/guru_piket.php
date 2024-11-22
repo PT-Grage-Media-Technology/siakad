31f5b42fc67c70c6982fe7e13facca907787d930
Baihaqi34 <abay999mmm@gmail.com>
2024-11-22 11:09:32 +0700

    sasas

--- application/guru_piket.php
+++ application/guru_piket.php
@@ -112,6 +112,8 @@
                                   <a href='index.php?view=absensiguru&act=detail&nip=$r[nip]&bulan=$bulan_dipilih&tanggal=$tanggal_dipilih' class='btn btn-info' title='detail'><i class='fa fa-eye'></i></a>";
                                   if (!$insertAbsensiGuru) { 
                                       echo"<a href='index.php?view=absensiguru&act=setujui&nip=$_GET[nip]&bulan=$_GET[bulan]&tanggal=$_GET[tanggal]' class='btn btn-success' title='Setujui'><i class='fa fa-check'></i></a>";
+                                  }else{
+                                    echo"dda";
                                   }
                                  echo" <a href='' class='btn btn-danger' title='Hapus' onclick='return confirm(\"Apakah Anda yakin ingin menghapus?\")'><i class='fa fa-times'></i></a>
                                 </td>";
