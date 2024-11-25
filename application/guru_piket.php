46a33d3cd98cce00ee264dc3b10e771b28d28bf6
Baihaqi34 <abay999mmm@gmail.com>
2024-11-25 13:21:02 +0700

    sasas

--- application/guru_piket.php
+++ application/guru_piket.php
@@ -210,9 +210,11 @@
                                 <td>" . tgl_indo($r['tanggal']) . "</td>
                                 <td>$r[kode_kehadiran] </td>
                                 <td>
-                                  <a href='index.php?view=journalguru&act=edit&id=$r[id_journal]&jdwl=$r[kodejdwl]' class='btn btn-success' title='detail'><i class='fa fa-eye'></i>Agenda mengajar</a>
-                                  <a href='index.php?view=absensiguru&act=gantikan&id=$r[id_journal]&jdwl=$r[kodejdwl]&nip=$r[users]&bulan=$_GET[bulan]&tanggal=$_GET[tanggal]' class='btn btn-success' title='detail'><i class='fa fa-eye'></i>Gantikan Mengajar</a>
-                                  <a href='' class='btn btn-danger' title='Hapus' onclick='return confirm(\"Apakah Anda yakin ingin menghapus?\")'><i class='fa fa-times'></i></a>
+                                  <a href='index.php?view=journalguru&act=edit&id=$r[id_journal]&jdwl=$r[kodejdwl]' class='btn btn-success' title='detail'><i class='fa fa-eye'></i>Agenda mengajar</a>";
+                                  if(!$r[pengganti]){
+                                    echo"<a href='index.php?view=absensiguru&act=gantikan&id=$r[id_journal]&jdwl=$r[kodejdwl]&nip=$r[users]&bulan=$_GET[bulan]&tanggal=$_GET[tanggal]' class='btn btn-success' title='detail'><i class='fa fa-eye'></i>Gantikan Mengajar</a>";
+                                  }
+                                  echo"<a href='' class='btn btn-danger' title='Hapus' onclick='return confirm(\"Apakah Anda yakin ingin menghapus?\")'><i class='fa fa-times'></i></a>
                                 </td>";
 
                                 echo "</tr>";
