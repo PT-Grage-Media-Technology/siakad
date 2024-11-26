4be4227964cd367caeeae633fe766b3a8400bb42
ryunex15 <badarudin1511@gmail.com>
2024-11-26 14:41:42 +0700

    asdas

--- application/master_nilai.php
+++ application/master_nilai.php
@@ -2,9 +2,6 @@
   <div class="box">
     <div class="box-header">
       <h3 class="box-title">Data Nilai </h3>
-      <!-- <?php if ($_SESSION['level'] != 'kepala') { ?>
-        <a class='pull-right btn btn-primary btn-sm' href='index.php?view=ruangan&act=tambah'>Tambahkan Data</a>
-      <?php } ?> -->
     </div><!-- /.box-header -->
     <div class="box-body">
       <div class="table-responsive" style="max-height: 500px; overflow-y: auto;">
