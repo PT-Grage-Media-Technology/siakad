7f935f05d9ea6f8e782ba1e272f8c91fbf5dd896
Baihaqi34 <abay999mmm@gmail.com>
2024-11-26 14:05:54 +0700

    sasas

--- application/home_siswa.php
+++ application/home_siswa.php
@@ -148,7 +148,7 @@
                             <td>$r[nama_guru]</td>
                             <td>$r[materi]</td>
                             <td>$r[keterangan]</td>
-                            <td><a class='btn btn-success btn-xs' title='Lihat Data' href='index.php?view=home&act=detailpembelajaran&kodejdwl=$r[kodejdwl]&tanggal=$r[tanggal]&jam_ke=$r[jam_ke]'><span class='glyphicon glyphicon-list'></span> Detail</a></td>
+                            <td><a class='btn btn-success btn-xs' title='Lihat Data' href='index.php?view=home&act=detailpembelajaran&kodejdwl=$r[kodejdwl]&tanggal=$r[tanggal]&jam_ke=$r[jam_ke]'><span class='glyphicon glyphicon-list'></span> Tujuan Pembelajaran</a></td>
                         </tr>";
     $no++;
   }
