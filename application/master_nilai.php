4311a8973bfdbfab884e56ecf577928a0e2dca73
ryunex15 <badarudin1511@gmail.com>
2024-11-26 14:41:13 +0700

    asdas

--- application/master_nilai.php
+++ application/master_nilai.php
@@ -2,12 +2,13 @@
   <div class="box">
     <div class="box-header">
       <h3 class="box-title">Data Nilai </h3>
-      <?php if ($_SESSION['level'] == 'kepala') { ?>
+      <!-- <?php if ($_SESSION['level'] != 'kepala') { ?>
         <a class='pull-right btn btn-primary btn-sm' href='index.php?view=ruangan&act=tambah'>Tambahkan Data</a>
-      <?php } ?>
+      <?php } ?> -->
     </div><!-- /.box-header -->
     <div class="box-body">
       <div class="table-responsive" style="max-height: 500px; overflow-y: auto;">
+        <!-- <p><input type="text"> = <input type="text"> - <input type="text"></p> -->
         <?php
         if (isset($_POST['update'])) {
           foreach ($_POST['id'] as $key => $id) {
@@ -27,29 +28,27 @@
         ?>
 
         <?php
-        // Hanya tampilkan view jika level adalah kepala
-        if ($_SESSION['level'] == 'kepala') {
-          $tampil = mysql_query("SELECT * FROM rb_kriteria_nilai");
+        $tampil = mysql_query("SELECT * FROM rb_kriteria_nilai");
 
-          // Form untuk mengupdate semua data
-          echo "<form method='POST' action=''>";
+        //   var_dump($kriteriaNilai) ; // Mengambil data dari array ke-0
+        //   echo $kriteriaNilai['kode_nilai'][0];
+        // Form untuk mengupdate semua data
+        echo "<form method='POST' action=''>";
 
-          // Loop untuk menampilkan semua data dalam satu form
-          while ($kriteriaNilai = mysql_fetch_array($tampil)) {
-            echo "<p>
-            <input type='hidden' name='id[]' value='{$kriteriaNilai['id']}'>
-            <input type='text' name='kode_nilai[]' placeholder='Nilai Huruf' style='width: 40px;' value='{$kriteriaNilai['kode_nilai']}'>
-            = <input type='text' name='nilai_bawah[]' style='width: 50px;' value='{$kriteriaNilai['nilai_bawah']}'> 
-            - <input type='text' name='nilai_atas[]' style='width: 50px;' value='{$kriteriaNilai['nilai_atas']}'>
-            <br>";
-          }
-
-          // Tombol Update untuk mengupdate semua data
-          echo "<button type='submit' name='update' class='pull-right btn btn-primary btn-sm mt-2'>Update Semua</button>";
-          echo "</form>";
-        } else {
-          echo "<p>Anda tidak memiliki akses untuk melihat data ini.</p>";
+        // Loop untuk menampilkan semua data dalam satu form
+        while ($kriteriaNilai = mysql_fetch_array($tampil)) {
+          echo "<p>
+        <input type='hidden' name='id[]' value='{$kriteriaNilai['id']}'>
+        <input type='text' name='kode_nilai[]' placeholder='Nilai Huruf' style='width: 40px;' value='{$kriteriaNilai['kode_nilai']}'>
+        = <input type='text' name='nilai_bawah[]' style='width: 50px;' value='{$kriteriaNilai['nilai_bawah']}'> 
+        - <input type='text' name='nilai_atas[]' style='width: 50px;' value='{$kriteriaNilai['nilai_atas']}'>
+        <br>";
         }
+
+        // Tombol Update untuk mengupdate semua data
+        echo "<button type='submit' name='update' class='pull-right btn btn-primary btn-sm mt-2'>Update Semua</button>";
+        echo "</form>";
+
         ?>
 
       </div><!-- /.table-responsive -->
