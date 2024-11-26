d79496d85b279f4c97c8db2f7011621f3d69c536
Baihaqi34 <abay999mmm@gmail.com>
2024-11-26 14:38:05 +0700

    sasas

--- application/master_pertanyaanrefleksi.php
+++ application/master_pertanyaanrefleksi.php
@@ -79,8 +79,7 @@
 }elseif($_GET[act]=='tambah'){
     if (isset($_POST[tambah])){
         mysql_query("INSERT INTO rb_pertanyaan_penilaian VALUES('','$_POST[a]','refleksi','".date('Y-m-d H:i:s')."')");
-        // echo "<script>document.location='index.php?view=pertanyaanrefleksi';</script>";
-        echo "INSERT INTO rb_pertanyaan_penilaian VALUES('','$_POST[a]','refleksi','".date('Y-m-d H:i:s')."')";
+        echo "<script>document.location='index.php?view=pertanyaanrefleksi';</script>";
     }
 
     echo "<div class='col-md-12'>
