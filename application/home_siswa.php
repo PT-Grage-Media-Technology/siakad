0f9e33c3285f3dc24109f89c8762a64a2ddb17a8
Baihaqi34 <abay999mmm@gmail.com>
2024-11-26 09:52:57 +0700

    sasas

--- application/home_siswa.php
+++ application/home_siswa.php
@@ -90,7 +90,7 @@
                         <td>$r[jam_selesai]</td>
                         <td>$r[nama_ruangan]</td>
                         <td>$r[id_tahun_akademik]</td>
-                        <td><a class='btn btn-success btn-xs' title='Lihat Data' href='index.php?view=home&act=kompetensidasar&kodejdwl=$r[kodejdwl]'><span class='glyphicon glyphicon-list'></span> Kompetensi</a></td>
+                        <td><a class='btn btn-success btn-xs' title='Lihat Data' href='index.php?view=home&act=kompetensidasar&kodejdwl=$r[kodejdwl]'><span class='glyphicon glyphicon-list'></span> Detail</a></td>
                     </tr>";
                 $no++;
               }
