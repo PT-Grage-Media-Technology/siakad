b58e17069021b2548c7ba12a0fc9dc7d5d96ecb8
Baihaqi34 <abay999mmm@gmail.com>
2024-11-26 13:29:28 +0700

    sasa

--- application/home_siswa.php
+++ application/home_siswa.php
@@ -186,7 +186,7 @@ elseif ($_GET[act] == 'detailpembelajaran') {
                          }
                        echo"</td>
                        </tr>";
-                       echo"<tr><td><img src='$d[file]' alt='Gambar' style='width:500px; height:500px;'></td></tr>
+                       echo"<tr><td scope='row' ><img src='$d[file]' alt='Gambar' style='width:500px; height:500px;'></td></tr>
                         
 
                         
