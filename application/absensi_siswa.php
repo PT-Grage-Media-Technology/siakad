c2123996abb29935b8cff027d6dca455228c7f75
Baihaqi34 <abay999mmm@gmail.com>
2024-11-28 12:27:42 +0700

    sassa

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -209,7 +209,7 @@
                             </tr>
                         </tbody>
                     </table>
-                     <a class='btn btn-success btn-sm mb-2' title='Bahan dan Tugas' href='index.php?view=home&act=refleksiguru'>
+                     <a class='btn btn-success btn-sm mb-2' title='Bahan dan Tugas' href='index.php?view=forum&act=detailtopic&jdwl=$j[kodejdwl]'>
                     <div class='d-flex flex-column align-items-center'>
                       <div class='glyphicon glyphicon-tasks' style='font-size:28px; margin-right:5px;'></div>
                       <div class='' style='font-size:14px;'>Refleksi</div>
@@ -638,6 +638,8 @@
                     </div>
                   </div>";
 }
+
+
 ?>
 <?php
 if (isset($_GET['id_pemberitahuan'])) {
