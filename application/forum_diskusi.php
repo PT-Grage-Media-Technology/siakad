c12644a05954b5de7e4f7384d2522adceb210864
Baihaqi34 <abay999mmm@gmail.com>
2024-11-28 14:20:12 +0700

    sasas

--- application/forum_diskusi.php
+++ application/forum_diskusi.php
@@ -324,7 +324,8 @@ if ($_GET[act] == '') {
                     $no = 1;
                     while($r=mysql_fetch_array($tampil)){
                       $jwb = mysql_fetch_array(mysql_query("SELECT * FROM rb_pertanyaan_penilaian_jawab where kodejdwl='$r[kodejdwl]'"));
-                      var_dump($jwb);
+                      var_dump($r);
+                      // var_dump($jwb);
                     echo "<tr><td>$no</td>
                               <td>$r[pertanyaan]</td>";
                               if($_SESSION[level]!='kepala'){
