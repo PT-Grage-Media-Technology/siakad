d660b13dbc160cebe4d6073af55fcd500d10a0ca
jipyy <jahdanpandita16@gmail.com>
2024-11-28 10:31:10 +0700

    v

--- application/master_nilai.php
+++ application/master_nilai.php
@@ -9,25 +9,7 @@
     <div class="box-body">
       <div class="table-responsive" style="max-height: 500px; overflow-y: auto;">
         <!-- <p><input type="text"> = <input type="text"> - <input type="text"></p> -->
-        <?php
-        if (isset($_POST['update'])) {
-          foreach ($_POST['id'] as $key => $id) {
-            $kode_nilai = $_POST['kode_nilai'][$key];
-            $nilai_bawah = $_POST['nilai_bawah'][$key];
-            $nilai_atas = $_POST['nilai_atas'][$key];
-
-            // Query untuk update data berdasarkan ID
-            $update_query = "UPDATE rb_kriteria_nilai SET 
-                                kode_nilai = '$kode_nilai', 
-                                nilai_bawah = '$nilai_bawah', 
-                                nilai_atas = '$nilai_atas' 
-                             WHERE id = '$id'";
-            mysql_query($update_query);
-          }
-        }
-        ?>
-
-        
+       
 
       </div><!-- /.table-responsive -->
     </div><!-- /.box-body -->
