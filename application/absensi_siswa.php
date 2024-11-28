d067fc3622fb14ad216545116131154b5074aa73
Baihaqi34 <abay999mmm@gmail.com>
2024-11-28 11:48:14 +0700

    sasas

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -212,7 +212,7 @@
                      <a class='btn btn-success btn-sm mb-2' title='Bahan dan Tugas' href='index.php?view=bahantugas&act=listbahantugas&jdwl=$_GET[idjr]&id=$_GET[id]&kd=$_GET[kd]'>
                     <div class='d-flex flex-column align-items-center'>
                       <div class='glyphicon glyphicon-tasks' style='font-size:28px; margin-right:5px;'></div>
-                      <div class='' style='font-size:14px;'>Tugas</div>
+                      <div class='' style='font-size:14px;'>Refleksi dan Komentar</div>
                     </div>
                  </a>
                 </div>
