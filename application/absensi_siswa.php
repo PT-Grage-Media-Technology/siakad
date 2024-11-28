0799786a6a20b1bec3e4b2bc91e5124463ec0db6
Baihaqi34 <abay999mmm@gmail.com>
2024-11-28 12:30:16 +0700

    sasas

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -160,6 +160,7 @@
   $m = mysql_fetch_array(mysql_query("SELECT * FROM rb_mata_pelajaran where kode_pelajaran='$_GET[kd]'"));
   // $j = mysql_fetch_array(mysql_query("SELECT * FROM rb_journal_list where kodejdwl='$_GET[kd]'"));
   $j = mysql_fetch_array(mysql_query("SELECT * FROM rb_journal_list where kodejdwl='$_GET[idjr]' AND tanggal='$_GET[tgl]' AND jam_ke='$_GET[jam]'"));
+  $idtopic = mysql_fetch_array(mysql_query("SELECT id_forum_topic FROM rb_forum_topic WHERE judul_topic='$j[materi]'"));
 
   $ex = explode('-', $filtertgl);
   $tahun = $ex[0];
@@ -209,7 +210,7 @@
                             </tr>
                         </tbody>
                     </table>
-                     <a class='btn btn-success btn-sm mb-2' title='Bahan dan Tugas' href='index.php?view=forum&act=detailtopic&jdwl=$j[kodejdwl]'>
+                     <a class='btn btn-success btn-sm mb-2' title='Bahan dan Tugas' href='index.php?view=forum&act=detailtopic&jdwl=$j[kodejdwl]&idtopic=$idtopic'>
                     <div class='d-flex flex-column align-items-center'>
                       <div class='glyphicon glyphicon-tasks' style='font-size:28px; margin-right:5px;'></div>
                       <div class='' style='font-size:14px;'>Refleksi</div>
