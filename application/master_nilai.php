cc367449df2ba074d61150930a53976726a15b84
jipyy <jahdanpandita16@gmail.com>
2024-11-28 10:32:08 +0700

    B

--- application/master_nilai.php
+++ application/master_nilai.php
@@ -1,17 +1,1 @@
-<div class="col-xs-12">
-  <div class="box">
-    <div class="box-header">
-      <h3 class="box-title">Data Nilai </h3>
-      <?php if ($_SESSION['level'] != 'kepala') { ?>
-        <!-- <a class='pull-right btn btn-primary btn-sm' href='index.php?view=nilai'>Tambahkan Data</a> -->
-      <?php } ?>
-    </div><!-- /.box-header -->
-    <div class="box-body">
-      <div class="table-responsive" style="max-height: 500px; overflow-y: auto;">
-        <!-- <p><input type="text"> = <input type="text"> - <input type="text"></p> -->
-       
-
-      </div><!-- /.table-responsive -->
-    </div><!-- /.box-body -->
-  </div><!-- /.box -->
-</div>
+DIKA KONTOL 123
