cdb5d362fd06d6ce2e07bdf995d5943f6a1b674b
Baihaqi34 <abay999mmm@gmail.com>
2024-11-28 09:49:17 +0700

    sasas

--- application/journal_guru.php
+++ application/journal_guru.php
@@ -351,13 +351,11 @@
   }
 
   if (isset($_GET['hapus'])) {
-      $j = $r['materi'];
-      echo $j;
-      var_dump(isset($_GET['hapus']));
-      exit;
       // Ambil nama file berdasarkan ID
-      $query = mysql_query("SELECT file FROM rb_journal_list WHERE id_journal='$_GET[hapus]'");
+      $query = mysql_query("SELECT file,materi FROM rb_journal_list WHERE id_journal='$_GET[hapus]'");
       $data = mysql_fetch_assoc($query);
+      var_dump($data);
+      exit;
       
       // Tentukan lokasi file
       $file_path = 'files/' . $data['file'];
@@ -369,7 +367,7 @@
 
       // Hapus data dari database
       mysql_query("DELETE FROM rb_journal_list WHERE id_journal='$_GET[hapus]'");
-      mysql_query("DELETE FROM rb_forum_topic WHERE judul_topic=$r[materi]");
+      mysql_query("DELETE FROM rb_forum_topic WHERE judul_topic=$data[materi]");
 
       // Redirect ke halaman sebelumnya
       echo "<script>document.location='index.php?view=journalguru&act=lihat&id=$_GET[jdwl]';</script>";
