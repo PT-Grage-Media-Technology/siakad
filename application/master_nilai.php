620b6955e9b4ee86ac6f14c949e3de04f267a35f
jipyy <jahdanpandita16@gmail.com>
2024-11-28 10:29:53 +0700

    g

--- application/master_nilai.php
+++ application/master_nilai.php
@@ -27,30 +27,7 @@
         }
         ?>
 
-        <?php
-
-        $tampil = mysql_query("SELECT * FROM rb_kriteria_nilai");
-
-        //   var_dump($kriteriaNilai) ; // Mengambil data dari array ke-0
-        //   echo $kriteriaNilai['kode_nilai'][0];
-        // Form untuk mengupdate semua data
-        echo "<form method='POST' action=''>";
-
-        // Loop untuk menampilkan semua data dalam satu form
-        while ($kriteriaNilai = mysql_fetch_array($tampil)) {
-          echo "<p>
-        <input type='hidden' name='id[]' value='{$kriteriaNilai['id']}'>
-        <input type='text' name='kode_nilai[]' placeholder='Nilai Huruf' style='width: 40px;' value='{$kriteriaNilai['kode_nilai']}'>
-        = <input type='text' name='nilai_bawah[]' style='width: 50px;' value='{$kriteriaNilai['nilai_bawah']}'> 
-        - <input type='text' name='nilai_atas[]' style='width: 50px;' value='{$kriteriaNilai['nilai_atas']}'>
-        <br>";
-        }
-
-        // Tombol Update untuk mengupdate semua data
-        echo "<button type='submit' name='update' class='pull-right btn btn-primary btn-sm mt-2'>Update Semua</button>";
-        echo "</form>";
-
-        ?>
+        
 
       </div><!-- /.table-responsive -->
     </div><!-- /.box-body -->
