e4f13c85589ad1c1cbadc73d54bfc21b83d42f4e
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-11-29 09:50:44 +0700

    zxczczx

--- menu-admin.php
+++ menu-admin.php
@@ -71,7 +71,7 @@
             <li class="treeview">
               <a href="#"><i class="fa fa-th-large"></i> <span>Data Absensi</span><i class="fa fa-angle-left pull-right"></i></a>
               <ul class="treeview-menu">
-                <li><a href="index.php?view=absenguru"><i class="fa fa-circle-o"></i> Absensi Guru</a></li>
+                <!-- <li><a href="index.php?view=absenguru"><i class="fa fa-circle-o"></i> Absensi Guru</a></li> -->
                 <li><a href="index.php?view=absensiswa"><i class="fa fa-circle-o"></i> Absensi Siswa</a></li>
                 <li><a href="index.php?view=rekapabsensiswa"><i class="fa fa-circle-o"></i> Rekap Absensi Siswa</a></li>
               </ul>
