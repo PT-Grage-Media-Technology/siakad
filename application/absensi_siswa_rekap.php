7ddb94cda663b309512ac29d5acf706285c91c89
kulkad <rdarmawan281@gmail.com>
2024-11-29 09:24:09 +0700

    sssss

--- application/absensi_siswa_rekap.php
+++ application/absensi_siswa_rekap.php
@@ -162,7 +162,8 @@
                     $sakit = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` where kodejdwl='$_GET[jdwl]' AND nisn='$r[nisn]' AND kode_kehadiran='S'"));
                     $izin = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` where kodejdwl='$_GET[jdwl]' AND nisn='$r[nisn]' AND kode_kehadiran='I'"));
                     $alpa = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` where kodejdwl='$_GET[jdwl]' AND nisn='$r[nisn]' AND kode_kehadiran='A'"));
-                    $akademik = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` where kodejdwl='$_GET[jdwl]' AND nisn='$r[nisn]'"));
+                    $akademik_query = mysql_query("SELECT * FROM `rb_absensi_siswa` WHERE kodejdwl='$_GET[jdwl]' AND nisn='$r[nisn]'");
+                    $akademik = mysql_fetch_array($akademik_query); // Ambil data sebagai array
                     $persen = $hadir/($total)*100;
                     echo "<tr bgcolor=$warna>
                             <td>$no</td>
@@ -174,9 +175,9 @@
                             <td align=center>$sakit</td>
                             <td align=center>$izin</td>
                             <td align=center>$alpa</td>
-                            <td align=center>$akademik[nilai_sikap]</td>
-                            <td align=center>$akademik[nilai_keterampilan]</td>
-                            <td align=center>$akademik[nilai_pengetahuan]</td>
+                            <td align=center>{$akademik['nilai_sikap']}</td>
+                            <td align=center>{$akademik['nilai_keterampilan']}</td>
+                            <td align=center>{$akademik['nilai_pengetahuan']}</td>
                             <td align=right>".number_format($persen, 2)." %</td>";
                     echo "</tr>";
                       $no++;
