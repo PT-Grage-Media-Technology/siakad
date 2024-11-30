00ac6516f010cfb291ba3496a24657119467248a
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-11-30 17:28:58 +0700

    sdafsdf

--- menu-kepsek.php
+++ menu-kepsek.php
@@ -37,8 +37,9 @@
               Siswa</span></a></li> -->
                 <!-- <li><a href="index.php?view=absensiswa"><i class="fa fa-circle-o"></i> Absensi Siswa</a></li>
                 <li><a href="index.php?view=absenguru"><i class="fa fa-circle-o"></i> Absensi Guru</a></li> -->
-              <li><a href="index.php?view=rekapabsensiswa"><i class="fa fa-th-large"></i> <span>Rekap Absensi
-        Siswa</span></a></li>
+              <li><a href="index.php?view=rekapabsensiswa"><i class="fa fa-th-large"></i> <span>Rekap Absensi Siswa</span></a></li>
+              <li><a href="index.php?view=rekapguru"><i class="fa fa-address-book-o"></i> <span>Rekap Absensi Guru</span></a></li>
+
               </ul>
             </li>
             <li><a href="index.php?view=bahantugas"><i class="fa fa-file"></i><span>Bahan dan Tugas</span></a></li>
