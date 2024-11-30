e192732f47c83bca8762a6be7e3075bea39c5c36
Ahmad Baihaqi <121011402+Baihaqi34@users.noreply.github.com>
2024-12-01 06:05:17 +0700

    SASA

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
 <div class="pull-right hidden-xs">
-    <b></b>
+    <b><strong>Copyright &copy; <?php echo date('Y'); ?> <a target='_BLANK' href="https://members.lokomedia.web.id">- SIAKAD (Sistem Informasi Akademik)</a>.</strong> All rights reserved.</b>
 </div>
-<!-- <strong>Copyright &copy; <?php echo date('Y'); ?> <a target='_BLANK' href="https://members.lokomedia.web.id">- SIAKAD (Sistem Informasi Akademik)</a>.</strong> All rights reserved. -->
