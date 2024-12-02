10839097d49d0c927864a3a9014a295161e84ec6
jipyy <jahdanpandita16@gmail.com>
2024-12-02 09:46:07 +0700

    hhh

--- application/raport_uts.php
+++ application/raport_uts.php
@@ -187,7 +187,7 @@ cek_session_guru();
                           <td>$r[nama]</td>
                           <input type='hidden' name='nisn".$no."' value='$r[nisn]'>
                           <td align=center colspan='2'><input type='number' name='a".$no."' value='$n[angka_pengetahuan]' style='width:90px; text-align:center; padding:0px' placeholder='-' colspan='2'></td>
-                          <td align=center colspan='2'><input type='textarea' name='' value='' style='width:90px; text-align:center; padding:0px' placeholder='-' colspan='2'></td>
+                          <td align=center colspan='2'><textarea type='text' name='' value='' style='width:90px; text-align:center; padding:0px' placeholder='-' colspan='2'></textarea></td>
                         </tr>";
                   $no++;
                   }
