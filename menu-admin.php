2b29a7f0ac68d79e04439cd36d39d6ef14ca1a39
Baihaqi34 <abay999mmm@gmail.com>
2024-12-02 11:49:46 +0700

    sasas

--- menu-admin.php
+++ menu-admin.php
@@ -44,6 +44,7 @@
     <li class="treeview">
       <a href="#"><i class="fa fa-tag"></i> <span>Data Akademik</span><i class="fa fa-angle-left pull-right"></i></a>
       <ul class="treeview-menu">
+        <li><a href="index.php?view=datarating"><i class="fa fa-circle-o"></i> Data Rating</a></li>
         <li><a href="index.php?view=kelompokmapel"><i class="fa fa-circle-o"></i> Data Kelompok Mapel</a></li>
         <li><a href="index.php?view=matapelajaran"><i class="fa fa-circle-o"></i> Data Mata Pelajaran</a></li>
         <li><a href="index.php?view=jadwalpelajaran"><i class="fa fa-circle-o"></i> Data Jadwal Pelajaran</a></li>
