75af2702f2a7a0eb0b223943c8169f898f88bc94
kulkad <rdarmawan281@gmail.com>
2024-12-03 14:51:21 +0700

    asdasd

--- application/home_siswa.php
+++ application/home_siswa.php
@@ -221,27 +221,6 @@
   if (isset($_POST['submit'])) {
     $jml = mysql_fetch_array(mysql_query("SELECT count(*) as jmlp FROM `rb_pertanyaan_penilaian` where status='refleksi'"));
     $n = $jml['jmlp'];
-    $alert_message = '<div id="myModal" class="modal fade">
-	<div class="modal-dialog modal-confirm">
-		<div class="modal-content">
-			<div class="modal-header flex-column">
-				<div class="icon-box">
-					<i class="material-icons">&#xE5CD;</i>
-				</div>						
-				<h4 class="modal-title w-100">Are you sure?</h4>	
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-			</div>
-			<div class="modal-body">
-				<p>Do you really want to delete these records? This process cannot be undone.</p>
-			</div>
-			<div class="modal-footer justify-content-center">
-				<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-				<button type="button" class="btn btn-danger">Delete</button>
-			</div>
-		</div>
-	</div>
-</div>';
-
     for ($i = 0; $i <= $n; $i++) {
       if (isset($_POST['jawab' . $i])) {
         $jawab = $_POST['jawab' . $i];
@@ -251,45 +230,15 @@
         // Cek apakah sudah ada jawaban untuk nisn ini
         $cek_jawaban = mysql_fetch_array(mysql_query("SELECT count(*) as total FROM rb_pertanyaan_penilaian_jawab WHERE nisn='$_SESSION[id]' AND id_pertanyaan_penilaian='$pertanyaan' AND status='refleksi' AND kode_kelas='$kelas'"));
         if ($cek_jawaban['total'] > 0) {
-          $alert_message = '<div id="myModal" class="modal fade">
-	<div class="modal-dialog modal-confirm">
-		<div class="modal-content">
-			<div class="modal-header flex-column">
-				<div class="icon-box">
-					<i class="material-icons">&#xE5CD;</i>
-				</div>						
-				<h4 class="modal-title w-100">Are you sure?</h4>	
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-			</div>
-			<div class="modal-body">
-				<p>Do you really want to delete these records? This process cannot be undone.</p>
-			</div>
-			<div class="modal-footer justify-content-center">
-				<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-				<button type="button" class="btn btn-danger">Delete</button>
-			</div>
-		</div>
-	</div>
-</div>';
-          break; // Keluar dari loop jika sudah ada jawaban
+          echo "<script>alert('Anda sudah memberikan jawaban untuk pertanyaan ini.');</script>";
+          continue; // Lewati input jika sudah ada
         }
 
         mysql_query("INSERT INTO rb_pertanyaan_penilaian_jawab VALUES('','$pertanyaan','$_SESSION[id]','','$jawab','$_GET[kodejdwl]','refleksi','$kelas','" . date('Y-m-d H:i:s') . "','$_GET[id_journal]')");
       }
     }
-
-    // Tampilkan alert jika ada
-    if ($alert_message) {
-      echo "
-      <script>
-          $(document).ready(function() {
-              $('#alertContainer').html(`$alert_message`).fadeIn();
-          });
-      </script>
-      ";
-    } else {
-      echo "<script>window.alert('Sukses Simpan Jawaban Penilaian refleksi...');</script>";
-    }
+    echo "<script>window.alert('Sukses Simpan Jawaban Penilaian refleksi...');
+           history.back();</script>";
   }
   echo " <div class='col-12'>  
               <div class='box'>
@@ -459,110 +408,5 @@
       overflow-x: visible;
       /* Nonaktifkan scroll horizontal di desktop */
     }
-  }
-
-  body {
-    font-family: 'Varela Round', sans-serif;
-  }
-
-  .modal-confirm {
-    color: #636363;
-    width: 400px;
-  }
-
-  .modal-confirm .modal-content {
-    padding: 20px;
-    border-radius: 5px;
-    border: none;
-    text-align: center;
-    font-size: 14px;
-  }
-
-  .modal-confirm .modal-header {
-    border-bottom: none;
-    position: relative;
-  }
-
-  .modal-confirm h4 {
-    text-align: center;
-    font-size: 26px;
-    margin: 30px 0 -10px;
-  }
-
-  .modal-confirm .close {
-    position: absolute;
-    top: -5px;
-    right: -2px;
-  }
-
-  .modal-confirm .modal-body {
-    color: #999;
-  }
-
-  .modal-confirm .modal-footer {
-    border: none;
-    text-align: center;
-    border-radius: 5px;
-    font-size: 13px;
-    padding: 10px 15px 25px;
-  }
-
-  .modal-confirm .modal-footer a {
-    color: #999;
-  }
-
-  .modal-confirm .icon-box {
-    width: 80px;
-    height: 80px;
-    margin: 0 auto;
-    border-radius: 50%;
-    z-index: 9;
-    text-align: center;
-    border: 3px solid #f15e5e;
-  }
-
-  .modal-confirm .icon-box i {
-    color: #f15e5e;
-    font-size: 46px;
-    display: inline-block;
-    margin-top: 13px;
-  }
-
-  .modal-confirm .btn,
-  .modal-confirm .btn:active {
-    color: #fff;
-    border-radius: 4px;
-    background: #60c7c1;
-    text-decoration: none;
-    transition: all 0.4s;
-    line-height: normal;
-    min-width: 120px;
-    border: none;
-    min-height: 40px;
-    border-radius: 3px;
-    margin: 0 5px;
-  }
-
-  .modal-confirm .btn-secondary {
-    background: #c1c1c1;
-  }
-
-  .modal-confirm .btn-secondary:hover,
-  .modal-confirm .btn-secondary:focus {
-    background: #a8a8a8;
-  }
-
-  .modal-confirm .btn-danger {
-    background: #f15e5e;
-  }
-
-  .modal-confirm .btn-danger:hover,
-  .modal-confirm .btn-danger:focus {
-    background: #ee3535;
-  }
-
-  .trigger-btn {
-    display: inline-block;
-    margin: 100px auto;
   }
 </style>
