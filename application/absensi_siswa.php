e77296f9f1a24882e1744554d76681cd178fe98a
kulkad <rdarmawan281@gmail.com>
2024-12-03 15:05:06 +0700

    adas

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -209,14 +209,16 @@
                         </tbody>
                     </table>
                      <a class='btn btn-success btn-sm mb-2' title='Bahan dan Tugas' href='index.php?view=forum&act=detailtopic&jdwl=$j[kodejdwl]&idtopic=$idtopic[id_forum_topic]&id_jawaban=$jawaban_refleksi[id_pertanyaan_penilaian]&id_journal=$_GET[id_journal]'>
-                    <div class='d-flex flex-column align-items-center'>
-                      <div class='glyphicon glyphicon-tasks' style='font-size:28px; margin-right:5px;'></div>
-                      <div class='' style='font-size:14px;'>Refleksi</div>
-                    </div>
-                    <div class='d-flex flex-column align-items-center'>
-                      <div class='bi bi-list-ul' style='font-size:28px; margin-right:5px;'></div>
-                      <div class='' style='font-size:14px;'>Rekap SRL</div>
-                    </div>
+                     <div class='d-flex flex-row'>  
+                          <div class='d-flex flex-column align-items-center'>
+                            <div class='glyphicon glyphicon-tasks' style='font-size:28px; margin-right:5px;'></div>
+                            <div class='' style='font-size:14px;'>Refleksi</div>
+                          </div>
+                          <div class='d-flex flex-column align-items-center'>
+                            <div class='bi bi-list-ul' style='font-size:28px; margin-right:5px;'></div>
+                            <div class='' style='font-size:14px;'>Rekap SRL</div>
+                          </div>
+                     </div>
                  </a>
                 </div>
             </div>
