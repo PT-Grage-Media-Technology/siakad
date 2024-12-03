3814a699b4643ac61168359e2517c298da9c8e13
kulkad <rdarmawan281@gmail.com>
2024-12-03 15:03:45 +0700

    ssdasdas

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -206,16 +206,16 @@
                                 <th scope='row'>Tujuan Pembelajaran</th>
                                 <td>$j[materi]</td>
                             </tr>
-                            <tr>
-                                <th scope='row'>Tujuan Pembelajaran</th>
-                                <td>$j[materi]</td>
-                            </tr>
                         </tbody>
                     </table>
                      <a class='btn btn-success btn-sm mb-2' title='Bahan dan Tugas' href='index.php?view=forum&act=detailtopic&jdwl=$j[kodejdwl]&idtopic=$idtopic[id_forum_topic]&id_jawaban=$jawaban_refleksi[id_pertanyaan_penilaian]&id_journal=$_GET[id_journal]'>
                     <div class='d-flex flex-column align-items-center'>
                       <div class='glyphicon glyphicon-tasks' style='font-size:28px; margin-right:5px;'></div>
                       <div class='' style='font-size:14px;'>Refleksi</div>
+                    </div>
+                    <div class='d-flex flex-column align-items-center'>
+                      <div class='bi bi-list-ul' style='font-size:28px; margin-right:5px;'></div>
+                      <div class='' style='font-size:14px;'>Rekap SRL</div>
                     </div>
                  </a>
                 </div>
