05fb1d3a00de3a0caab43eaa17e4586b27368bca
Baihaqi34 <abay999mmm@gmail.com>
2024-12-04 12:53:53 +0700

    sasasa

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -324,7 +324,6 @@
 
   while ($r = mysql_fetch_array($tampil)) {
     // Ambil nilai dari tabel terkait
-    
     $nilai_pengetahuan = mysql_fetch_array(mysql_query("SELECT nilai_pengetahuan FROM rb_elearning_jawab WHERE id_elearning='$data_tugas[id_elearning]' AND nisn='$r[nisn]' AND jenis_nilai='pengetahuan'"));
     $nilai_keterampilan = mysql_fetch_array(mysql_query("SELECT nilai_keterampilan FROM rb_elearning_jawab WHERE id_elearning='$data_tugas[id_elearning]' AND nisn='$r[nisn]' AND jenis_nilai='keterampilan'"));
     $nilai_sikap = mysql_fetch_array(mysql_query("SELECT nilai_sikap FROM rb_elearning_jawab WHERE id_elearning='$data_tugas[id_elearning]' AND nisn='$r[nisn]' AND jenis_nilai='sikap'"));
