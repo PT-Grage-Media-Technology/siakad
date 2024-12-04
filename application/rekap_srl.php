b2cfab940481cc3c138cdc5ad41b1769706a2e19
Baihaqi34 <abay999mmm@gmail.com>
2024-12-04 15:41:41 +0700

    sasasas

--- application/rekap_srl.php
+++ application/rekap_srl.php
@@ -75,7 +75,13 @@
             <tbody>
               <tr>
                 <td>$no</td>
-                <td>$r[nama]/td>
+                <td>$r[nama]
+                <td>
+                <td>
+              $r[nisn]
+              <input type='number' value='$r[nisn]' name='nisn[$no]' style='width:50px;' hidden>                
+                <td>
+
                 <td>90</td>
                 <td>90</td>
                 <td>90</td>
