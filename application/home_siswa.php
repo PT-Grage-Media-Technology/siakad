e2773275f75c3953287fa4af445a951be94bda88
Baihaqi34 <abay999mmm@gmail.com>
2024-12-04 11:29:51 +0700

    sasasas

--- application/home_siswa.php
+++ application/home_siswa.php
@@ -235,7 +235,7 @@
           continue; // Lewati input jika sudah ada
         }
 
-        mysql_query("INSERT INTO rb_pertanyaan_penilaian_jawab VALUES('','$pertanyaan','$_SESSION[id]','','$jawab','$_GET[kodejdwl]','refleksi','$kelas','$guru[users]','" . date('Y-m-d H:i:s') . "','$_GET[id_journal]')");
+        mysql_query("INSERT INTO rb_pertanyaan_penilaian_jawab VALUES('','$pertanyaan','$_SESSION[id]','$guru[users]','','$jawab','$_GET[kodejdwl]','refleksi','$kelas','$guru[users]','" . date('Y-m-d H:i:s') . "','$_GET[id_journal]')");
       }
     }
     echo"INSERT INTO rb_pertanyaan_penilaian_jawab VALUES('','$pertanyaan','$_SESSION[id]','$guru[users]','','$jawab','$_GET[kodejdwl]','refleksi','$kelas','" . date('Y-m-d H:i:s') . "','$_GET[id_journal]')";
