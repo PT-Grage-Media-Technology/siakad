d5087fbc01b75e27c287039717f44d2967f52f67
kulkad <rdarmawan281@gmail.com>
2024-12-05 14:22:49 +0700

    sadas

--- application/rekap_srl.php
+++ application/rekap_srl.php
@@ -83,8 +83,9 @@
               <?php
               $no = 1;
               $tampil = mysql_query("SELECT * FROM rb_siswa a JOIN rb_jenis_kelamin b ON a.id_jenis_kelamin=b.id_jenis_kelamin WHERE a.kode_kelas='$_GET[id]' ORDER BY a.id_siswa");
-              $kktp = mysql_query("SELECT kktp FROM rb_jadwal_pelajaran WHERE kodejdwl='$_GET[idjr]'");
-              while ($r = mysql_fetch_array($tampil, $kktp)) {
+              $kktp = mysql_query("SELECT * FROM rb_jadwal_pelajaran WHERE kodejdwl='$_GET[idjr]'");
+              $kk = mysql_fetch_array($kktp);
+              while ($r = mysql_fetch_array($tampil)) {
                 // var_dump($tanggalArray);
                 echo "
                 <tr>
@@ -92,7 +93,7 @@
                 <td>$r[nama]
                 <input type='number' value='$r[nisn]' name='nisn[$no]' style='width:50px;' hidden>
                 </td>
-                <td>$r[kktp]</td>";
+                <td>$kk</td>";
                 for ($i = 0; $i < $header_count; $i++) {
                   $abs = mysql_fetch_array(mysql_query("SELECT * FROM rb_absensi_siswa 
                                        WHERE kodejdwl='" . mysql_real_escape_string($_GET['idjr']) . "' 
