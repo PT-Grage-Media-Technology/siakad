197c32d46cc68548035504a1db4c2c5d2687f3a1
Baihaqi34 <abay999mmm@gmail.com>
2024-12-05 09:37:29 +0700

    sasas

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -424,7 +424,7 @@
       $cek = mysql_query("SELECT * FROM rb_absensi_siswa WHERE kodejdwl='$kodejdwl' AND nisn='" . $nisn[$i] . "' AND tanggal='$tgl'");
       $total = mysql_num_rows($cek);
 
-      $total_nilai[$i] = $nilai_keterampilan[$i] + $nilai_pengetahuan[$i] + $nilai_sikap[$i];
+      $total_nilai[$i] = ($nilai_keterampilan[$i] + $nilai_pengetahuan[$i] + $nilai_sikap[$i]) / 3;
 
 
       if ($total >= 1) {
