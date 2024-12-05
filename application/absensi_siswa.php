c48eec903f7abe38beb32c1a7e32a8c321053f65
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-12-05 16:28:49 +0700

    sdfsaf

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -356,18 +356,7 @@
                   rb_journal_list.kodejdwl = rb_absensi_siswa.kodejdwl 
                   AND rb_journal_list.tanggal = rb_absensi_siswa.tanggal
               WHERE 
-                  rb_journal_list.tujuan_pembelajaran = '$tujuan_pembelajaran'
-                  AND (
-                    rb_absensi_siswa.nilai_keterampilan IS NULL 
-                    OR rb_absensi_siswa.nilai_keterampilan = 0 
-                    OR rb_absensi_siswa.nilai_keterampilan = '' 
-                    OR rb_absensi_siswa.nilai_pengetahuan IS NULL 
-                    OR rb_absensi_siswa.nilai_pengetahuan = 0 
-                    OR rb_absensi_siswa.nilai_pengetahuan = '' 
-                    OR rb_absensi_siswa.nilai_sikap IS NULL 
-                    OR rb_absensi_siswa.nilai_sikap = 0 
-                    OR rb_absensi_siswa.nilai_sikap = ''
-                  );";
+                  rb_journal_list.tujuan_pembelajaran = '$tujuan_pembelajaran'";
 
               $jadwal = mysql_query($query);
 
