d8499f930feb3ae94f73a9d5603fc8df38e266bc
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-12-05 16:19:24 +0700

    asdfsdf

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -345,31 +345,39 @@
     //                                       AND nisn='$r[nisn]'"));
     
 
-      $tujuan_pembelajaran = mysql_real_escape_string($j['tujuan_pembelajaran']);
-  
-      // Query ke database
-      $query = "SELECT 
-                    rb_journal_list.*, 
-                    rb_absensi_siswa.* 
-                FROM 
-                    rb_journal_list 
-                JOIN 
-                    rb_absensi_siswa 
-                ON 
-                    rb_journal_list.kodejdwl = rb_absensi_siswa.kodejdwl 
-                    AND rb_journal_list.tanggal = rb_absensi_siswa.tanggal
-                WHERE 
-                    rb_journal_list.tujuan_pembelajaran = '$tujuan_pembelajaran';
-                ";
-      $jadwal = mysql_query($query);
+    $query = "SELECT 
+                  rb_journal_list.*, 
+                  rb_absensi_siswa.* 
+              FROM 
+                  rb_journal_list 
+              JOIN 
+                  rb_absensi_siswa 
+              ON 
+                  rb_journal_list.kodejdwl = rb_absensi_siswa.kodejdwl 
+                  AND rb_journal_list.tanggal = rb_absensi_siswa.tanggal
+              WHERE 
+                  rb_journal_list.tujuan_pembelajaran = '$tujuan_pembelajaran'
+                  AND (
+                      rb_absensi_siswa.nilai_keterampilan IS NULL 
+                      OR rb_absensi_siswa.nilai_keterampilan = 0 
+                      OR rb_absensi_siswa.nilai_pengetahuan IS NULL 
+                      OR rb_absensi_siswa.nilai_pengetahuan = 0 
+                      OR rb_absensi_siswa.nilai_sikap IS NULL 
+                      OR rb_absensi_siswa.nilai_sikap = 0
+                  );";
 
-      // Ambil data satu per satu
-      while ($row = mysql_fetch_assoc($jadwal)) {
-        // Menampilkan data per baris
-        echo "<pre>";
-        print_r($row);
-        echo "</pre>";
-      }
+              $jadwal = mysql_query($query);
+
+              if (!$jadwal) {
+              die("Query gagal: " . mysql_error());
+              }
+
+              // Ambil data
+              while ($row = mysql_fetch_assoc($jadwal)) {
+              echo "<pre>";
+              print_r($row);
+              echo "</pre>";
+              }
     
     $a = mysql_fetch_array(mysql_query("SELECT * FROM rb_absensi_siswa 
                                     WHERE kodejdwl='" . mysql_real_escape_string($_GET['idjr']) . "' 
