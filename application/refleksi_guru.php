61083f4d27881b7427058ba7e8aad19508d06728
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-12-05 10:48:33 +0700

    sasas

--- application/refleksi_guru.php
+++ application/refleksi_guru.php
@@ -23,7 +23,7 @@ if ($_GET[act] == '') {
                         <th>Nama Guru</th>";
   $rating_query = mysql_query("SELECT kesan FROM rb_rating ORDER BY id");
   while ($rating = mysql_fetch_array($rating_query)) {
-    echo "<th>" . $rating["kesan"] . "</th>";
+    echo "<th>" . $rating["kesan"] . " , " . $rating["id"] . "</th>";
   }
   echo "
                       </tr>
