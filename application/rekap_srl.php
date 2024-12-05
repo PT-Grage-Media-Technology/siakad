d091806b47aef8a3d90c409b67edf3b21678c8df
Baihaqi34 <abay999mmm@gmail.com>
2024-12-05 08:53:45 +0700

    sasasas

--- application/rekap_srl.php
+++ application/rekap_srl.php
@@ -1,17 +1,14 @@
 <style>
   .table-responsive {
     overflow-x: auto;
-    /* Hanya aktifkan scroll horizontal jika diperlukan */
   }
 
   @media (min-width: 768px) {
     .table-responsive {
       overflow-x: visible;
-      /* Nonaktifkan scroll horizontal di desktop */
     }
   }
 
-  /* Gaya tabel baru */
   table {
     border-collapse: collapse;
     width: 100%;
@@ -30,20 +27,23 @@
   }
 </style>
 
-<?php if ($_GET[act] == '') { ?>
+<?php if ($_GET['act'] == '') { ?>
   <div class="col-xs-12">
     <div class="box">
       <div class="box-header">
-        <h3 class="box-title"><?php if (isset($_GET[kelas]) and isset($_GET[tahun])) {
-          echo "Rekap Absensi siswa";
-        } else {
-          echo "Rekap Sumatif Ruang Lingkup " . date('Y');
-        } ?></h3>
-        <?php
-        $siswa = mysql_fetch_array(mysql_query("SELECT * FROM rb_siswa where nisn='$_GET[id]'"))
+        <h3 class="box-title">
+          <?php
+          if (isset($_GET['kelas']) and isset($_GET['tahun'])) {
+            echo "Rekap Absensi Siswa";
+          } else {
+            echo "Rekap Sumatif Ruang Lingkup " . date('Y');
+          }
           ?>
-
-      </div><!-- /.box-header -->
+        </h3>
+        <?php
+        $siswa = mysql_fetch_array(mysql_query("SELECT * FROM rb_siswa WHERE nisn='$_GET[id]'"));
+        ?>
+      </div>
       <div class="box-body">
         <div class="table-responsive">
           <table>
@@ -51,7 +51,13 @@
               <tr>
                 <th rowspan="2">No</th>
                 <th rowspan="2">Nama Siswa</th>
-                <th colspan="5">SUMATIF LINGKUP MATERI</th>
+                <?php
+                // Ambil data header dari tabel rb_journal_list
+                $headers = mysql_query("SELECT * FROM rb_journal_list where kodejdwl='$_GET[idjr]'");
+                $header_count = mysql_num_rows($headers);
+
+                echo "<th colspan='$header_count'>SUMATIF LINGKUP MATERI</th>";
+                ?>
                 <th rowspan="2">NA SUMATIF (S)</th>
                 <th rowspan="2">STS</th>
                 <th rowspan="2">NON TES</th>
@@ -59,45 +65,42 @@
                 <th rowspan="2">Nilai Rapor<br>(Rerata S + AS)</th>
               </tr>
               <tr>
-                <th>Proses perumusan pancasila</th>
-                <th>Proses perumusan pancasila</th>
-                <th>Proses perumusan pancasila</th>
-                <th>Proses perumusan pancasila</th>
-                <th>Proses perumusan pancasila</th>
+                <?php
+                // Loop untuk menampilkan header dinamis
+                while ($header = mysql_fetch_array($headers)) {
+                  echo "<th>{$header['column_name']}</th>"; // Ganti 'column_name' dengan nama kolom header yang relevan
+                }
+                ?>
               </tr>
             </thead>
             <tbody>
               <?php
               $no = 1;
-              $tampil = mysql_query("SELECT * FROM rb_siswa a JOIN rb_jenis_kelamin b ON a.id_jenis_kelamin=b.id_jenis_kelamin where a.kode_kelas='$_GET[id]' ORDER BY a.id_siswa");
+              $tampil = mysql_query("SELECT * FROM rb_siswa a JOIN rb_jenis_kelamin b ON a.id_jenis_kelamin=b.id_jenis_kelamin WHERE a.kode_kelas='$_GET[id]' ORDER BY a.id_siswa");
               while ($r = mysql_fetch_array($tampil)) {
                 echo "
-      <tr>
-        <td>$no</td>
-        <td>$r[nama]
-              <input type='number' value='$r[nisn]' name='nisn[$no]' style='width:50px;' hidden>
-              </td>
-        <td>90</td>
-        <td>90</td>
-        <td>90</td>
-        <td>90</td>
-        <td>90</td>
-        <td>80</td>
-        <td>88</td>
-        <td>95</td>
-        <td>90</td>
-        <td>90</td>
-      </tr>";
+                  <tr>
+                    <td>$no</td>
+                    <td>$r[nama]
+                      <input type='number' value='$r[nisn]' name='nisn[$no]' style='width:50px;' hidden>
+                    </td>";
+                for ($i = 0; $i < $header_count; $i++) {
+                  echo "<td>90</td>";
+                }
+                echo "
+                    <td>80</td>
+                    <td>88</td>
+                    <td>95</td>
+                    <td>90</td>
+                    <td>90</td>
+                  </tr>";
                 $no++;
               }
               ?>
             </tbody>
           </table>
-
         </div>
-      </div><!-- /.box-body -->
+      </div>
     </div>
   </div>
-  <?php
-}
-?>
+<?php } ?>
