5d22c3347a9eeeacbdd812defa1c4d0c617cefae
Baihaqi34 <abay999mmm@gmail.com>
2024-12-06 08:47:29 +0700

    sasas

--- application/raport/raport_nilai_pengetahuan.php
+++ application/raport/raport_nilai_pengetahuan.php
@@ -53,7 +53,7 @@
                                 $nilaiAkhir = 0.5*$nilaiUH['nilai']+0.2*$nilaiSTS['angka_pengetahuan']+0.3*$nilaiSAS['nilai'];
 
                                 echo"SELECT * FROM rb_rekap_srl WHERE kodejdwl='$_GET[jdwl]' AND nisn='$r[nisn]'";
-                                var_dump($nilaiUH['nilai']);
+                                var_dump($nilaiUH);
                                   if (isset($_GET['edit_pengetahuan'])){
                                       $e = mysql_fetch_array(mysql_query("SELECT * FROM rb_nilai_pengetahuan where id_nilai_pengetahuan='$_GET[edit_pengetahuan]'"));
                                       $name = 'Update';
