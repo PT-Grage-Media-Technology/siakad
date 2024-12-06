a427da6a7c83c376ccb5abd9e51f8c8a1a674ab2
Baihaqi34 <abay999mmm@gmail.com>
2024-12-06 14:50:44 +0700

    c

--- application/rekap_srl.php
+++ application/rekap_srl.php
@@ -239,7 +239,11 @@
                                        AND tanggal='" . mysql_real_escape_string($tanggalArray[$i]) . "' ORDER BY tanggal ASC"));
                   $totalAbsensi += (isset($abs['total']) ? $abs['total'] : 0); // Tambahkan absensi
                   $nilaiArray[] = isset($abs['total']) ? $abs['total'] : 0; // Simpan nilai absensi ke dalam array
-                  echo "<td>" . (isset($abs['total']) ? $abs['total'] : 0) . "</td>";
+                  if($header_count < 1){
+                    echo "<td>" . (isset($abs['total']) ? $abs['total'] : 0) . "</td>";
+                  }else{
+                    echo"<td>no data</td>";
+                  }
                 }
                 $maxIndex = array_search(max($nilaiArray), $nilaiArray); 
                 echo "<td class='nilai-max'>";
