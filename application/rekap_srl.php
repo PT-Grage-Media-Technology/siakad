089dd81d6547ccf8db00d58693a1879b693c4138
Baihaqi34 <abay999mmm@gmail.com>
2024-12-06 09:24:55 +0700

    sasas

--- application/rekap_srl.php
+++ application/rekap_srl.php
@@ -95,7 +95,7 @@
                   $nilaiArray[] = isset($abs['total']) ? $abs['total'] : 0; // Simpan nilai absensi ke dalam array
                   echo "<td>" . (isset($abs['total']) ? $abs['total'] : 0) . "</td>";
                 }
-                echo"<td>".max($abs['total'])."</td>";
+                echo"<td>".max($nilaiArray)."</td>";
                 // Hitung rata-rata
                 echo "<td>";
                 if ($header_count > 0) {
