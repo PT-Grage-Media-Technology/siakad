3f315fe11532f5ad9dff98603fd40e9be0e24997
Baihaqi34 <abay999mmm@gmail.com>
2024-12-06 12:59:43 +0700

    sasas

--- application/rekap_srl.php
+++ application/rekap_srl.php
@@ -54,12 +54,9 @@
                 <?php
                 // Ambil data header dari tabel rb_journal_list
                 $headers = mysql_query("SELECT * FROM rb_journal_list where kodejdwl='$_GET[idjr]' AND id_parent_journal IS NULL ORDER BY tanggal ASC");
-                $tanggalArray = []; // Array untuk menyimpan tanggal
-                while ($header = mysql_fetch_array($headers)) {
-                  $tanggalArray[] = $header['tanggal']; // Simpan tanggal
-                  echo "<th>{$header['tujuan_pembelajaran']}</th>";
-                }
-                $header_count = count($tanggalArray); // Hitung jumlah header
+                $header_count = mysql_num_rows($headers);
+
+                echo "<th colspan='$header_count'>SUMATIF LINGKUP MATERI</th>";
                 ?>
                 <th rowspan="2">NA SUMATIF (S)</th>
                 <th rowspan="2">STS</th>
@@ -67,49 +64,47 @@
                 <th rowspan="2">NA SUMATIF AKHIR SEMESTER (AS)</th>
                 <th rowspan="2">Nilai Rapor<br>(Rerata S + AS)</th>
               </tr>
+              <tr>
+                <?php
+                // Loop untuk menampilkan header dinamis
+                while ($header = mysql_fetch_array($headers)) {
+                  // echo"SELECT * FROM rb_journal_list where kodejdwl='$_GET[idjr]' AND id_parent_journal IS NULL";
+                  var_dump($header);
+                  $tanggalArray[] = $header['tanggal'];
+                  $headerCount = count($tanggalArray);
+                  echo $headerCount;
+                  echo "<th>{$header['tujuan_pembelajaran']}</th>"; // Ganti 'column_name' dengan nama kolom header yang relevan
+                }
+                ?>
+              </tr>
             </thead>
             <tbody>
               <?php
               $no = 1;
               $tampil = mysql_query("SELECT * FROM rb_siswa a JOIN rb_jenis_kelamin b ON a.id_jenis_kelamin=b.id_jenis_kelamin WHERE a.kode_kelas='$_GET[id]' ORDER BY a.id_siswa");
               while ($r = mysql_fetch_array($tampil)) {
-                $total_nilai = 0; // Variabel untuk menjumlahkan nilai
+                // var_dump($tanggalArray);
                 echo "
                 <tr>
                 <td>$no</td>
                 <td>$r[nama]
                 <input type='number' value='$r[nisn]' name='nisn[$no]' style='width:50px;' hidden>
                 </td>";
-
-                // Loop untuk setiap header/tanggal
                 for ($i = 0; $i < $header_count; $i++) {
                   $abs = mysql_fetch_array(mysql_query("SELECT * FROM rb_absensi_siswa 
                                        WHERE kodejdwl='" . mysql_real_escape_string($_GET['idjr']) . "' 
                                        AND nisn='" . mysql_real_escape_string($r['nisn']) . "' 
                                        AND tanggal='" . mysql_real_escape_string($tanggalArray[$i]) . "' ORDER BY tanggal ASC"));
 
-                  $nilai = $abs ? $abs['total'] : 0; // Gunakan 0 jika tidak ada nilai
-                  $total_nilai += $nilai; // Tambahkan nilai ke total
-                  echo "<td>$nilai</td>";
+                  echo "<td>$abs[total]</td>";
                 }
-
-                // Hitung NA SUMATIF (S)
-                $na_sumatif = $total_nilai / $header_count;
-
-                // Contoh nilai STS, NON TES, dan NA SUMATIF AKHIR SEMESTER
-                $sts = 88;
-                $non_tes = 95;
-                $na_sumatif_akhir = 90;
-
-                // Hitung Nilai Rapor
-                $nilai_rapor = ($na_sumatif + $na_sumatif_akhir) / 2;
-
+                
                 echo "
-                    <td>" . round($na_sumatif, 2) . "</td>
-                    <td>$sts</td>
-                    <td>$non_tes</td>
-                    <td>$na_sumatif_akhir</td>
-                    <td>" . round($nilai_rapor, 2) . "</td>
+                    <td>";echo $abs['total'] / 2; echo"</td> 
+                    <td>88</td>
+                    <td>95</td>
+                    <td>90</td>
+                    <td>90</td>
                   </tr>";
                 $no++;
               }
