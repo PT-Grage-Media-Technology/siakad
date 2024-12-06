086d12b9d379603a046c34b9cb9945a6b72f2ff9
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-12-06 10:48:28 +0700

    sdfdsf

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -369,19 +369,19 @@
             // echo "$absen[nisn] : $absen[nilai_keterampilan], ";
 
             // Hitung data nilai_keterampilan yang 0, NULL, atau ''
-            // echo "nilai_keterampilan kosong atau $absen[nilai_keterampilan]\n";
+            echo "nilai_keterampilan kosong atau $absen[nilai_keterampilan]\n";
             if (empty($absen['nilai_keterampilan']) || $absen['nilai_keterampilan'] == 0) {
               $keterampilan_kosong++;
             }
             
             // Cek nilai_pengetahuan
-            // echo "nilai_pengetahuan kosong atau $absen[nilai_pengetahuan]\]\n";
+            echo "nilai_pengetahuan kosong atau $absen[nilai_pengetahuan]\]\n";
             if (empty($absen['nilai_pengetahuan']) || $absen['nilai_pengetahuan'] == 0) {
                 $pengetahuan_kosong++;
             }
         
             // Cek nilai_sikap
-            // echo "nilai_sikap kosong atau $absen[nilai_sikap]\n";
+            echo "nilai_sikap kosong atau $absen[nilai_sikap]\n";
             if (empty($absen['nilai_sikap']) || $absen['nilai_sikap'] == 0) {
                 $sikap_kosong++;
             }
@@ -514,7 +514,7 @@
                 nilai_pengetahuan='" . (isset($nilai_pengetahuanInput[$i]) ? mysql_real_escape_string($nilai_pengetahuanInput[$i]) : 0) . "',
                 nilai_keterampilan='" . (isset($nilai_keterampilanInput[$i]) ? mysql_real_escape_string($nilai_keterampilanInput[$i]) : 0) . "', 
                 total='" . mysql_real_escape_string(round($total_nilai[$i])) . "' 
-            WHERE nisn='" . $nisn[$i] . "' 
+              WHERE nisn='" . $nisn[$i] . "' 
               AND kodejdwl='" . $kodejdwl . "'
               AND tanggal='" . $tgl . "'"
         );
