f00f1427d4745e352a656a42ba963bacb9dfd298
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-12-06 15:33:22 +0700

    asdsda

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -651,7 +651,7 @@
                 // Jalankan query
                 $updateAbsensiSiswaParent = mysql_query($updateQuery);
         
-                var_dump($dataParentQuery);
+                var_dump($j['id_parent_journal']);
                 exit;
                 // Cek keberhasilan query
                 if ($updateAbsensiSiswaParent) {
