fb3e044937ca5190417f2811cb900431ffd9f2ce
kulkad <rdarmawan281@gmail.com>
2024-12-09 08:41:31 +0700

    ssss

--- print_raport/print-hal4.php
+++ print_raport/print-hal4.php
@@ -1,41 +1,47 @@
-<?php 
+<?php
 session_start();
 error_reporting(0);
-include "../config/koneksi.php"; 
-include "../config/fungsi_indotgl.php"; 
-$frt = mysql_fetch_array(mysql_query("SELECT * FROM rb_header_print ORDER BY id_header_print DESC LIMIT 1")); 
+include "../config/koneksi.php";
+include "../config/fungsi_indotgl.php";
+$frt = mysql_fetch_array(mysql_query("SELECT * FROM rb_header_print ORDER BY id_header_print DESC LIMIT 1"));
 ?>
+
 <head>
-<title>Hal 4 - Raport Siswa</title>
-<script>
-        function handlePrint() {
-            // Membuka dialog cetak
-            window.print();
-            
-            // Setelah dialog cetak ditutup, kembali ke halaman sebelumnya
-            setTimeout(() => {
-                window.close();
-            }, 500); // Tambahkan sedikit jeda untuk memastikan dialog selesai ditutup
-        }
-    </script>
-<link rel="stylesheet" href="../bootstrap/css/printer.css">
+  <title>Hal 4 - Raport Siswa</title>
+  <script>
+    function handlePrint() {
+      // Membuka dialog cetak
+      window.print();
+
+      // Setelah dialog cetak ditutup, kembali ke halaman sebelumnya
+      setTimeout(() => {
+        window.close();
+      }, 500); // Tambahkan sedikit jeda untuk memastikan dialog selesai ditutup
+    }
+  </script>
+  <link rel="stylesheet" href="../bootstrap/css/printer.css">
 </head>
+
 <body onload="handlePrint()">
-<?php
-$t = mysql_fetch_array(mysql_query("SELECT * FROM rb_tahun_akademik where id_tahun_akademik='$_GET[tahun]'"));
-$s = mysql_fetch_array(mysql_query("SELECT a.*, b.*, c.nama_guru as walikelas, c.nip FROM rb_siswa a 
+  <?php
+  $t = mysql_fetch_array(mysql_query("SELECT * FROM rb_tahun_akademik where id_tahun_akademik='$_GET[tahun]'"));
+  $s = mysql_fetch_array(mysql_query("SELECT a.*, b.*, c.nama_guru as walikelas, c.nip FROM rb_siswa a 
                                       JOIN rb_kelas b ON a.kode_kelas=b.kode_kelas 
                                         LEFT JOIN rb_guru c ON b.nip=c.nip where a.nisn='$_GET[id]'"));
-if (substr($_GET[tahun],4,5)=='1'){ $semester = 'Ganjil'; }else{ $semester = 'Genap'; }
-$iden = mysql_fetch_array(mysql_query("SELECT * FROM rb_identitas_sekolah ORDER BY id_identitas_sekolah DESC LIMIT 1"));
-echo "<table width=100%>
+  if (substr($_GET[tahun], 4, 5) == '1') {
+    $semester = 'Ganjil';
+  } else {
+    $semester = 'Genap';
+  }
+  $iden = mysql_fetch_array(mysql_query("SELECT * FROM rb_identitas_sekolah ORDER BY id_identitas_sekolah DESC LIMIT 1"));
+  echo "<table width=100%>
         <tr><td width=140px>Nama Sekolah</td> <td> : $iden[nama_sekolah] </td>       <td width=140px>Kelas </td>   <td>: $s[kode_kelas]</td></tr>
         <tr><td>Alamat</td>                   <td> : $iden[alamat_sekolah] </td>     <td>Semester </td> <td>: $semester</td></tr>
         <tr><td>Nama Peserta Didik</td>       <td> : <b>$s[nama]</b> </td>           <td>Tahun Pelajaran </td> <td>: $t[keterangan]</td></tr>
         <tr><td>No Induk/NISN</td>            <td> : $s[nipd] / $s[nisn]</td>        <td></td></tr>
       </table><br>";
 
-echo "<table id='tablemodul1' width='100%' border='1'>
+  echo "<table id='tablemodul1' width='100%' border='1'>
           <thead>
           <tr>
             <th width='40px' rowspan='2' style='text-align:center;'>No</th>
@@ -46,31 +52,31 @@ echo "<table id='tablemodul1' width='100%' border='1'>
           </tr>
           </thead>
           <tbody>";
-          
-$kelompok = mysql_query("SELECT * FROM rb_kelompok_mata_pelajaran");  
+
+  $kelompok = mysql_query("SELECT * FROM rb_kelompok_mata_pelajaran");
 
 
 
-while ($k = mysql_fetch_array($kelompok)){
-  echo "<tr>
+  while ($k = mysql_fetch_array($kelompok)) {
+    echo "<tr>
   <td colspan='6'><b>$k[nama_kelompok_mata_pelajaran]</b></td>
   </tr>";
-  $mapel = mysql_query("SELECT * FROM rb_jadwal_pelajaran a 
+    $mapel = mysql_query("SELECT * FROM rb_jadwal_pelajaran a 
                           JOIN rb_mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran 
                           WHERE a.kode_kelas='$_GET[kelas]' 
                           AND a.id_tahun_akademik='$_GET[tahun]' 
                           AND b.id_kelompok_mata_pelajaran='$k[id_kelompok_mata_pelajaran]'");
     $no = 1;
-    while ($m = mysql_fetch_array($mapel)) {      
+    while ($m = mysql_fetch_array($mapel)) {
       $rapot = mysql_fetch_array(mysql_query("SELECT * FROM rb_nilai_pengetahuan WHERE kodejdwl='$m[kodejdwl]' AND nisn='$s[nisn]'"));
       $rapn = mysql_fetch_array(mysql_query("SELECT SUM((nilai1+nilai2+nilai3+nilai4+nilai5)/5)/COUNT(nisn) AS raport FROM rb_nilai_pengetahuan WHERE kodejdwl='$m[kodejdwl]' AND nisn='$s[nisn]'"));
-        $rapnk = mysql_fetch_array(mysql_query("SELECT SUM(GREATEST(nilai1,nilai2,nilai3,nilai4,nilai5,nilai6))/COUNT(nisn) AS raport FROM rb_nilai_keterampilan WHERE kodejdwl='$m[kodejdwl]' AND nisn='$s[nisn]'"));
-        
-        echo "<tr>
+      $rapnk = mysql_fetch_array(mysql_query("SELECT SUM(GREATEST(nilai1,nilai2,nilai3,nilai4,nilai5,nilai6))/COUNT(nisn) AS raport FROM rb_nilai_keterampilan WHERE kodejdwl='$m[kodejdwl]' AND nisn='$s[nisn]'"));
+
+      echo "<tr>
                 <td rowspan='2' align='center'>$no</td>
                 <td colspan='2' rowspan='2'>$m[namamatapelajaran]</td>
                 <td rowspan='2' align='center' style='color: " . ($rapn['raport'] < $m['kkm'] ? 'red' : 'black') . ";'>
-                    " . number_format($rapn['raport']) . "
+                    " . number_format($rapot['nilai_akhir']) . "
                 </td>
                 
                     <td align='center'>$rapot[deskripsi_tertinggi]</td>
@@ -79,15 +85,15 @@ while ($k = mysql_fetch_array($kelompok)){
                     <td align='center'>$rapot[deskripsi_terendah]</td>
                     </tr>
               </tr>";
-        $no++;
+      $no++;
     }
-}
-echo "</tbody></table><br/>";
-?>
+  }
+  echo "</tbody></table><br/>";
+  ?>
 
-      
-<!-- tanda tangan ada di raport halaman teralkhir -->
-<!-- <table border=0 width=100%>
+
+  <!-- tanda tangan ada di raport halaman teralkhir -->
+  <!-- <table border=0 width=100%>
   <tr>
     <td width="260" align="left">Orang Tua / Wali</td>
     <td width="520"align="center">Mengetahui <br> Kepala SMA Negeri 1 Padang</td>
