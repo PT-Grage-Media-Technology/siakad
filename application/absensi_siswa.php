36785f4a8d448e2a325a5f64d712abcf7c44d258
kulkad <rdarmawan281@gmail.com>
2024-12-10 09:34:55 +0700

    dsa

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -294,7 +294,7 @@
                     </div>
                  </a>
                  </a>
-                      <a class='btn btn-success btn-sm mb-2' title='Remadial' href='#'>
+                 <a class='btn btn-success btn-sm mb-2' title='Remadial' href='#' hidden>
                     <div class='d-flex flex-column align-items-center'>
                       <div class='glyphicon glyphicon-tasks' style='font-size:28px; margin-right:5px;'></div>
                       <div class='' style='font-size:14px;'>Remadial</div>
