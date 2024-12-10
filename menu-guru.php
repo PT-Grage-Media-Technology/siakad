b37f4ea354a9a589181bee60165fbaacce4431f3
Baihaqi34 <abay999mmm@gmail.com>
2024-12-10 14:21:21 +0700

    sasasa

--- menu-guru.php
+++ menu-guru.php
@@ -47,7 +47,7 @@
       <ul class='treeview-menu'>
         <li><a href='index.php?view=jadwalguru' class='" . ($_GET['view'] == 'jadwalguru' ? 'active' : '') . "'>Aktivitas Mengajar</a></li>";
         $mapel = mysql_fetch_array(mysql_query("SELECT * FROM rb_jadwal_pelajaran WHERE kodejdwl=$_GET[id]"));
-    if (isset($_GET['act']) && $_GET['act'] === 'lihat' && isset($_GET['id']) && isset($_GET['tahun']) ) {
+    if (isset($_GET['act']) && $_GET['act'] === 'lihat' && $_GET['act'] === 'listsiswa' && isset($_GET['id']) && isset($_GET['tahun']) ) {
       echo "
       <li><a href='index.php?view=raportuts&act=listsiswa&jdwl={$_GET['id']}&kd={$mapel[kode_pelajaran]}&id={$mapel[kode_kelas]}&tahun={$_GET['tahun']}'>Nilai STS</a></li>
       <li><a href='index.php?view=raportsas&act=listsiswa&jdwl={$_GET['id']}&kd={$mapel[kode_pelajaran]}&id={$mapel[kode_kelas]}&tahun={$_GET['tahun']}'>Nilai SAS</a></li>
