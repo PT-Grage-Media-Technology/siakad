7e3b2ba9d34a3d86d058e13996d5b361a61dc27e
Baihaqi34 <abay999mmm@gmail.com>
2024-12-10 09:13:37 +0700

    sasas

--- application/master_guru.php
+++ application/master_guru.php
@@ -243,7 +243,7 @@
     var_dump($id_waka);
     var_dump($id_mapel);
     echo '</pre>';
-    exit;
+    // exit;
     
     if($_POST['as'] == $id_waka['id_jenis_ptk'] ){
       // var_dump($_POST['as']);
@@ -251,7 +251,7 @@
       $nip = $_POST['aa'] . rand(10, 99);
 
       mysql_query("INSERT INTO rb_guru VALUES('$nip','$_POST[ab]','$_POST[ac]','$_POST[af]','$_POST[ad]',
-      '$_POST[ae]','$_POST[ba]','$_POST[bv]','$_POST[aq]','$_POST[au]','$_POST[as]','$_POST[ar]', 
+      '$_POST[ae]','$_POST[ba]','$_POST[bv]','$_POST[aq]','$_POST[au]','$id_mapel','$_POST[ar]', 
       '$_POST[ag]','$_POST[ak]','$rt','$rw','$_POST[am]','$_POST[an]','$_POST[ao]','$_POST[ap]',
       '$_POST[ai]','$_POST[ah]','$_POST[aj]','$_POST[at]','$_POST[av]','$_POST[bb]','$_POST[bc]', 
       '$_POST[bd]','$_POST[be]','$_POST[bf]','$_POST[bg]','$_POST[bi]','$_POST[bh]','$_POST[bj]',
