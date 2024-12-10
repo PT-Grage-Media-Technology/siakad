6e76f58521e6b549913284280c37239db7e199f9
Baihaqi34 <abay999mmm@gmail.com>
2024-12-10 14:43:25 +0700

    sasass

--- menu-guru.php
+++ menu-guru.php
@@ -53,7 +53,7 @@
           isset($_GET['tahun']) &&
           ($_GET['view'] == 'journalguru' || $_GET['view'] == 'raportuts' || $_GET['view'] == 'raportsas' || $_GET['view'] == 'raport')
       ) {
-        $idjr = $_GET['id'];
+        $idjr = $mapel['kodejdwl'];
       echo "
       <li><a href='index.php?view=raportuts&act=listsiswa&jdwl={$idjr}&kd={$mapel[kode_pelajaran]}&id={$mapel[kode_kelas]}&tahun={$_GET['tahun']}'>Nilai STS</a></li>
       <li><a href='index.php?view=raportsas&act=listsiswa&jdwl={$idjr}&kd={$mapel[kode_pelajaran]}&id={$mapel[kode_kelas]}&tahun={$_GET['tahun']}'>Nilai SAS</a></li>
