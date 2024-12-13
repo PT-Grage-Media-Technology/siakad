e83c348705b047eaa782d55c4ffae55ed2291cf0
Baihaqi34 <abay999mmm@gmail.com>
2024-12-13 14:09:27 +0700

    sasas

--- application/export_to_excel.php
+++ application/export_to_excel.php
@@ -1,18 +1,1 @@
-<?php
-// Sertakan pustaka SimpleXLSXGen
-require_once 'config/SimpleXLSXGen.php'; // Sesuaikan dengan path di proyek Anda
-
-// Data untuk diekspor
-$data = [
-    ['ID', 'Nama', 'Email'], // Header
-    [1, 'John Doe', 'john@example.com'],
-    [2, 'Jane Doe', 'jane@example.com'],
-    [3, 'Mark Smith', 'mark@example.com']
-];
-
-// Buat file Excel
-$xlsx = SimpleXLSXGen::fromArray($data);
-
-// Unduh file
-$xlsx->downloadAs('data-export.xlsx'); // Nama file yang akan diunduh
-?>
+sasas
