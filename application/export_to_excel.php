638f74da0195878aa8d73feab5665b7a17a6eb22
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-12-13 15:35:00 +0700

    gfsdgdsf

--- application/export_to_excel.php
+++ application/export_to_excel.php
@@ -1,29 +1,3 @@
 <?php
-// Sertakan file SimpleCSV yang telah diunduh
-include 'config/SimpleCSV.php'; // Ganti dengan path yang sesuai
-
-// Data yang akan diekspor ke CSV
-$data = [
-    ['Nama', 'Email', 'Telepon'],
-    ['John Doe', 'john@example.com', '08123456789'],
-    ['Jane Doe', 'jane@example.com', '08198765432']
-];
-
-// Membuat objek SimpleCSV
-$csv = new SimpleCSV();
-
-// Menambahkan data ke file CSV
-$csv->addRows($data);
-
-// Memberikan nama file CSV
-$fileName = "data_export_" . date("Y-m-d") . ".csv";
-
-// Set header untuk mengunduh file CSV
-header('Content-Type: text/csv');
-header('Content-Disposition: attachment;filename="' . $fileName . '"');
-header('Cache-Control: max-age=0');
-
-// Menulis dan mengunduh file CSV
-$csv->save('php://output');
-exit;
+var_dump('coba');
 ?>
