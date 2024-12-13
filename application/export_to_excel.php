751b0da523c88ba573242eb3bd69fb187d4579e1
Baihaqi34 <abay999mmm@gmail.com>
2024-12-13 14:15:35 +0700

    sasa

--- application/export_to_excel.php
+++ application/export_to_excel.php
@@ -1,16 +1,1 @@
-<?php
-// Sertakan pustaka SimpleXLSXGen
-// Data untuk diekspor
-$data = [
-    ['ID', 'Nama', 'Email'], // Header
-    [1, 'John Doe', 'john@example.com'],
-    [2, 'Jane Doe', 'jane@example.com'],
-    [3, 'Mark Smith', 'mark@example.com']
-];
-
-// Buat file Excel
-$xlsx = SimpleXLSXGen::fromArray($data);
-
-// Unduh file
-$xlsx->downloadAs('data-export.xlsx'); // Nama file yang akan diunduh
-?>
+adsdsd
