4628ec86691d50a85b8d772eb29fb19167d4bad3
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-12-13 10:47:54 +0700

    fghjk

--- application/export_to_excel.php
+++ application/export_to_excel.php
@@ -1,85 +1,80 @@
 <?php
-// Koneksi database
-$connection = new mysqli('localhost', 'username', 'password', 'database');
-if ($connection->connect_error) {
-    die("Koneksi gagal: " . $connection->connect_error);
-}
+// Include PhpSpreadsheet library
+require 'vendor/autoload.php'; // Pastikan path ini sesuai dengan lokasi PhpSpreadsheet di proyek Anda
 
-// Validasi input
-$tahun = $connection->real_escape_string($_GET['tahun']);
-$kd = $connection->real_escape_string($_GET['kd']);
-$id = $connection->real_escape_string($_GET['id']);
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
-// Fetch data kelas dan mata pelajaran
-$d = $connection->query("SELECT * FROM rb_kelas WHERE kode_kelas='$id'")->fetch_assoc();
-$m = $connection->query("SELECT * FROM rb_mata_pelajaran WHERE kode_pelajaran='$kd'")->fetch_assoc();
+// Cek apakah parameter GET sudah diterima
+if (isset($_GET['tahun'], $_GET['kd'], $_GET['id'])) {
+    // Ambil data berdasarkan parameter GET
+    $tahun = $_GET['tahun'];
+    $kd = $_GET['kd'];
+    $id = $_GET['id'];
 
-// Header file Excel
-header('Content-Type: application/vnd.ms-excel');
-header("Content-Disposition: attachment; filename=\"Rekap_Absensi_{$d['nama_kelas']}_{$m['namamatapelajaran']}_$tahun.xls\"");
-header('Pragma: no-cache');
-header('Expires: 0');
+    // Query untuk mendapatkan data absensi
+    $tampil = mysql_query("SELECT * FROM rb_siswa a 
+                            JOIN rb_jenis_kelamin b ON a.id_jenis_kelamin = b.id_jenis_kelamin 
+                            WHERE a.kode_kelas = '$id' 
+                            ORDER BY a.id_siswa");
 
-// Output header tabel
-echo "<table border='1'>
-        <tr>
-            <th>No</th>
-            <th>NISN</th>
-            <th>Nama Siswa</th>
-            <th>Jenis Kelamin</th>
-            <th>Pertemuan</th>
-            <th>Hadir</th>
-            <th>Sakit</th>
-            <th>Izin</th>
-            <th>Alpa</th>
-            <th>% Kehadiran</th>
-            <th>Tanggal Absen</th>
-        </tr>";
+    // Membuat objek spreadsheet baru
+    $spreadsheet = new Spreadsheet();
+    $sheet = $spreadsheet->getActiveSheet();
+    $sheet->setTitle('Absensi Siswa');
 
-// Fetch data siswa
-$tampil = $connection->query("SELECT a.nisn, a.nama, b.jenis_kelamin 
-                              FROM rb_siswa a
-                              JOIN rb_jenis_kelamin b ON a.id_jenis_kelamin=b.id_jenis_kelamin
-                              WHERE a.kode_kelas='$id'
-                              ORDER BY a.id_siswa");
+    // Menambahkan header kolom
+    $sheet->setCellValue('A1', 'No');
+    $sheet->setCellValue('B1', 'NISN');
+    $sheet->setCellValue('C1', 'Nama Siswa');
+    $sheet->setCellValue('D1', 'Jenis Kelamin');
+    $sheet->setCellValue('E1', 'Pertemuan');
+    $sheet->setCellValue('F1', 'Hadir');
+    $sheet->setCellValue('G1', 'Sakit');
+    $sheet->setCellValue('H1', 'Izin');
+    $sheet->setCellValue('I1', 'Alpa');
+    $sheet->setCellValue('J1', '% Kehadiran');
 
-$no = 1;
-while ($r = $tampil->fetch_assoc()) {
-    $total = $connection->query("SELECT DISTINCT tanggal FROM rb_absensi_siswa WHERE kodejdwl='$kd'")->num_rows;
-    $hadir = $connection->query("SELECT * FROM rb_absensi_siswa WHERE kodejdwl='$kd' AND nisn='{$r['nisn']}' AND kode_kehadiran='H'")->num_rows;
-    $sakit = $connection->query("SELECT * FROM rb_absensi_siswa WHERE kodejdwl='$kd' AND nisn='{$r['nisn']}' AND kode_kehadiran='S'")->num_rows;
-    $izin = $connection->query("SELECT * FROM rb_absensi_siswa WHERE kodejdwl='$kd' AND nisn='{$r['nisn']}' AND kode_kehadiran='I'")->num_rows;
-    $alpa = $connection->query("SELECT * FROM rb_absensi_siswa WHERE kodejdwl='$kd' AND nisn='{$r['nisn']}' AND kode_kehadiran='A'")->num_rows;
+    // Menambahkan data absensi ke dalam sheet
+    $rowNum = 2; // Dimulai dari baris kedua (baris pertama adalah header)
+    $no = 1;
+    while ($r = mysql_fetch_array($tampil)) {
+        // Query untuk menghitung absensi
+        $total = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` WHERE kodejdwl='$kd' GROUP BY tanggal"));
+        $hadir = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` WHERE kodejdwl='$kd' AND nisn='$r[nisn]' AND kode_kehadiran='H'"));
+        $sakit = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` WHERE kodejdwl='$kd' AND nisn='$r[nisn]' AND kode_kehadiran='S'"));
+        $izin = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` WHERE kodejdwl='$kd' AND nisn='$r[nisn]' AND kode_kehadiran='I'"));
+        $alpa = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` WHERE kodejdwl='$kd' AND nisn='$r[nisn]' AND kode_kehadiran='A'"));
 
-    // Ambil semua tanggal absen
-    $absence_dates = $connection->query("SELECT DISTINCT tanggal FROM rb_absensi_siswa WHERE kodejdwl='$kd' AND nisn='{$r['nisn']}'");
-    $dates_array = [];
-    while ($absence_record = $absence_dates->fetch_assoc()) {
-        $dates_array[] = $absence_record['tanggal'];
+        // Hitung persentase kehadiran
+        $persen = ($total > 0) ? ($hadir / $total) * 100 : 0;
+
+        // Menambahkan data ke dalam baris
+        $sheet->setCellValue("A$rowNum", $no);
+        $sheet->setCellValue("B$rowNum", $r['nisn']);
+        $sheet->setCellValue("C$rowNum", $r['nama']);
+        $sheet->setCellValue("D$rowNum", $r['jenis_kelamin']);
+        $sheet->setCellValue("E$rowNum", $total);
+        $sheet->setCellValue("F$rowNum", $hadir);
+        $sheet->setCellValue("G$rowNum", $sakit);
+        $sheet->setCellValue("H$rowNum", $izin);
+        $sheet->setCellValue("I$rowNum", $alpa);
+        $sheet->setCellValue("J$rowNum", number_format($persen, 2) . " %");
+
+        $rowNum++;
+        $no++;
     }
-    $tanggal_absen = implode(", ", $dates_array);
 
-    $persen = ($total > 0) ? number_format(($hadir / $total) * 100, 2) : 0;
+    // Menulis file Excel
+    $writer = new Xlsx($spreadsheet);
 
-    echo "<tr>
-            <td>$no</td>
-            <td>{$r['nisn']}</td>
-            <td>{$r['nama']}</td>
-            <td>{$r['jenis_kelamin']}</td>
-            <td align='center'>$total</td>
-            <td align='center'>$hadir</td>
-            <td align='center'>$sakit</td>
-            <td align='center'>$izin</td>
-            <td align='center'>$alpa</td>
-            <td align='right'>{$persen} %</td>
-            <td align='center'>$tanggal_absen</td>
-          </tr>";
-    $no++;
+    // Mengatur header untuk download file
+    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    header('Content-Disposition: attachment;filename="Rekap_Absensi_Siswa_' . $tahun . '.xlsx"');
+    header('Cache-Control: max-age=0');
+
+    // Menyimpan dan mengunduh file Excel
+    $writer->save('php://output');
+    exit;
 }
-
-echo "</table>";
-
-// Tutup koneksi
-$connection->close();
-exit();
 ?>
