d8150fc31caf494d7614136a202601f92d6dd6d4
jipyy <jahdanpandita16@gmail.com>
2024-12-13 09:26:45 +0700

    hh

--- application/absensi_siswa.php
+++ application/absensi_siswa.php
@@ -494,7 +494,6 @@
   if ($_SESSION['level'] != 'kepala') {
     $tglAbsen = $_GET['tgl'];
     $isDisabled = (strtotime(date('Y-m-d')) > strtotime($tglAbsen)) ? 'hidden' : '';
-    if (strtotime(date('Y-m-d')) <= strtotime($tglAbsen)) {
     } else {
       echo "
        </tbody>
@@ -505,7 +504,6 @@
               <button type='submit' name='simpann' class='btn btn-info pull-right' >Simpan Absensi</button>
             </div>
         ";
-    }
   }
 
   echo "
