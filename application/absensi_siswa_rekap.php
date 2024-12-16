e8fbc8d7f45ea9d9baa2d235b7452b1e8cf39c34
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-12-16 08:56:28 +0700

    sdsazd

--- application/absensi_siswa_rekap.php
+++ application/absensi_siswa_rekap.php
@@ -130,19 +130,19 @@
 } elseif ($_GET[act] == 'tampilabsen') {
 $d = mysql_fetch_array(mysql_query("SELECT * FROM rb_kelas where kode_kelas='$_GET[id]'"));
 $m = mysql_fetch_array(mysql_query("SELECT * FROM rb_mata_pelajaran where kode_pelajaran='$_GET[kd]'"));
-
 echo "<div class='col-md-12 table-responsive'>
         <div class='box box-info table-responsive'>
             <div class='box-header with-border'>
-                <h3 class='box-title'>Rekap Data Absensi Siswa Pada $tahun</h3>
+                <h3 class='box-title'>Rekap Data Absensi Siswa Pada $_GET[tahun]</b></h3>
             </div>
             <div class='box-body'>
                 <div class='col-md-12'>
                     <table class='table table-condensed table-hover'>
                         <tbody>
-                            <tr><th width='120px' scope='row'>Kode Kelas</th> <td>{$d['kode_kelas']}</td></tr>
-                            <tr><th scope='row'>Nama Kelas</th> <td>{$d['nama_kelas']}</td></tr>
-                            <tr><th scope='row'>Mata Pelajaran</th> <td>{$m['namamatapelajaran']}</td></tr>
+                            <input type='hidden' name='id' value='$s[kode_kelas]'>
+                            <tr><th width='120px' scope='row'>Kode Kelas</th> <td>$d[kode_kelas]</td></tr>
+                            <tr><th scope='row'>Nama Kelas</th> <td>$d[nama_kelas]</td></tr>
+                            <tr><th scope='row'>Mata Pelajaran</th> <td>$m[namamatapelajaran]</td></tr>
                         </tbody>
                     </table>
                 </div>
@@ -164,44 +164,53 @@ echo "<div class='col-md-12 table-responsive'>
                         </thead>
                         <tbody>";
 
-$no = 1;
-// Ambil data siswa berdasarkan kelas
-$siswa = $koneksi->query("SELECT * FROM rb_siswa a 
-                          JOIN rb_jenis_kelamin b ON a.id_jenis_kelamin=b.id_jenis_kelamin 
-                          WHERE a.kode_kelas='$id_kelas' 
-                          ORDER BY a.id_siswa");
+            $no = 1;
+            $tampil = mysql_query("SELECT * FROM rb_siswa a JOIN rb_jenis_kelamin b ON a.id_jenis_kelamin=b.id_jenis_kelamin where a.kode_kelas='$_GET[id]' ORDER BY a.id_siswa");
+            while ($r = mysql_fetch_array($tampil)) {
+                $total = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` where kodejdwl='$_GET[jdwl]' GROUP BY tanggal"));
+                $hadir = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` where kodejdwl='$_GET[jdwl]' AND nisn='$r[nisn]' AND kode_kehadiran='H'"));
+                $sakit = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` where kodejdwl='$_GET[jdwl]' AND nisn='$r[nisn]' AND kode_kehadiran='S'"));
+                $izin = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` where kodejdwl='$_GET[jdwl]' AND nisn='$r[nisn]' AND kode_kehadiran='I'"));
+                $alpa = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` where kodejdwl='$_GET[jdwl]' AND nisn='$r[nisn]' AND kode_kehadiran='A'"));
+                
+                $akademik_query = mysql_query("SELECT * FROM `rb_absensi_siswa` WHERE kodejdwl='$_GET[jdwl]' AND nisn='$r[nisn]'");
+                $total_nilai_sikap = 0;
+                $total_nilai_keterampilan = 0;
+                $total_nilai_pengetahuan = 0;
+                $jumlah_pertemuan = 0;
 
-while ($r = $siswa->fetch_assoc()) {
-    // Hitung jumlah kehadiran
-    $total = $koneksi->query("SELECT DISTINCT tanggal FROM rb_absensi_siswa WHERE kodejdwl='$kode_jadwal'")->num_rows;
-    $hadir = $koneksi->query("SELECT * FROM rb_absensi_siswa WHERE kodejdwl='$kode_jadwal' AND nisn='{$r['nisn']}' AND kode_kehadiran='H'")->num_rows;
-    $sakit = $koneksi->query("SELECT * FROM rb_absensi_siswa WHERE kodejdwl='$kode_jadwal' AND nisn='{$r['nisn']}' AND kode_kehadiran='S'")->num_rows;
-    $izin = $koneksi->query("SELECT * FROM rb_absensi_siswa WHERE kodejdwl='$kode_jadwal' AND nisn='{$r['nisn']}' AND kode_kehadiran='I'")->num_rows;
-    $alpa = $koneksi->query("SELECT * FROM rb_absensi_siswa WHERE kodejdwl='$kode_jadwal' AND nisn='{$r['nisn']}' AND kode_kehadiran='A'")->num_rows;
+                while ($akademik = mysql_fetch_array($akademik_query)) {
+                    $total_nilai_sikap += $akademik['nilai_sikap'];
+                    $total_nilai_keterampilan += $akademik['nilai_keterampilan'];
+                    $total_nilai_pengetahuan += $akademik['nilai_pengetahuan'];
+                    $jumlah_pertemuan++;
+                }
 
-    // Persentase kehadiran
-    $persen = ($total > 0) ? ($hadir / $total) * 100 : 0;
+                // Hitung rata-rata
+                $divider = $jumlah_pertemuan * 3; // Total nilai sikap, keterampilan, pengetahuan per pertemuan
+                $rata_rata = ($divider > 0) ? (($total_nilai_sikap + $total_nilai_keterampilan + $total_nilai_pengetahuan) / $divider) : 0;
 
-    echo "<tr>
-            <td>$no</td>
-            <td>{$r['nisn']}</td>
-            <td>{$r['nama']}</td>
-            <td>{$r['jenis_kelamin']}</td>
-            <td align='center'>$total</td>
-            <td align='center'>$hadir</td>
-            <td align='center'>$sakit</td>
-            <td align='center'>$izin</td>
-            <td align='center'>$alpa</td>
-            <td align='right'>".number_format($persen, 2)." %</td>
-          </tr>";
-    $no++;
-}
+                $persen = $hadir / ($total) * 100;
+                echo "<tr>
+                        <td>$no</td>
+                        <td>$r[nisn]</td>
+                        <td>$r[nama]</td>
+                        <td>$r[jenis_kelamin]</td>
+                        <td align=center>$jumlah_pertemuan</td>
+                        <td align=center>$hadir</td>
+                        <td align=center>$sakit</td>
+                        <td align=center>$izin</td>
+                        <td align=center>$alpa</td>
+                        <td align=right>".number_format($persen, 2)." %</td>
+                      </tr>";
+                $no++;
+            }
 
-echo "</tbody></table>
+            echo "</tbody>
+                  </table>
                 </div>
-            </div>
-        </div>
-    </div>";
+              </div>
+            </div>";
 
 }
 ?>
