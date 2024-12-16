43cde290aae39fa99f69ff13bc3ba4f0e7ebbeb0
Baihaqi34 <abay999mmm@gmail.com>
2024-12-16 08:43:50 +0700

    ds

--- application/absensi_siswa_detail.php
+++ application/absensi_siswa_detail.php
@@ -1,11 +1,12 @@
 <?php
-if ($_GET[act] == '') {
+if ($_GET['act'] == '') {
     $d = mysql_fetch_array(mysql_query("SELECT * FROM rb_kelas where kode_kelas='$_GET[id]'"));
     $m = mysql_fetch_array(mysql_query("SELECT * FROM rb_mata_pelajaran where kode_pelajaran='$_GET[kd]'"));
     echo "<div class='col-md-12'>
               <div class='box box-info'>
                 <div class='box-header with-border'>
-                  <h3 class='box-title'>Rekap Data Absensi Guru $_GET[tahun]</b></h3>
+                  <h3 class='box-title'>Rekap Data Absensi Guru $_GET[tahun]</h3>
+                  <button onclick='window.print()' class='btn btn-primary btn-sm' style='float: right;'>Print</button>
                 </div>
               <div class='box-body'>
 
@@ -19,7 +20,7 @@ if ($_GET[act] == '') {
                     <thead>
                       <tr>
                         <th>No</th>
-                        <th>Nip</th>
+                        <th>NIP</th>
                         <th>Nama Guru</th>
                         <th>Hadir</th>
                         <th>Sakit</th>
@@ -32,19 +33,12 @@ if ($_GET[act] == '') {
     $no = 1;
     $tampil = mysql_query("SELECT * FROM rb_guru WHERE id_jenis_ptk NOT IN (6, 7) ORDER BY nama_guru ASC");
     while ($r = mysql_fetch_array($tampil)) {
-      // var_dump($r);
-        // $total = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_siswa` where kodejdwl='$_GET[jdwl]' GROUP BY tanggal"));
         $hadir = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_guru` where nip='$r[nip]' AND kode_kehadiran='Hadir'"));
         $sakit = mysql_num_rows(mysql_query("SELECT * FROM `rb_rekap_absen_guru` where nip='$r[nip]' AND kode_kehadiran='sakit'"));
         $izin = mysql_num_rows(mysql_query("SELECT * FROM `rb_rekap_absen_guru` where nip='$r[nip]' AND kode_kehadiran='izin'"));
         $alpa = mysql_num_rows(mysql_query("SELECT * FROM `rb_rekap_absen_guru` where nip='$r[nip]' AND kode_kehadiran='alpa'"));
-        // $persen = $hadir / ($total) * 100;
-        // <th><center>% Kehadiran</center></th>
 
-        // <td align=right>" . number_format($persen, 2) . " %</td>";
-
-        // var_dump($hadir);
-        echo "<tr bgcolor=$warna>
+        echo "<tr>
                             <td>$no</td>
                             <td>$r[nip]</td>
                             <td>$r[nama_guru]</td>
@@ -74,6 +68,13 @@ if ($_GET[act] == '') {
     .table-responsive {
       overflow-x: visible;
       /* Nonaktifkan scroll horizontal di desktop */
+    }
+  }
+
+  @media print {
+    .btn {
+      display: none;
+      /* Sembunyikan tombol saat dicetak */
     }
   }
 </style>
