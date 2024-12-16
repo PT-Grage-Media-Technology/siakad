af0fda090a0adf207ce0af903477ee564c871088
Baihaqi34 <abay999mmm@gmail.com>
2024-12-16 08:46:24 +0700

    sasas

--- application/absensi_siswa_detail.php
+++ application/absensi_siswa_detail.php
@@ -1,52 +1,74 @@
 <?php
 if ($_GET['act'] == '') {
-    $d = mysql_fetch_array(mysql_query("SELECT * FROM rb_kelas where kode_kelas='$_GET[id]'"));
-    $m = mysql_fetch_array(mysql_query("SELECT * FROM rb_mata_pelajaran where kode_pelajaran='$_GET[kd]'"));
+    $tahun = $_GET['tahun'] ?? date('Y');
+    $bulan = $_GET['bulan'] ?? date('m');
+
+    // Nama bulan
+    $nama_bulan = date("F", mktime(0, 0, 0, $bulan, 10));
+    $jumlah_hari = cal_days_in_month(CAL_GREGORIAN, $bulan, $tahun);
+
     echo "<div class='col-md-12'>
               <div class='box box-info'>
                 <div class='box-header with-border'>
-                  <h3 class='box-title'>Rekap Data Absensi Guru $_GET[tahun]</h3>
+                  <h3 class='box-title'>Rekap Data Absensi Guru - $nama_bulan $tahun</h3>
                   <button onclick='window.print()' class='btn btn-primary btn-sm' style='float: right;'>Print</button>
                 </div>
               <div class='box-body'>
-
-              <div class='col-md-12'>
-              
-              </div>
-
-              <div class='col-md-12'>
                 <div class='table-responsive'>
                   <table class='table table-condensed table-bordered table-striped table-hover'>
                     <thead>
                       <tr>
-                        <th>No</th>
-                        <th>NIP</th>
-                        <th>Nama Guru</th>
-                        <th>Hadir</th>
-                        <th>Sakit</th>
-                        <th>Izin</th>
-                        <th>Alpa</th>
+                        <th rowspan='2' style='vertical-align: middle;'>No</th>
+                        <th rowspan='2' style='vertical-align: middle;'>NIP</th>
+                        <th rowspan='2' style='vertical-align: middle;'>Nama Guru</th>";
+                        
+    // Header tanggal dinamis
+    for ($i = 1; $i <= $jumlah_hari; $i++) {
+        echo "<th>$i</th>";
+    }
+
+    echo "          <th rowspan='2' style='vertical-align: middle;'>Hadir</th>
+                        <th rowspan='2' style='vertical-align: middle;'>Sakit</th>
+                        <th rowspan='2' style='vertical-align: middle;'>Izin</th>
+                        <th rowspan='2' style='vertical-align: middle;'>Alpa</th>
                       </tr>
+                      <tr>";
+
+    // Subheader untuk tanggal
+    for ($i = 1; $i <= $jumlah_hari; $i++) {
+        echo "<th style='font-size: 10px;'>".date('D', strtotime("$tahun-$bulan-$i"))."</th>";
+    }
+
+    echo "          </tr>
                     </thead>
                     <tbody>";
 
     $no = 1;
     $tampil = mysql_query("SELECT * FROM rb_guru WHERE id_jenis_ptk NOT IN (6, 7) ORDER BY nama_guru ASC");
     while ($r = mysql_fetch_array($tampil)) {
-        $hadir = mysql_num_rows(mysql_query("SELECT * FROM `rb_absensi_guru` where nip='$r[nip]' AND kode_kehadiran='Hadir'"));
-        $sakit = mysql_num_rows(mysql_query("SELECT * FROM `rb_rekap_absen_guru` where nip='$r[nip]' AND kode_kehadiran='sakit'"));
-        $izin = mysql_num_rows(mysql_query("SELECT * FROM `rb_rekap_absen_guru` where nip='$r[nip]' AND kode_kehadiran='izin'"));
-        $alpa = mysql_num_rows(mysql_query("SELECT * FROM `rb_rekap_absen_guru` where nip='$r[nip]' AND kode_kehadiran='alpa'"));
-
         echo "<tr>
-                            <td>$no</td>
-                            <td>$r[nip]</td>
-                            <td>$r[nama_guru]</td>
-                            <td align=center>$hadir</td>
-                            <td align=center>$sakit</td>
-                            <td align=center>$izin</td>
-                            <td align=center>$alpa</td>";
-        echo "</tr>";
+                <td>$no</td>
+                <td>$r[nip]</td>
+                <td>$r[nama_guru]</td>";
+
+        // Kolom absensi untuk setiap tanggal dalam bulan
+        for ($i = 1; $i <= $jumlah_hari; $i++) {
+            $tanggal = "$tahun-$bulan-$i";
+            $cek_absen = mysql_num_rows(mysql_query("SELECT * FROM rb_absensi_guru WHERE nip='$r[nip]' AND tanggal='$tanggal'"));
+            echo "<td align='center'>".($cek_absen > 0 ? '✔' : '')."</td>";
+        }
+
+        // Hitung total kehadiran
+        $hadir = mysql_num_rows(mysql_query("SELECT * FROM rb_absensi_guru WHERE nip='$r[nip]' AND kode_kehadiran='Hadir' AND MONTH(tanggal)='$bulan' AND YEAR(tanggal)='$tahun'"));
+        $sakit = mysql_num_rows(mysql_query("SELECT * FROM rb_absensi_guru WHERE nip='$r[nip]' AND kode_kehadiran='Sakit' AND MONTH(tanggal)='$bulan' AND YEAR(tanggal)='$tahun'"));
+        $izin = mysql_num_rows(mysql_query("SELECT * FROM rb_absensi_guru WHERE nip='$r[nip]' AND kode_kehadiran='Izin' AND MONTH(tanggal)='$bulan' AND YEAR(tanggal)='$tahun'"));
+        $alpa = mysql_num_rows(mysql_query("SELECT * FROM rb_absensi_guru WHERE nip='$r[nip]' AND kode_kehadiran='Alpa' AND MONTH(tanggal)='$bulan' AND YEAR(tanggal)='$tahun'"));
+
+        echo "  <td align='center'>$hadir</td>
+                <td align='center'>$sakit</td>
+                <td align='center'>$izin</td>
+                <td align='center'>$alpa</td>
+              </tr>";
         $no++;
     }
 
@@ -61,20 +83,16 @@ if ($_GET['act'] == '') {
 <style>
   .table-responsive {
     overflow-x: auto;
-    /* Hanya aktifkan scroll horizontal jika diperlukan */
-  }
-
-  @media (min-width: 768px) {
-    .table-responsive {
-      overflow-x: visible;
-      /* Nonaktifkan scroll horizontal di desktop */
-    }
   }
 
   @media print {
     .btn {
       display: none;
-      /* Sembunyikan tombol saat dicetak */
     }
+  }
+
+  th, td {
+    text-align: center;
+    vertical-align: middle;
   }
 </style>
