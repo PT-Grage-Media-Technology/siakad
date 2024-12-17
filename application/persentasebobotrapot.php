2aa9172792ba6967bbceaac9d1cb863c9e02f648
Baihaqi34 <abay999mmm@gmail.com>
2024-12-17 10:33:09 +0700

    sasas

--- application/persentasebobotrapot.php
+++ application/persentasebobotrapot.php
@@ -1,60 +1,37 @@
-<div class="col-xs-12">
-  <div class="box">
-    <div class="box-header">
-      <h3 class="box-title">Data Bobot Nilai Raport </h3>
-      <?php if ($_SESSION['level'] != 'kepala') { ?>
-        <!-- <a class='pull-right btn btn-primary btn-sm' href='index.php?view=nilai'>Tambahkan Data</a> -->
-      <?php } ?>
-    </div><!-- /.box-header -->
-    <div class="box-body">
-      <div class="table-responsive" style="max-height: 500px; overflow-y: auto;">
-        <!-- <p><input type="text"> = <input type="text"> - <input type="text"></p> -->
-        <?php
-        if (isset($_POST['update'])) {
-          $bobot = $_POST['bobot'];
-          $id = $_POST['id'];
-          mysql_query("UPDATE rb_bobot_raport SET bobot='$bobot' WHERE id='$id'");
-          echo "UPDATE rb_bobot_raport SET bobot='$bobot' WHERE id='$id'";
-        
-        }
-        ?>
+<?php
+if (isset($_POST['update'])) {
+  foreach ($_POST['bobot'] as $id => $bobot) {
+    // Escape input untuk mencegah SQL Injection
+    $id_safe = mysql_real_escape_string($id);
+    $bobot_safe = mysql_real_escape_string($bobot);
 
-        <?php
+    // Update hanya nilai bobot yang dikirimkan
+    mysql_query("UPDATE rb_bobot_raport SET bobot='$bobot_safe' WHERE id='$id_safe'");
+  }
+  echo "<div class='alert alert-success'>Data berhasil diperbarui!</div>";
+}
 
-        $tampil = mysql_query("SELECT * FROM rb_bobot_raport");
+$tampil = mysql_query("SELECT * FROM rb_bobot_raport");
 
-        //   var_dump($kriteriaNilai) ; // Mengambil data dari array ke-0
-        //   echo $kriteriaNilai['kode_nilai'][0];
-        // Form untuk mengupdate semua data
-        echo "<form method='POST' action=''>";
+// Form untuk menampilkan semua data
+echo "<form method='POST' action=''>";
+echo "<table style='border-collapse: collapse; width: auto;'>";
 
-        // Loop untuk menampilkan semua data dalam satu form
-        while ($kriteriaNilai = mysql_fetch_array($tampil)) {
-      
-          echo "<table style='border-collapse: collapse; width: auto;'>
-          <tr>
-            <td style='text-align: left; vertical-align: middle; padding: 3px;'>
-              {$kriteriaNilai['jenis_nilai']} =
-              <input type='hidden' name='id' value='{$kriteriaNilai['id']}'>
-            </td>
-            <td style='text-align: center; vertical-align: middle; padding: 3px;'>
-              <input name='bobot' type='number' value='{$kriteriaNilai['bobot']}'
-                     style='width: 50px; text-align: center; margin: 0;'>
-            </td>
-            <td style='text-align: left; vertical-align: middle; padding: 3px;'>%</td>
-          </tr>
-        </table>";
-  
-        
-        }
+while ($kriteriaNilai = mysql_fetch_array($tampil)) {
+  echo "
+    <tr>
+        <td style='text-align: left; vertical-align: middle; padding: 3px;'>
+            {$kriteriaNilai['jenis_nilai']} =
+        </td>
+        <td style='text-align: center; vertical-align: middle; padding: 3px;'>
+            <input name='bobot[{$kriteriaNilai['id']}]' type='number' value='{$kriteriaNilai['bobot']}'
+                   style='width: 50px; text-align: center; margin: 0;'>
+        </td>
+        <td style='text-align: left; vertical-align: middle; padding: 3px;'>%</td>
+    </tr>";
+}
 
-        // Tombol Update untuk mengupdate semua data
-        echo "<button type='submit' name='update' class='pull-right btn btn-primary btn-sm mt-2'>Update Semua</button>";
-        echo "</form>";
-
-        ?>
-
-      </div><!-- /.table-responsive -->
-    </div><!-- /.box-body -->
-  </div><!-- /.box -->
-</div>
+echo "</table>";
+echo "<button type='submit' name='update' class='pull-right btn btn-primary btn-sm mt-2'>Update Semua</button>";
+echo "</form>";
+?>
