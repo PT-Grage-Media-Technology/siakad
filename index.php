c1f1637654ab59f18c446493690a24022e3724db
Baihaqi34 <abay999mmm@gmail.com>
2024-12-19 13:32:58 +0700

    fdxg

--- index.php
+++ index.php
@@ -838,7 +838,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['action']) && $_POST['a
         $data = mysql_fetch_assoc($result);
         // Mengirim data dalam format teks
         // echo "id_pertanyaan_objektif={$data['id_pertanyaan_objektif']}&soal={$data['pertanyaan_objektif']}&jawab_a={$data['jawab_a']}&jawab_b={$data['jawab_b']}&jawab_c={$data['jawab_c']}&jawab_d={$data['jawab_d']}&jawab_e={$data['jawab_e']}&kunci={$data['kunci']}";
-        echo json_encode($dataArray);
+        // echo json_encode($dataArray);
         // var_dump($data);
     } else {
         echo "error=Data tidak ditemukan";
