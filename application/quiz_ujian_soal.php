e8a46f8524f67cc14f433344a28493b63d402196
PKLGrage <pklgrage@gmail.com>
2024-12-24 10:03:14 +0700

    xfxd

--- application/quiz_ujian_soal.php
+++ application/quiz_ujian_soal.php
@@ -689,7 +689,7 @@ $quiz = mysql_fetch_array(mysql_query("
     rb_pertanyaan_essai.* 
     FROM rb_pertanyaan_objektif
     JOIN rb_pertanyaan_essai 
-    ON rb_pertanyaan_objektif.id_soal = rb_pertanyaan_essai.id_soal
+    ON rb_pertanyaan_objektif.id_quiz_ujian = rb_pertanyaan_essai.id_quiz_ujian
     WHERE rb_pertanyaan_objektif.id_quiz_ujian = '$_GET[id_soal]'
 "));
 
