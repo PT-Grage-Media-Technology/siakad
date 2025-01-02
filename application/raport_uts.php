ed7f849552c3b5cc6788f5e4943d6d72f75f19bc
Baihaqi34 <abay999mmm@gmail.com>
2025-01-02 09:41:39 +0700

    sass

--- application/raport_uts.php
+++ application/raport_uts.php
@@ -183,7 +183,6 @@ cek_session_guru();
                     $grade1 = mysql_fetch_array(mysql_query("SELECT * FROM `rb_predikat` where ($n[angka_pengetahuan] >=nilai_a) AND ($n[angka_pengetahuan] <= nilai_b) AND kode_kelas='0'"));
                     $grade2 = mysql_fetch_array(mysql_query("SELECT * FROM `rb_predikat` where ($n[angka_keterampilan] >=nilai_a) AND ($n[angka_keterampilan] <= nilai_b) AND kode_kelas='0'"));
                   }
-                  // echo "SELECT * FROM rb_nilai_quiz ni JOIN rb_quiz_ujian qu ON ni.id_quiz_ujian=qu.id_quiz where ni.kodejdwl = '$_GET[jdwl]' AND ni.nisn = '$r[nisn]' AND ni.kategori_quiz = 4";
                     echo "<tr>
                           <td>$no</td>
                           <td>$r[nisn]</td>
@@ -194,19 +193,10 @@ cek_session_guru();
 
                           // $cekSTS = mysql_query("SELECT * FROM rb_nilai_sts")
                           $cekQuiz = mysql_fetch_array(mysql_query(
-                          "SELECT ni.* 
-                          FROM rb_nilai_quiz ni 
-                          JOIN rb_quiz_ujian qu ON ni.id_quiz = qu.id_quiz_ujian 
-                          WHERE qu.kodejdwl = '$_GET[jdwl]' 
-                            AND ni.nisn = '$r[nisn]' 
-                            AND qu.kategori_quiz = 4"
-
+                            "SELECT * FROM rb_nilai_quiz where kodejdwl = '$_GET[jdwl]' AND nisn = '$r[nisn]' AND kategori_quiz = 3"
                           ));
-
-                          // echo $cekQuiz;
                           
                           if($cekQuiz){
-                            // echo $cekQuiz;
                           echo"<input type='number' name='a".$no."' value='$cekQuiz[nilai]' style='width:90px; text-align:center; padding:0px' placeholder='-' colspan='2'>";
 
                           }else{
